754b8b43dd81ec35425d075160e778a8786afaa0
xiaolan <wollt1992@163.com>
2017-02-23 11:44:03 +0800

    增加Receiver代码的channelId和packageName保护
    N/A
    
    Change-Id: I618e9094c8f3efeb0276f000abf6aef5185c64a4
    Signed-off-by: xiaolan <wollt1992@163.com>

--- pluginmanager/src/main/java/com/wali/live/watchsdk/ipc/receiver/MiLiveSdkReceiver.java
+++ pluginmanager/src/main/java/com/wali/live/watchsdk/ipc/receiver/MiLiveSdkReceiver.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.wali.live.sdk.manager.MiLiveSdkController;
+import com.wali.live.sdk.manager.global.GlobalData;
 import com.wali.live.sdk.manager.log.Logger;
 import com.wali.live.watchsdk.ipc.service.MiLiveSdkEvent;
 
@@ -13,15 +15,18 @@ import com.wali.live.watchsdk.ipc.service.MiLiveSdkEvent;
 public class MiLiveSdkReceiver extends BroadcastReceiver {
     public final static String TAG = MiLiveSdkReceiver.class.getSimpleName();
 
-    @Override
     public void onReceive(Context context, Intent intent) {
         if (intent != null) {
             String action = intent.getAction();
             String packageName = intent.getPackage();
-            Logger.w(TAG, "action:" + action + ";package=" + packageName);
+            int channelId = intent.getIntExtra(ReceiverConstant.EXTRA_CHANNEL_ID, 0);
+            Logger.d(TAG, "action:" + action + ";package=" + packageName + ";channelId=" + channelId);
 
-            // 保证是自己app和action发出的闹钟。
-            if (action.equals(ReceiverConstant.ACTION_WANT_LOGIN)) {
+            // 保证是自己app和action发出的闹钟
+            if (action.equals(ReceiverConstant.ACTION_WANT_LOGIN) &&
+                    packageName.equals(GlobalData.app().getPackageName()) &&
+                    channelId == MiLiveSdkController.getInstance().getChannelId()) {
+                Logger.d(TAG, "want login");
                 MiLiveSdkEvent.postWantLogin();
             }
         }
