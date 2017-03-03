cdc8e1bb7ed79ee601bee29376c38126cee1452a
chenyong1 <chenyong1@xiaomi.com>
2017-03-03 12:19:32 +0800

    优化打包脚本
    Change-Id: I54c12925b48670e2736f9a22fcd62d6a76e752c5

--- livesdkapp/gen_build_settings.py
+++ livesdkapp/gen_build_settings.py
@@ -2,6 +2,21 @@
 # -*- coding: utf-8 -*-
 
 import sys
+
+f = open('../common/src/main/java/com/base/utils/Constants.java', 'r')
+s = ''
+channlName = sys.argv[1];
+if (channlName == 'DEFAULT_1'):
+    channlName = 'DEFAULT'
+for line in f:
+  line = line.replace("@SHIP.TO.2A2FE0D7@", channlName)
+  s += line
+f.close()
+#修改了constant的包路径
+f = open('../common/src/main/java/com/base/utils/Constants.java', 'w')
+f.write(s)
+f.close()
+
 f = open('./src/main/AndroidManifest.xml', 'r')
 s = ''
 for line in f:
@@ -10,3 +25,15 @@ for line in f:
 f.close()
 f = open('./src/main/AndroidManifest.xml', 'w')
 f.write(s)
+f.close()
+
+if(sys.argv[1] == 'debugmi'):
+    f = open('./build.gradle', 'r')
+    s = ''
+    for line in f:
+      line = line.replace("//    debugCompile project(':debugmi')", "compile project(':debugmi')")
+      s += line
+    f.close()
+    f = open('./build.gradle', 'w')
+    f.write(s)
+    f.close()
