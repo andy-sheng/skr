e827853c68a72c800a9125c2d979a87434d3c7aa
chenyong1 <chenyong1@xiaomi.com>
2017-04-06 17:54:03 +0800

    整体结构调整
    Change-Id: I148a61084b0d76d265f32e8a3339c1395cdf1ebd

---
+++ aardemo/src/main/java/com/wali/live/sdk/manager/aardemo/AARDemoApplication.java
@@ -0,0 +1,8 @@
+package com.wali.live.sdk.manager.aardemo;
+
+/**
+ * Created by chenyong on 2017/4/6.
+ */
+
+public class AARDemoApplication {
+}
---
+++ aardemo/src/main/java/com/wali/live/sdk/manager/aardemo/MainActivity.java
@@ -0,0 +1,78 @@
+package com.wali.live.sdk.manager.aardemo;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+
+import com.wali.live.livesdk.live.IMiLiveSdk;
+import com.wali.live.livesdk.live.MiLiveSdkController;
+import com.wali.live.sdk.manager.aardemo.global.GlobalData;
+import com.wali.live.sdk.manager.aardemo.utils.ToastUtils;
+
+public class MainActivity extends AppCompatActivity {
+    private final static String TAG = "MainActivity";
+
+    public static final int CHANNEL_ID = 50000;
+
+    private int currentChannelId = CHANNEL_ID;
+
+    private RecyclerView mRecyclerView;
+    private MenuRecyclerAdapter mMenuRecyclerAdapter;
+    private TextView mChannelTv;
+
+    private Handler mUiHander = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mChannelTv = (TextView) findViewById(R.id.channel_tv);
+        mRecyclerView = (RecyclerView) findViewById(R.id.menu_recyclerview);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        mMenuRecyclerAdapter = new MenuRecyclerAdapter(this);
+        mRecyclerView.setAdapter(mMenuRecyclerAdapter);
+        GlobalData.setApplication(this.getApplication());
+        //建议在 application里初始化这个
+        MiLiveSdkController.getInstance().init(this.getApplication(), CHANNEL_ID, "TEST SECRET", new IMiLiveSdk.ICallback() {
+
+            @Override
+            public void notifyLogin(int var1) {
+                if (var1 == IMiLiveSdk.ICallback.CODE_SUCCESS) {
+                    ToastUtils.showToast("登录成功");
+                } else {
+                    ToastUtils.showToast("登录错误，错误码：" + var1);
+                }
+            }
+
+            @Override
+            public void notifyLogoff(int var1) {
+                if (var1 == IMiLiveSdk.ICallback.CODE_SUCCESS) {
+                    ToastUtils.showToast("登出成功");
+                }
+            }
+
+            @Override
+            public void notifyWantLogin() {
+                ToastUtils.showToast("用户触发了只有登录才有的操作,回调给宿主,宿主传递账号信息给插件");
+                mMenuRecyclerAdapter.oauthLogin();
+            }
+
+            @Override
+            public void notifyVerifyFailure(int var1) {
+                ToastUtils.showToast("验证失败，errCode=" + var1);
+            }
+
+            @Override
+            public void notifyOtherAppActive() {
+                ToastUtils.showToast("有其他APP在活跃");
+            }
+        });
+    }
+
+    public int getCurrentChannelId() {
+        return currentChannelId;
+    }
+}
