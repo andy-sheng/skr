cc5f9e549134f104a31a1d4805f9c32aebc4c03c
chenyong1 <chenyong1@xiaomi.com>
2017-04-06 18:09:43 +0800

    更新脚本
    Change-Id: I498495caa5f84bc1164df9c55b48605f3507a9fe

--- aardemo/src/main/java/com/wali/live/sdk/manager/aardemo/AARDemoApplication.java
+++ aardemo/src/main/java/com/wali/live/sdk/manager/aardemo/AARDemoApplication.java
@@ -1,8 +1,24 @@
 package com.wali.live.sdk.manager.aardemo;
 
+import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
+import com.base.log.MyLog;
+
 /**
  * Created by chenyong on 2017/4/6.
  */
 
-public class AARDemoApplication {
+public class AARDemoApplication extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        try {
+            MultiDex.install(this);
+        } catch (Exception e) {
+            MyLog.e(e);
+        }
+    }
 }
--- livesdkaar/build_aar.sh
+++ livesdkaar/build_aar.sh
@@ -5,4 +5,4 @@ gradle assembleRelease --info
 if [ ! -d "../livesdkapp/libs" ]; then
   mkdir "../livesdkapp/libs"
 fi
-cp build/outputs/aar/livesdkaar-release.aar ../pluginaardemo/libs/livesdk.aar
+cp build/outputs/aar/livesdkaar-release.aar ../aardemo/libs/livesdk.aar
