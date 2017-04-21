19626fce4e087d97c6b0aae97d6bdb78383067a7
xiaolan <wollt1992@163.com>
2017-04-21 17:30:46 +0800

    移到common包里
    N/A
    
    Change-Id: I58708be0c5448b18237803264e8ccdc5cb759666
    Signed-off-by: xiaolan <wollt1992@163.com>

--- lite/src/main/java/com/mi/liveassistant/common/global/GlobalData.java
+++ lite/src/main/java/com/mi/liveassistant/common/global/GlobalData.java
@@ -1,4 +1,4 @@
-package com.mi.liveassistant.global;
+package com.mi.liveassistant.common.global;
 
 import android.app.Application;
 import android.content.Context;
--- lite/src/main/java/com/mi/liveassistant/common/preference/PreferenceUtils.java
+++ lite/src/main/java/com/mi/liveassistant/common/preference/PreferenceUtils.java
@@ -1,98 +1,41 @@
-package com.mi.liveassistant.preference;
+package com.mi.liveassistant.common.preference;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
-import com.base.global.GlobalData;
-import com.base.log.MyLog;
+import com.mi.liveassistant.common.log.MyLog;
+import com.mi.liveassistant.common.global.GlobalData;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public abstract class PreferenceUtils {
     private static final String TAG = PreferenceUtils.class.getSimpleName();
-    private static final ArrayList<PrefObserver> sPrefObs = new ArrayList<>();
-
-    public static final String KEY_RELEASE_CHANNEL = "pref_channel";
-
-    // 生成attachment_id
-    public static final String PREF_KEY_ATTACHMENT_BASE_ID = "pref_key_attachment_base_id";
-
-    // 是否开启debug信息，boolean值
-    public static final String KEY_DEBUG_INFO = "pref_debug_info";
-    // 是否开启媒体参数调节界面，boolean值
-    public static final String KEY_DEBUG_MEDIA_INFO = "pref_debug_media_info";
-    // 是否开启低码率，boolean值
-    public static final String KEY_DEBUG_DROP_BITRATE = "pref_debug_drop_bitrate";
-
-    public static final String KEY_DEBUG_FIX_STREAMER = "pref_debug_fix_streamer";
-
-    public static final String KEY_LIVE_ID = "key_live_id";
-
-    public static final String KEY_LIVE_TIMESTAMP = "key_live_timestamp";
-
-    public static final String KEY_LIVE_CLARITY = "key_live_clarity";
-
-    public static final String PREF_KEY_UNZIP_MEDIA_FILE = "pref_key_unzip_media_file";
-
-    public static final String PREF_KEY_MEDIA_FILE_VERSION_CODE = "pref_key_media_file_version_code";
-
-    public static final String PREF_KEY_FACE_BEAUTY_LEVEL = "pref_key_face_beauty_level"; //保存上次选中的美颜等级
-
-    public static final String PREF_KEY_FILTER_INTENSITY = "pref_key_filter_intensity";
-
-    public static final String PREF_KEY_FILTER_CATEGORY = "pref_key_filter_category";
-
-    public static final String KEY_ACCEPT_CONVENTION = "key_accept_convention";
-
-    public static final String KEY_FEED_NOTIFY_UNREAD_COUNT = "key_feed_notify_unread_count";
-
-    public static final String KEY_FEED_NOTIFY_AVATAR_URL = "key_feed_notify_avatar_url";
-
-    public static final String KET_HAS_NEW_FEEDS = "ket_has_new_feeds";
-
-    public static final String KEY_LAST_SYNC_NEW_FEEDS_TIME = "key_last_sync_new_feeds_time";
-
-    public static final String KEY_CREATE_FAILED_FEEDS_ID = "key_create_failed_feeds_id";
-
-    public static final String PREF_KEY_SIXIN_SYNC_PAGE_ID = "pref_sixin_sync_page_id";
-
-    public static final String PREF_ANCHOR_RANK_LIST_CONFIG = "pref_anchor_rank_list_config";
-
-    public static final String PREF_ANCHOR_RANK_LIST_CONFIG_TIME = "pref_anchor_rank_list_config_time";
-
-    //搜索框显示的文案
-    public static final String PREF_FUZZY_SEARCH_DEFAULT_KEY = "fuzzy_search_default_key_list";
-
-    //搜索框显示的文案以及对应的搜索词
-    public static final String PREF_FUZZY_SEARCH_DEFAULT_TEXT = "fuzzy_search_default_text_list";
-
-    public static final String PREF_KEY_LIVE_NORMAL_TAG = "pref_key_live_normal_tag";
-    public static final String PREF_KEY_LIVE_GAME_TAG = "pref_key_live_game_tag";
-    public static final String PREF_KEY_LIVE_GAME_CLARITY = "pref_key_live_game_clarity";
+    private static final List<PrefObserver> PREF_OBSERVER_LIST = new ArrayList();
 
     public interface PrefObserver {
         void notifyPrefChange(final String key, Object value);
     }
 
     public static void addPrefObserver(final PrefObserver ob) {
-        sPrefObs.add(ob);
+        PREF_OBSERVER_LIST.add(ob);
     }
 
     public static void removePrefObserver(final PrefObserver obToRm) {
-        for (final PrefObserver ob : sPrefObs) {
+        for (final PrefObserver ob : PREF_OBSERVER_LIST) {
             if (ob == obToRm) {
-                sPrefObs.remove(ob);
+                PREF_OBSERVER_LIST.remove(ob);
                 break;
             }
         }
     }
 
     public static void notifyPrefChange(final String key, final Object value) {
-        for (final PrefObserver ob : sPrefObs) {
+        for (final PrefObserver ob : PREF_OBSERVER_LIST) {
             ob.notifyPrefChange(key, value);
         }
     }
@@ -105,20 +48,19 @@ public abstract class PreferenceUtils {
         return sp.getString(key, defaultValue);
     }
 
-    public static String getSettingString(final Context c, final String key,
-                                          final String defaultValue) {
+    public static String getSettingString(final String key, final String defaultValue) {
         return PreferenceManager.getDefaultSharedPreferences(GlobalData.app()).getString(key, defaultValue);
     }
 
-    public static void setSettingString(final Context c, final String key, final String value) {
+    public static void setSettingString(final String key, final String value) {
         PreferenceManager.getDefaultSharedPreferences(GlobalData.app()).edit().putString(key, value).apply();
     }
 
-    public static void setSettingSet(final Context c, final String key, final Set<String> value) {
+    public static void setSettingSet(final String key, final Set<String> value) {
         PreferenceManager.getDefaultSharedPreferences(GlobalData.app()).edit().putStringSet(key, value).apply();
     }
 
-    public static Set<String> getSettingSet(final Context c, final String key, final Set<String> defaultValue) {
+    public static Set<String> getSettingSet(final String key, final Set<String> defaultValue) {
         return PreferenceManager.getDefaultSharedPreferences(GlobalData.app()).getStringSet(key, defaultValue);
     }
 
@@ -130,29 +72,27 @@ public abstract class PreferenceUtils {
         return sp.getBoolean(key, defaultValue);
     }
 
-    public static boolean getSettingBoolean(final Context c, final String key,
-                                            final boolean defaultValue) {
+    public static boolean getSettingBoolean(final String key, final boolean defaultValue) {
         return PreferenceManager.getDefaultSharedPreferences(GlobalData.app()).getBoolean(key, defaultValue);
     }
 
-    public static void setSettingBoolean(final Context c, final String key, final boolean value) {
+    public static void setSettingBoolean(final String key, final boolean value) {
         PreferenceManager.getDefaultSharedPreferences(GlobalData.app()).edit().putBoolean(key, value).apply();
     }
 
-    public static void setSettingInt(final Context c, final String key, final int value) {
+    public static void setSettingInt(final String key, final int value) {
         PreferenceManager.getDefaultSharedPreferences(GlobalData.app()).edit().putInt(key, value).apply();
     }
 
-    public static void clear(final Context c, final String key) {
+    public static void clear(final String key) {
         PreferenceManager.getDefaultSharedPreferences(GlobalData.app()).edit().remove(key).apply();
     }
 
-
-    public static void increaseSettingInt(final Context c, final String key) {
+    public static void increaseSettingInt(final String key) {
         increaseSettingInt(PreferenceManager.getDefaultSharedPreferences(GlobalData.app()), key);
     }
 
-    public static int getSettingInt(final Context c, final String key, final int defaultValue) {
+    public static int getSettingInt(final String key, final int defaultValue) {
         return PreferenceManager.getDefaultSharedPreferences(GlobalData.app()).getInt(key, defaultValue);
     }
 
@@ -164,19 +104,19 @@ public abstract class PreferenceUtils {
         return sp.getFloat(key, defaultValue);
     }
 
-    public static void setSettingFloat(final Context c, final String key, final float value) {
+    public static void setSettingFloat(final String key, final float value) {
         PreferenceManager.getDefaultSharedPreferences(GlobalData.app()).edit().putFloat(key, value).apply();
     }
 
-    public static float getSettingFloat(final Context c, final String key, final float defaultValue) {
+    public static float getSettingFloat(final String key, final float defaultValue) {
         return PreferenceManager.getDefaultSharedPreferences(GlobalData.app()).getFloat(key, defaultValue);
     }
 
-    public static void setSettingLong(final Context c, final String key, final long value) {
+    public static void setSettingLong(final String key, final long value) {
         PreferenceManager.getDefaultSharedPreferences(GlobalData.app()).edit().putLong(key, value).apply();
     }
 
-    public static long getSettingLong(final Context c, final String key, final long defaultValue) {
+    public static long getSettingLong(final String key, final long defaultValue) {
         return PreferenceManager.getDefaultSharedPreferences(GlobalData.app()).getLong(key, defaultValue);
     }
 
@@ -213,11 +153,11 @@ public abstract class PreferenceUtils {
         sp.edit().putLong(key, v).apply();
     }
 
-    public static boolean hasKey(final Context c, final String key) {
+    public static boolean hasKey(final String key) {
         return PreferenceManager.getDefaultSharedPreferences(GlobalData.app()).contains(key);
     }
 
-    public static void removePreference(final Context context, final String key) {
+    public static void removePreference(final String key) {
         PreferenceManager.getDefaultSharedPreferences(GlobalData.app()).edit().remove(key).apply();
     }
 
@@ -227,12 +167,12 @@ public abstract class PreferenceUtils {
         editor.apply();
     }
 
-    public static void dumpDefaultPreference(final Context context) {
+    public static void dumpDefaultPreference() {
         dumpPreference(PreferenceManager.getDefaultSharedPreferences(GlobalData.app()),
                 "default preference:");
     }
 
-    public static void dumpDefaultPreference(final Context context, final String preference) {
+    public static void dumpDefaultPreference(final String preference) {
         dumpPreference(GlobalData.app().getSharedPreferences(preference, Context.MODE_PRIVATE), preference);
     }
 
