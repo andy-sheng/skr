f951f05aec485e804db3aa9b88d6d885733cdb9d
chenyong1 <chenyong1@xiaomi.com>
2017-04-21 20:12:16 +0800

    更新rx
    Change-Id: Ia4af2a63e7c805868494bdea749cafbb1d5c04e3

---
+++ lite/src/main/java/com/mi/liveassistant/screenrecord/AudioRecordWorker.java
@@ -0,0 +1,20 @@
+package com.mi.liveassistant.screenrecord;
+
+import android.os.Message;
+
+import com.mi.milink.sdk.base.CustomHandlerThread;
+
+/**
+ * Created by chenyong on 2016/11/25.
+ */
+
+public class AudioRecordWorker extends CustomHandlerThread {
+    public AudioRecordWorker() {
+        super("AudioRecordWorker", android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
+    }
+
+    @Override
+    protected void processMessage(Message msg) {
+
+    }
+}
