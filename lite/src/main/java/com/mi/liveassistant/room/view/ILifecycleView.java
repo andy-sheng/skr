52f48958669f6724ad63d09394b35bd3ea32c522
xiaolan <wollt1992@163.com>
2017-04-25 09:42:35 +0800

    继承IView
    N/A
    
    Change-Id: I6f5eb6bd8d4e894ec3d9a3df223ebf608fc85f5f
    Signed-off-by: xiaolan <wollt1992@163.com>

--- lite/src/main/java/com/mi/liveassistant/room/view/ILifecycleView.java
+++ lite/src/main/java/com/mi/liveassistant/room/view/ILifecycleView.java
@@ -1,9 +1,11 @@
 package com.mi.liveassistant.room.view;
 
+import com.mi.liveassistant.common.mvp.IView;
+
 /**
  * Created by lan on 17/4/24.
  */
-public interface ILifecycleView {
+public interface ILifecycleView extends IView {
     void pause();
 
     void resume();
