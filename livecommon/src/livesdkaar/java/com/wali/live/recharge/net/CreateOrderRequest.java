2633dd160e2381b882b1ab1168dbba54ce4832a3
xiaolan <wollt1992@163.com>
2017-05-03 15:54:12 +0800

    删除BaseLiveRequest
    N/A
    
    Change-Id: I6c1f77b8518d0094ee1dbfccfc96360fbc5d9901
    Signed-off-by: xiaolan <wollt1992@163.com>

--- livecommon/src/livesdkaar/java/com/wali/live/recharge/net/CreateOrderRequest.java
+++ livecommon/src/livesdkaar/java/com/wali/live/recharge/net/CreateOrderRequest.java
@@ -5,7 +5,7 @@ import android.support.annotation.NonNull;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.mi.live.data.account.HostChannelManager;
 import com.mi.live.data.account.UserAccountManager;
-import com.mi.live.data.api.request.live.BaseLiveRequest;
+import com.mi.live.data.api.request.BaseRequest;
 import com.mi.live.data.milink.command.MiLinkCommand;
 import com.wali.live.pay.model.Diamond;
 import com.wali.live.proto.PayProto;
@@ -14,10 +14,9 @@ import com.wali.live.proto.PayProto;
  * Created by rongzhisheng on 16-12-31.
  */
 
-public class CreateOrderRequest extends BaseLiveRequest {
-
+public class CreateOrderRequest extends BaseRequest {
     public CreateOrderRequest(@NonNull Diamond goods, PayProto.PayType payType, PayProto.RChannel channel) {
-        super(MiLinkCommand.COMMAND_PAY_CREATE_ORDER,"createOrder",null);
+        super(MiLinkCommand.COMMAND_PAY_CREATE_ORDER, "createOrder", null);
         mRequest = PayProto.CreateOrderRequest.newBuilder()
                 .setUuid(UserAccountManager.getInstance().getUuidAsLong())
                 .setPlatform(PayProto.Platform.ANDROID)
