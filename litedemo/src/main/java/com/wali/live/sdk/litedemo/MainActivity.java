b2abbb5cf464a8e21c89e1000382dd25e75ebc5e
yangli <yangli1@xiaomi.com>
2017-05-09 20:44:50 +0800

    更新第三方登录Demo
    N/A
    
    Change-Id: Idd5669af6e4b99c7601af4e010a4b37630393333
    Signed-off-by: yangli <yangli1@xiaomi.com>

--- litedemo/src/main/java/com/wali/live/sdk/litedemo/MainActivity.java
+++ litedemo/src/main/java/com/wali/live/sdk/litedemo/MainActivity.java
@@ -77,6 +77,7 @@ public class MainActivity extends RxActivity implements View.OnClickListener, IC
                         public void run() {
                             mLoginBtn.setText("已登录");
                             mLoginBtn.setEnabled(false);
+                            mThirdPartyLoginBtn.setVisibility(View.GONE);
                         }
                     });
                 }
@@ -133,6 +134,7 @@ public class MainActivity extends RxActivity implements View.OnClickListener, IC
                             public void notifySuccess() {
                                 mLoginBtn.setText("已登录");
                                 mLoginBtn.setEnabled(false);
+                                mThirdPartyLoginBtn.setVisibility(View.GONE);
                             }
                         });
                     }
@@ -143,33 +145,14 @@ public class MainActivity extends RxActivity implements View.OnClickListener, IC
                 });
     }
 
-    private static final String RSA_PRIVATE_KEY = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAMC8ISWECSak6Z1X" +
-            "tgTy9jrq85dZ7Z95CndJ6Sz0ty5fiVqiJ4WrRf7d+78hlEOvlE0fwLQraHZ28gkD" +
-            "kdNX1ycFDV+SBDTn+rFnRJQZjA8t3cQGiJmpyFIpaSzpz9PMTScxDmmxygUzsTXe" +
-            "sCcFV8p9thCyJj5kGsUFxzkfwR7dAgMBAAECgYAqUCMmzVoE9eej94GqjHyqarKX" +
-            "49JbVIOLtNpQWFlvAOJy12691eBEGBAQ4hpe0clJNVNlOrJwb6SrffEh6QL+2Aht" +
-            "oocO7ST4kGpYTk53ofkK9AOwdZkhhzn226qRlDFN+OyAedLsv5sZ3166KTfxaCkO" +
-            "5/KeXuD9BucT4eHTMQJBAPUGugXFJBVUZimsqwi5PKBGtmqQEJAi5M0vZvGz4vtq" +
-            "H8pXQVYHOwAQA2Kmx7LSWqUa5EZCfKQIHE88dhmcru8CQQDJXd825pM0FW6ENr/9" +
-            "IZLZBMgOlFG06WkVa442trbViGP0TPJMeEzBHoCDtlxDUxKcbFworXvVk+f8SYUo" +
-            "6g7zAkAJSIb1vwFd+YOhYpRcUUBVxjgVE349J8VJbNlWoP0hj2TC8slb7Aw1NWYb" +
-            "b7wzLzsV9E3fx5cXU+NWsTC8Sa5rAkEAw8DL4/UWmQVUoJcQ4KUoumwZh4LMQ1C8" +
-            "5SPf5nSNHNwwPygmTAyOoRZj3KcE3jX9267DkI/F2ISmeu2F05Zl3QJAX8qggola" +
-            "wpkdbvZn81X80lFuye6b0KjSWqlrrQLtjSR9/ov/avbuEDI+Ni4rDZn5a0rkGuaN" +
-            "DzBZBemtWvPkjg==";
-
     private void clickThirdPartyLoginBtn() {
-        MyLog.w(TAG, "checkLogin isTouristMode=" + MiLinkClientAdapter.getsInstance().isTouristMode());
-        if (!MiLinkClientAdapter.getsInstance().isTouristMode()) {
-            return;
-        }
         String uid = "100067";
         String name = "游不动的鱼";
         String headUrl = "";
         int sex = 1;
         int channelId = 50001;
         String signStr = "channelId=" + channelId + "&headUrl=" + headUrl + "&nickname=" + name + "&sex=" + sex + "&xuid=" + uid;
-        String sign = RSASignature.sign(signStr, RSA_PRIVATE_KEY, "UTF-8");
+        String sign = RSASignature.sign(signStr, AccountManager.RSA_PRIVATE_KEY, "UTF-8");
 
         LoginManager.thirdPartLogin(channelId, uid, name, headUrl, sex, sign, new ILoginCallback() {
             @Override
@@ -183,6 +166,7 @@ public class MainActivity extends RxActivity implements View.OnClickListener, IC
                 MyLog.d(TAG, "notifySuccess");
                 mThirdPartyLoginBtn.setText("已登录");
                 mThirdPartyLoginBtn.setEnabled(false);
+                mLoginBtn.setVisibility(View.GONE);
             }
         });
     }
--- litedemo/src/main/java/com/wali/live/sdk/litedemo/account/AccountManager.java
+++ litedemo/src/main/java/com/wali/live/sdk/litedemo/account/AccountManager.java
@@ -19,6 +19,21 @@ public class AccountManager {
     public static final String REDIRECT_URL = "https://login.game.xiaomi.com/zhibo/login";
     public static final long APP_ID_LOGIN = 2882303761517438806L;
 
+    public static final String RSA_PRIVATE_KEY = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAMC8ISWECSak6Z1X" +
+            "tgTy9jrq85dZ7Z95CndJ6Sz0ty5fiVqiJ4WrRf7d+78hlEOvlE0fwLQraHZ28gkD" +
+            "kdNX1ycFDV+SBDTn+rFnRJQZjA8t3cQGiJmpyFIpaSzpz9PMTScxDmmxygUzsTXe" +
+            "sCcFV8p9thCyJj5kGsUFxzkfwR7dAgMBAAECgYAqUCMmzVoE9eej94GqjHyqarKX" +
+            "49JbVIOLtNpQWFlvAOJy12691eBEGBAQ4hpe0clJNVNlOrJwb6SrffEh6QL+2Aht" +
+            "oocO7ST4kGpYTk53ofkK9AOwdZkhhzn226qRlDFN+OyAedLsv5sZ3166KTfxaCkO" +
+            "5/KeXuD9BucT4eHTMQJBAPUGugXFJBVUZimsqwi5PKBGtmqQEJAi5M0vZvGz4vtq" +
+            "H8pXQVYHOwAQA2Kmx7LSWqUa5EZCfKQIHE88dhmcru8CQQDJXd825pM0FW6ENr/9" +
+            "IZLZBMgOlFG06WkVa442trbViGP0TPJMeEzBHoCDtlxDUxKcbFworXvVk+f8SYUo" +
+            "6g7zAkAJSIb1vwFd+YOhYpRcUUBVxjgVE349J8VJbNlWoP0hj2TC8slb7Aw1NWYb" +
+            "b7wzLzsV9E3fx5cXU+NWsTC8Sa5rAkEAw8DL4/UWmQVUoJcQ4KUoumwZh4LMQ1C8" +
+            "5SPf5nSNHNwwPygmTAyOoRZj3KcE3jX9267DkI/F2ISmeu2F05Zl3QJAX8qggola" +
+            "wpkdbvZn81X80lFuye6b0KjSWqlrrQLtjSR9/ov/avbuEDI+Ni4rDZn5a0rkGuaN" +
+            "DzBZBemtWvPkjg==";
+
     /**
      * 获取小米OAuth登录的code
      * 这个是一个耗时的操作，不能在UI线程调用
