3f7e46ed1e8d7423ef06e867365981aa54490944
yangli <yangli1@xiaomi.com>
2017-05-11 21:00:08 +0800

    更新Unity推流逻辑
    N/A
    
    Change-Id: I97603a8da88fd326d049a1af7266b0fa5d2d5060
    Signed-off-by: yangli <yangli1@xiaomi.com>

--- lite/src/main/java/com/mi/liveassistant/common/api/ErrorCode.java
+++ lite/src/main/java/com/mi/liveassistant/common/api/ErrorCode.java
@@ -8,6 +8,8 @@ public class ErrorCode {
     // 0：任何业务返回0，都代表成功
     public static final int CODE_SUCCESS = 0;
     public static final int CODE_ERROR_NORMAL = -1;
+    public static final int CODE_ERROR_MISSING_PROJECTION = -2;
+    public static final int CODE_ERROR_TOO_LOW_SDK = -3;
 
     public static final int CODE_ACCOUT_FORBIDDEN = 6021;
 }
