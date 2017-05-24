0dadda47dc85f22c9d9aca1d60b6f30b8dcf9434
zhangyuehuan <zhangyuehuan@xiaomi.com>
2017-05-24 17:40:59 +0800

    monkey需要
    N/A
    
    Change-Id: I71e14780655a2df46c78055b5d23f2d3871f52a6
    Signed-off-by: zhangyuehuan <zhangyuehuan@xiaomi.com>

--- livesdkapp/gen_build_settings.py
+++ livesdkapp/gen_build_settings.py
@@ -17,15 +17,16 @@ f = open('../common/src/main/java/com/base/utils/Constants.java', 'w')
 f.write(s)
 f.close()
 
-f = open('./src/main/AndroidManifest.xml', 'r')
-s = ''
-for line in f:
-    line = line.replace("<category android:name=\"android.intent.category.LAUNCHER\" />","")
-    s += line
-f.close()
-f = open('./src/main/AndroidManifest.xml', 'w')
-f.write(s)
-f.close()
+if (cmp(channlName,'DEBUG') != 0):
+    f = open('./src/main/AndroidManifest.xml', 'r')
+    s = ''
+    for line in f:
+        line = line.replace("<category android:name=\"android.intent.category.LAUNCHER\" />","")
+        s += line
+    f.close()
+    f = open('./src/main/AndroidManifest.xml', 'w')
+    f.write(s)
+    f.close()
 
 f = open('../buildsystem/dependencies.gradle', 'r')
 s = ''
