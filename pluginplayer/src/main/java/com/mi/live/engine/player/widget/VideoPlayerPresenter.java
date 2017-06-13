3ff8d6276ff14336cf41a9a331a3c7c00caaa597
xiaolan <wollt1992@163.com>
2017-06-13 15:11:51 +0800

    增加mute按钮
    N/A
    
    Change-Id: If5bc6c7e420a7fc9d0f94027ec27ea6ffe58dab9

--- pluginplayer/src/main/java/com/mi/live/engine/player/widget/VideoPlayerPresenter.java
+++ pluginplayer/src/main/java/com/mi/live/engine/player/widget/VideoPlayerPresenter.java
@@ -275,6 +275,14 @@ public class VideoPlayerPresenter implements IPlayerPresenter {
         }
     }
 
+    public void mute(boolean isMute) {
+        if (isMute) {
+            mPlayer.setVolume(0, 0);
+        } else {
+            mPlayer.setVolume(1, 1);
+        }
+    }
+
     private boolean stopOtherMusic() {
         // 关闭其他音乐
         if (null == mAudioManager) {
