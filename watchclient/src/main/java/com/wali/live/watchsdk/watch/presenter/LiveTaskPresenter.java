6a5c62c8ebc97c0775b81dcb353e3ae4789ecafc
zhangyuehuan <zhangyuehuan@xiaomi.com>
2017-06-26 09:44:23 +0800

    [push][push类型添加][弹幕消息]
    N/A
    
    Change-Id: I49f1c6f212031707cda69e1d74eb7e0718a8d7f6
    Signed-off-by: zhangyuehuan <zhangyuehuan@xiaomi.com>

--- watchclient/src/main/java/com/wali/live/watchsdk/watch/presenter/LiveTaskPresenter.java
+++ watchclient/src/main/java/com/wali/live/watchsdk/watch/presenter/LiveTaskPresenter.java
@@ -11,8 +11,7 @@ import com.base.utils.rx.RxRetryAssist;
 import com.base.utils.toast.ToastUtils;
 import com.mi.live.data.api.ErrorCode;
 import com.mi.live.data.cache.RoomInfoGlobalCache;
-import com.mi.live.data.data.LiveShow;
-import com.mi.live.data.manager.UserInfoManager;
+import com.mi.live.data.milink.MiLinkClientAdapter;
 import com.mi.live.data.push.SendBarrageManager;
 import com.mi.live.data.push.model.BarrageMsg;
 import com.mi.live.data.push.model.BarrageMsgType;
@@ -23,19 +22,13 @@ import com.mi.live.data.query.model.EnterRoomInfo;
 import com.mi.live.data.repository.RoomMessageRepository;
 import com.mi.live.data.repository.datasource.RoomMessageStore;
 import com.mi.live.data.room.model.RoomBaseDataModel;
-import com.trello.rxlifecycle.ActivityEvent;
 import com.wali.live.event.EventClass;
 import com.wali.live.watchsdk.R;
-import com.wali.live.watchsdk.watch.event.LiveEndEvent;
 
 import org.greenrobot.eventbus.EventBus;
 
-import rx.Observable;
 import rx.Observer;
 import rx.Subscription;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action1;
-import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 /**
@@ -51,7 +44,9 @@ public class LiveTaskPresenter implements ILiveTaskPresenter, IBindActivityLIfeC
 
     private RoomMessagePresenter mPullRoomMessagePresenter;
 
-    public LiveTaskPresenter(@NonNull RxActivity rxActivity, IWatchView view, @NonNull RoomBaseDataModel myRoomData) {
+    public LiveTaskPresenter(@NonNull RxActivity rxActivity,
+                             IWatchView view,
+                             @NonNull RoomBaseDataModel myRoomData) {
         mRxActivity = rxActivity;
         mView = view;
         mMyRoomData = myRoomData;
@@ -60,50 +55,6 @@ public class LiveTaskPresenter implements ILiveTaskPresenter, IBindActivityLIfeC
     //这里限制进入房间只会被调用一次
     boolean mHasEnter = false;
     Subscription mEnterRoomSubscription;
-
-    private void pullRoomMessage() {
-        // 对外的不收push了，统一走拉取模式
-        if (mPullRoomMessagePresenter == null) {
-            mPullRoomMessagePresenter = new RoomMessagePresenter(mMyRoomData, new RoomMessageRepository(new RoomMessageStore()), mRxActivity);
-        }
-        mPullRoomMessagePresenter.startWork();
-    }
-
-
-    public void queryLiveShowById(final long uuid, final String liveId) {
-        if (uuid <= 0 || TextUtils.isEmpty(liveId)) {
-            MyLog.d(TAG, "queryLiveShowById uuid=" + uuid + ", liveId=" + liveId);
-            // 直播结束
-            EventBus.getDefault().post(new LiveEndEvent());
-            return;
-        }
-        Observable.just(uuid)
-                .map(new Func1<Long, LiveShow>() {
-                    @Override
-                    public LiveShow call(Long userId) {
-                        LiveShow liveShow = UserInfoManager.getLiveShowByUserId(userId);
-                        return liveShow;
-                    }
-                }).subscribeOn(Schedulers.io())
-                .compose(mRxActivity.<LiveShow>bindUntilEvent(ActivityEvent.DESTROY))
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Action1<LiveShow>() {
-                    @Override
-                    public void call(LiveShow liveShow) {
-                        if (liveShow == null || liveShow.getUid() != uuid || !liveId.equals(liveShow.getLiveId())) {
-                            MyLog.d(TAG, "queryLiveShowById uuid=" + uuid + " liveId=" + liveId +
-                                    ", liveShow uid=" + liveShow.getUid() + " liveId=" + liveShow.getLiveId());
-                            // 直播结束
-                            EventBus.getDefault().post(new LiveEndEvent());
-                        }
-                    }
-                }, new Action1<Throwable>() {
-                    @Override
-                    public void call(Throwable throwable) {
-                        MyLog.w(TAG, throwable);
-                    }
-                });
-    }
 
     @Override
     public void enterLive() {
@@ -137,20 +88,23 @@ public class LiveTaskPresenter implements ILiveTaskPresenter, IBindActivityLIfeC
                     public void onNext(EnterRoomInfo enterRoomInfo) {
                         MyLog.w(TAG, "enterRoomInfo code: " + enterRoomInfo.getRetCode());
                         RoomDataMapper.fillRoomDataModelByEnterRoomInfo(mMyRoomData, enterRoomInfo);
-                        pullRoomMessage();
-//                        if (mMyRoomData.getGetMessageMode() == RoomMessagePresenter.PULL_MODE) {
-//                            if (mPullRoomMessagePresenter == null) {
-//                                mPullRoomMessagePresenter = new RoomMessagePresenter(mMyRoomData, new RoomMessageRepository(new RoomMessageStore()), mRxActivity);
-//                            }
-//                            mPullRoomMessagePresenter.startWork();
-//                        } else {
-//                            if (mPullRoomMessagePresenter != null) {
-//                                mPullRoomMessagePresenter.stopWork();
-//                            }
-//                        }
+                        //注：陈老师，让加回来push 17.06.23
+                        if (mMyRoomData.getGetMessageMode() == RoomMessagePresenter.PULL_MODE
+                                || MiLinkClientAdapter.getsInstance().isTouristMode()) {
+                            if (mPullRoomMessagePresenter == null) {
+                                mPullRoomMessagePresenter = new RoomMessagePresenter(mMyRoomData,
+                                        new RoomMessageRepository(new RoomMessageStore()), mRxActivity);
+                            }
+                            mPullRoomMessagePresenter.startWork();
+                        } else {
+                            if (mPullRoomMessagePresenter != null) {
+                                mPullRoomMessagePresenter.stopWork();
+                            }
+                        }
                         if (mView != null && enterRoomInfo.getRetCode() == ErrorCode.CODE_SUCCESS) {
                             mView.enterLive(enterRoomInfo);
-                        } else if (mView != null && enterRoomInfo.getRetCode() == ErrorCode.CODE_SERVER_RESPONSE_ERROR_CODE_NO_PERMISSION_TO_ENTER_ROOM) {
+                        } else if (mView != null && enterRoomInfo.getRetCode() ==
+                                ErrorCode.CODE_SERVER_RESPONSE_ERROR_CODE_NO_PERMISSION_TO_ENTER_ROOM) {
                             MyLog.w(TAG, "CODE_SERVER_RESPONSE_ERROR_CODE_NO_PERMISSION_TO_ENTER_ROOM");
                             EventBus.getDefault().post(new EventClass.KickEvent());
                         } else {
@@ -159,7 +113,9 @@ public class LiveTaskPresenter implements ILiveTaskPresenter, IBindActivityLIfeC
                             if (enterRoomInfo.getRetCode() == ErrorCode.CODE_PARAM_ERROR) {
                                 ToastUtils.showToast(R.string.token_live_error_toast_room_not_exist);
                             }
-                            BarrageMsg msg = SendBarrageManager.createBarrage(BarrageMsgType.B_MSG_TYPE_LIVE_END, GlobalData.app().getString(R.string.live_finish), mMyRoomData.getRoomId(), mMyRoomData.getUid(), System.currentTimeMillis(), ext);
+                            BarrageMsg msg = SendBarrageManager.createBarrage(BarrageMsgType.B_MSG_TYPE_LIVE_END,
+                                    GlobalData.app().getString(R.string.live_finish), mMyRoomData.getRoomId(),
+                                    mMyRoomData.getUid(), System.currentTimeMillis(), ext);
                             SendBarrageManager.pretendPushBarrage(msg);
                         }
                         mHasEnter = true;
@@ -170,7 +126,8 @@ public class LiveTaskPresenter implements ILiveTaskPresenter, IBindActivityLIfeC
     @Override
     public void leaveLive() {
         RoomInfoGlobalCache.getsInstance().leaveCurrentRoom(mMyRoomData.getRoomId());
-        LiveRoomQuery.leaveRoom(mMyRoomData.getUid(), mMyRoomData.getRoomId(), mMyRoomData.getGetMessageMode())
+        LiveRoomQuery.leaveRoom(mMyRoomData.getUid(), mMyRoomData.getRoomId(),
+                mMyRoomData.getGetMessageMode())
 //                .compose(mRxActivity.<Boolean>bindUntilEvent())
                 .subscribeOn(Schedulers.io())
                 .subscribe(new Observer<Boolean>() {
