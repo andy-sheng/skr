c9073780e021b7ef576c4f84f3b7abebfeddc5f5
xiaolan <wollt1992@163.com>
2017-06-29 14:01:57 +0800

    tab活跃增加milink打点
    N/A
    
    Change-Id: I49056fb53134e0bd4917faaccf4d94615d98e647
    Signed-off-by: xiaolan <wollt1992@163.com>

---
+++ watchclient/src/main/java/com/wali/live/watchsdk/statistics/MilinkStatistics.java
@@ -0,0 +1,79 @@
+package com.wali.live.watchsdk.statistics;
+
+import com.base.log.MyLog;
+import com.mi.live.data.api.ErrorCode;
+import com.wali.live.proto.StatisticsProto;
+import com.wali.live.watchsdk.statistics.item.LiveHelperItem;
+import com.wali.live.watchsdk.statistics.item.MilinkStatisticsItem;
+import com.wali.live.watchsdk.statistics.request.LiveRecvRequest;
+
+import org.json.JSONException;
+
+import rx.Observable;
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+
+/**
+ * Created by lan on 2017/6/28.
+ */
+public class MilinkStatistics {
+    private static final String TAG = MilinkStatistics.class.getSimpleName();
+
+    private static MilinkStatistics sInstance = null;
+
+    private MilinkStatistics() {
+    }
+
+    public synchronized static MilinkStatistics getInstance() {
+        if (sInstance == null) {
+            sInstance = new MilinkStatistics();
+        }
+        return sInstance;
+    }
+
+    public void statisticsGameActive(int channelId, String key) {
+        long time = System.currentTimeMillis();
+        try {
+            MilinkStatisticsItem item = new LiveHelperItem(time, LiveHelperItem.GAME_ACTIVE_TYPE, channelId, key);
+            statisticsInternal(item);
+        } catch (JSONException e) {
+            MyLog.e(TAG, e);
+        }
+    }
+
+    private void statisticsInternal(final MilinkStatisticsItem item) {
+        Observable.just(0)
+                .map(new Func1<Object, Integer>() {
+                    @Override
+                    public Integer call(Object o) {
+                        StatisticsProto.LiveRecvFlagRsp rsp = new LiveRecvRequest(item).syncRsp();
+                        if (rsp != null) {
+                            return rsp.getRetCode();
+                        }
+                        return ErrorCode.CODE_ERROR_NORMAL;
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onCompleted() {
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        MyLog.e(TAG, "statistics failure", e);
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        MyLog.w(TAG, "statistics onNext integer=" + integer);
+                        if (integer != null && integer == 0) {
+                            return;
+                        }
+                    }
+                });
+    }
+}
