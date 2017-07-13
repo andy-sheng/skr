adbe5cf75389c034d450bb61297edbfc815d6832
zhangyuehuan <zhangyuehuan@xiaomi.com>
2017-07-13 10:17:43 +0800

    [task][添加游戏直播动态引导][直播间]
    N/A
    
    Change-Id: If8ec3e9a8bbd1246ef4733be187c0ce0f7533270
    Signed-off-by: zhangyuehuan <zhangyuehuan@xiaomi.com>

--- livecommon/src/main/java/com/wali/live/component/ComponentController.java
+++ livecommon/src/main/java/com/wali/live/component/ComponentController.java
@@ -71,6 +71,9 @@ public abstract class ComponentController implements ComponentPresenter.ICompone
     public static final int MSG_SHOW_GAME_DOWNLOAD = MSG_POPUP_FIRST + 7;       // 展示 游戏中心下载框
     public static final int MSG_SHOW_SHARE_PANEL = MSG_POPUP_FIRST + 8;         // 显示 分享面板
     public static final int MSG_SHOW_PERSONAL_INFO = MSG_POPUP_FIRST + 9;       // 显示 个人信息页
+    public static final int MSG_SHOW_FOLLOW_GUIDE = MSG_POPUP_FIRST + 10;       // 显示 游戏引导页面
+    public static final int MSG_FOLLOW_COUNT_DOWN = MSG_POPUP_FIRST + 11;       // 显示 游戏引导页面之前的倒计时
+
 
     // 详情播放相关
     private static final int MSG_DETAIL_VIDEO_FIRST = 40000;
