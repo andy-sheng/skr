522bcda2f1b37bee17fd53bcdd8fd5f9261bcc16
zhangyuehuan <zhangyuehuan@xiaomi.com>
2017-09-27 11:44:02 +0800

    [bugfix][monkeyNumberFormatException][bugfix]
    N/A
    
    Change-Id: I77027c95c8ed8ed74090c37bd528bef9ba59667e
    Signed-off-by: zhangyuehuan <zhangyuehuan@xiaomi.com>

--- plugindemo/src/main/java/com/wali/live/sdk/manager/demo/MenuRecyclerAdapter.java
+++ plugindemo/src/main/java/com/wali/live/sdk/manager/demo/MenuRecyclerAdapter.java
@@ -377,10 +377,19 @@ public class MenuRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
             }
         }));
 
-        mDataList.add(new Bean("频道：关注(小米视频101015)", new Runnable() {
+        mDataList.add(new Bean("频道：关注(小米视频18)", new Runnable() {
             @Override
             public void run() {
                 String uri = "livesdk://channel?channel=50014&package_name=com.miui.video&title=%e5%85%b3%e6%b3%a8&channel_id=18";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                mActivity.startActivity(intent);
+            }
+        }));
+
+        mDataList.add(new Bean("频道：新世界小姐(小米视频22)", new Runnable() {
+            @Override
+            public void run() {
+                String uri = "livesdk://channel?channel=50014&package_name=com.miui.video&title=%e5%85%b3%e6%b3%a8&channel_id=22";
                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
                 mActivity.startActivity(intent);
             }
