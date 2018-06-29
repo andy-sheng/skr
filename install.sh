b2fd34185c11386721c3f17ee73dece60f1a0fa5
chengsimin <chengsimin@xiaomi.com>
2018-06-29 10:12:13 +0800

    打包脚本更新
    N.A
    
    Change-Id: I29f2e314eb6e3f335394d4b90d0c970d2fc3b7e1
    Signed-off-by: chengsimin <chengsimin@xiaomi.com>

--- install.sh
+++ install.sh
@@ -2,13 +2,23 @@ if [[ $1 == "debug" ]]; then
 	./gradlew :livesdkapp:assembleDebug
 	adb install -r  livesdkapp/build/outputs/apk/livesdkapp-debug.apk
 else
+		
+	if [[ $1 == "0" ]]; then
+		CHANNEL_ID="DEFAULT"
+	elif [[ $1 == "1" ]]; then
+		# 应用商店 meng_1254_48_android
+		CHANNEL_ID="meng_1254_48_android"
+	else
+		echo "错误的参数，0 DEFAULT | 1 meng_1254_48_android"
+		exit
+	fi
+
 	cd livesdkapp
-	# 应用商店 meng_1254_48_android
-	./ship_to.sh $1
+	./ship_to.sh $CHANNEL_ID
 	#./gradlew :livesdkapp:assembleRelease
 	cd ..
 	date1=`date +%m_%d_%H_%M`
-	cp livesdkapp/build/outputs/apk/livesdkapp-release.apk ~/Downloads/livesdkapp-$1-$date1.apk
+	cp livesdkapp/build/outputs/apk/livesdkapp-release.apk ~/Downloads/livesdkapp-$CHANNEL_ID-$date1.apk
 	adb install -r  livesdkapp/build/outputs/apk/livesdkapp-release.apk
 fi
 
