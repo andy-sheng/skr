6f044140b3860c8700dd18632129d4db666b19fc
liuting <liuting@xiaomi.com>
2018-08-06 10:44:47 +0800

    [打点][修改活跃打点计时方式][活跃打点]
    N/A
    
    Change-Id: I6909dc4dd2469008a2b0167f93aaaafd8ec693bd
    Signed-off-by: liuting <liuting@xiaomi.com>

--- livesdkapp/src/main/java/com/wali/live/init/LiveSdkApplication.java
+++ livesdkapp/src/main/java/com/wali/live/init/LiveSdkApplication.java
@@ -8,11 +8,13 @@ import android.support.multidex.MultiDex;
 
 import com.base.log.MyLog;
 import com.base.utils.Constants;
+import com.mi.live.data.account.MyUserInfoManager;
 import com.mi.liveassistant.BuildConfig;
 import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
 import com.wali.live.watchsdk.init.InitManager;
-import com.wali.live.watchsdk.statistics.AliveRegularUploadTask;
+import com.wali.live.watchsdk.statistics.MilinkStatistics;
+import com.wali.live.watchsdk.statistics.item.AliveStatisticItem;
 
 /**
  * Created by lan on 16/7/20.
@@ -23,8 +25,6 @@ public class LiveSdkApplication extends Application {
     private static final String TAG = LiveSdkApplication.class.getSimpleName();
 
     private static RefWatcher sRefWatcher;
-
-    private int mActivityForegrounCount; //onStart +1 onStop -1 0为应用退到后台
 
     @Override
     public void onCreate() {
@@ -56,6 +56,10 @@ public class LiveSdkApplication extends Application {
 
     private void registerActivityLifecycle() {
         registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+
+            private int mActivityForegrounCount; //onStart +1 onStop -1 0为应用退到后台
+            private long mStartTime = 0;
+
             @Override
             public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
 
@@ -65,7 +69,8 @@ public class LiveSdkApplication extends Application {
             public void onActivityStarted(Activity activity) {
                 if (mActivityForegrounCount == 0){
                     // 第一次出现在前台　开始活跃打点记时
-                    AliveRegularUploadTask.sInstance.startUpload();
+                    mStartTime = System.currentTimeMillis();
+                    MyLog.d(TAG, "start active time " + mStartTime);
                 }
                 mActivityForegrounCount++;
             }
@@ -84,8 +89,16 @@ public class LiveSdkApplication extends Application {
             public void onActivityStopped(Activity activity) {
                 mActivityForegrounCount--;
                 if (mActivityForegrounCount == 0){
-                   // 停止活跃打点记时
-                    AliveRegularUploadTask.sInstance.stopUpload();
+                   // 停止活跃打点记时 上报
+                    if (mStartTime != 0) {
+                        long activeTime = System.currentTimeMillis() - mStartTime;
+                        if (activeTime > 0) {
+                            MyLog.d(TAG, "active time " + activeTime);
+                            MilinkStatistics.getInstance().statisticAlive(MyUserInfoManager.getInstance().getUuid(),
+                                    activeTime, AliveStatisticItem.ALIVE_BIZ_TYPE_ALL);
+                        }
+                        mStartTime = 0;
+                    }
                 }
             }
 
