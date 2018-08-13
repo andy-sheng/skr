024041d8fc0a96eb543b6c01e13e9f4ead817b9c
jianglong5 <jianglong5@xiaomi.com>
2018-08-13 17:17:35 +0800

    [游戏观看聊天界面顶部主播信息][code][code]
    N/A
    
    Change-Id: Iecb3ebe4156a091533899e1200112417884998e2
    Signed-off-by: jianglong5 <jianglong5@xiaomi.com>

--- watchclient/src/main/java/com/wali/live/watchsdk/watch/presenter/watchgamepresenter/WatchGameChatTabPresenter.java
+++ watchclient/src/main/java/com/wali/live/watchsdk/watch/presenter/watchgamepresenter/WatchGameChatTabPresenter.java
@@ -1,16 +1,34 @@
 package com.wali.live.watchsdk.watch.presenter.watchgamepresenter;
 
 import android.support.annotation.CallSuper;
+import android.support.annotation.NonNull;
 
+import com.base.log.MyLog;
+import com.mi.live.data.account.UserAccountManager;
+import com.mi.live.data.api.relation.RelationApi;
+import com.mi.live.data.event.FollowOrUnfollowEvent;
+import com.mi.live.data.room.model.RoomBaseDataModel;
 import com.mi.live.data.room.model.RoomDataChangeEvent;
+import com.mi.live.data.user.User;
 import com.thornbirds.component.IEventController;
 import com.thornbirds.component.IParams;
 import com.wali.live.component.presenter.BaseSdkRxPresenter;
+import com.wali.live.dao.RelationDaoAdapter;
+import com.wali.live.event.UserActionEvent;
+import com.wali.live.proto.RelationProto;
+import com.wali.live.watchsdk.auth.AccountAuthManager;
 import com.wali.live.watchsdk.watch.view.watchgameview.WatchGameChatTabView;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
+
+import rx.Observable;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by vera on 2018/8/8.
@@ -20,8 +38,21 @@ public class WatchGameChatTabPresenter extends BaseSdkRxPresenter<WatchGameChatT
         implements WatchGameChatTabView.IPresenter {
     private static final String TAG = "WatchGameBottomEditPresenter";
 
-    public WatchGameChatTabPresenter(IEventController controller) {
+    private RoomBaseDataModel mMyRoomData;
+    private Subscription mFollowSubscription;
+
+    public WatchGameChatTabPresenter(@NonNull IEventController controller, @NonNull RoomBaseDataModel roomData) {
         super(controller);
+        mMyRoomData = roomData;
+    }
+
+    @Override
+    public void syncData() {
+        MyLog.d(TAG, "syncData");
+        mView.updateAnchorInfo(mMyRoomData.getUid(), mMyRoomData.getAvatarTs(),
+                mMyRoomData.getCertificationType(), mMyRoomData.getLevel(), mMyRoomData.getNickName());
+        mView.updateViewerNum(mMyRoomData.getViewerCnt());
+        mView.showFollowBtn(mMyRoomData.isEnableRelationChain(), mMyRoomData.isFocused());
     }
 
     @Override
@@ -55,6 +86,7 @@ public class WatchGameChatTabPresenter extends BaseSdkRxPresenter<WatchGameChatT
     public void onEventMainThread(RoomDataChangeEvent event) {
         switch (event.type) {
             case RoomDataChangeEvent.TYPE_CHANGE_USER_INFO_COMPLETE: {
+                syncData();
             }
             break;
             case RoomDataChangeEvent.TYPE_CHANGE_TICKET: {
@@ -74,4 +106,70 @@ public class WatchGameChatTabPresenter extends BaseSdkRxPresenter<WatchGameChatT
         }
     }
 
+    @Override
+    public void getAnchorInfo() {
+        UserActionEvent.post(UserActionEvent.EVENT_TYPE_REQUEST_LOOK_USER_INFO, mMyRoomData.getUid(), null);
+    }
+
+    @Override
+    public void followAnchor() {
+        MyLog.d(TAG, "follow anchor");
+        if (mFollowSubscription != null &&
+                !mFollowSubscription.isUnsubscribed() ||
+                !AccountAuthManager.triggerActionNeedAccount(mView.getRealView().getContext())) {
+            return;
+        }
+        mFollowSubscription = RelationApi.follow(UserAccountManager.getInstance().getUuidAsLong(),
+                mMyRoomData.getUid(), mMyRoomData.getRoomId())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<RelationProto.FollowResponse>() {
+                    @Override
+                    public void call(RelationProto.FollowResponse followResponse) {
+                        MyLog.d(TAG, "followResultCode = " + followResponse.getCode());
+                        mView.onFollowResult(followResponse.getCode());
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+                        mView.onFollowResult(-1);
+                    }
+                });
+
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onEventMainThread(FollowOrUnfollowEvent event) {
+        if (null != event && mMyRoomData != null && mMyRoomData.getUser() != null && mMyRoomData.getUser().getUid() == event.uuid) {
+            final User user = mMyRoomData.getUser();
+            if (user != null && user.getUid() == event.uuid) {
+                boolean needUpdateDb = false;
+
+                if (event.eventType == FollowOrUnfollowEvent.EVENT_TYPE_FOLLOW) {
+                    user.setIsFocused(true);
+                    mView.showFollowBtn(mMyRoomData.isEnableRelationChain(), true);
+                    needUpdateDb = true;
+                } else if (event.eventType == FollowOrUnfollowEvent.EVENT_TYPE_UNFOLLOW) {
+                    user.setIsFocused(false);
+                    mView.showFollowBtn(mMyRoomData.isEnableRelationChain(), false);
+                    needUpdateDb = true;
+                } else {
+                    MyLog.e(TAG, "type error");
+                }
+                MyLog.d(TAG, "needUpdateDb=" + needUpdateDb);
+                if (needUpdateDb) {
+                    // 其后台线程
+                    Observable.just(null)
+                            .map(new Func1<Object, Object>() {
+                                @Override
+                                public Object call(Object o) {
+                                    return RelationDaoAdapter.getInstance().insertRelation(user.getRelation());
+                                }
+                            })
+                            .subscribeOn(Schedulers.io())
+                            .subscribe();
+                }
+            }
+        }
+    }
 }
--- watchclient/src/main/java/com/wali/live/watchsdk/watch/view/watchgameview/WatchGameChatTabView.java
+++ watchclient/src/main/java/com/wali/live/watchsdk/watch/view/watchgameview/WatchGameChatTabView.java
@@ -2,21 +2,37 @@ package com.wali.live.watchsdk.watch.view.watchgameview;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.base.image.fresco.BaseImageView;
+import com.base.log.MyLog;
+import com.base.utils.CommonUtils;
+import com.base.utils.display.DisplayUtils;
+import com.base.utils.toast.ToastUtils;
+import com.mi.live.data.api.ErrorCode;
 import com.thornbirds.component.view.IComponentView;
 import com.thornbirds.component.view.IViewProxy;
+import com.wali.live.utils.AvatarUtils;
+import com.wali.live.utils.ItemDataFormatUtils;
 import com.wali.live.watchsdk.R;
 import com.wali.live.watchsdk.component.WatchComponentController;
 import com.wali.live.watchsdk.component.presenter.LiveCommentPresenter;
 import com.wali.live.watchsdk.watch.presenter.watchgamepresenter.WatchGameChatTabPresenter;
 import com.wali.live.watchsdk.watch.presenter.watchgamepresenter.WatchGameLiveCommentPresenter;
 
+import static com.wali.live.component.view.Utils.$click;
+
 public class WatchGameChatTabView extends RelativeLayout implements
         IComponentView<WatchGameChatTabView.IPresenter, WatchGameChatTabView.IView>, View.OnClickListener {
+
+    private static final int ANCHOR_BADGE_CERT = DisplayUtils.dip2px(16f);
+    private static final int ANCHOR_BADGE_UN_CERT = DisplayUtils.dip2px(11f);
+
+    private static final String TAG = "WatchGameChatTabView";
 
     WatchGameChatTabPresenter mWatchGameChatTabPresenter;
 
@@ -27,11 +43,14 @@ public class WatchGameChatTabView extends RelativeLayout implements
     RelativeLayout mAnchorInfoContainer;
     BaseImageView mAnchorAvatarIv;
     TextView mAnchorNameTv;
+    ImageView mUserBadgeIv;
     TextView mAnchorRoomTv;
     TextView mFocusBtn;
     TextView mViewerNum;
     RelativeLayout mCommentContainer;
     WatchGameLiveCommentView mLiveCommentView;
+
+    private boolean mEnableFollow = true;
 
     public WatchGameChatTabView(Context context, WatchComponentController componentController) {
         super(context);
@@ -40,19 +59,9 @@ public class WatchGameChatTabView extends RelativeLayout implements
 
     private void init(Context context, WatchComponentController componentController) {
         inflate(context, R.layout.watch_game_tab_chat_layout, this);
-        mWatchGameLiveCommentView = (WatchGameLiveCommentView) this.findViewById(R.id.live_comment_view);
-        mWatchGameLiveCommentView.setToken(componentController.mRoomChatMsgManager.toString());
-        mWatchGameLiveCommentPresenter = new WatchGameLiveCommentPresenter(componentController);
-        mWatchGameLiveCommentPresenter.setView(mWatchGameLiveCommentView.getViewProxy());
-        mWatchGameLiveCommentView.setPresenter(mWatchGameLiveCommentPresenter);
-
-        mWatchGameChatTabPresenter = new WatchGameChatTabPresenter(componentController);
-        mWatchGameChatTabPresenter.setView(this.getViewProxy());
-        this.setPresenter(mWatchGameChatTabPresenter);
-
-
         mAnchorInfoContainer = (RelativeLayout) this.findViewById(R.id.anchor_info_container);
         mAnchorAvatarIv = (BaseImageView) this.findViewById(R.id.anchor_avatar_iv);
+        mUserBadgeIv = (ImageView) this.findViewById(R.id.anchor_badge_iv);
         mAnchorNameTv = (TextView) this.findViewById(R.id.anchor_name_tv);
         mAnchorRoomTv = (TextView) this.findViewById(R.id.anchor_room_tv);
         mFocusBtn = (TextView) this.findViewById(R.id.focus_btn);
@@ -60,6 +69,18 @@ public class WatchGameChatTabView extends RelativeLayout implements
         mCommentContainer = (RelativeLayout) this.findViewById(R.id.comment_container);
         mLiveCommentView = (WatchGameLiveCommentView) this.findViewById(R.id.live_comment_view);
 
+        mWatchGameLiveCommentView = (WatchGameLiveCommentView) this.findViewById(R.id.live_comment_view);
+        mWatchGameLiveCommentView.setToken(componentController.mRoomChatMsgManager.toString());
+        mWatchGameLiveCommentPresenter = new WatchGameLiveCommentPresenter(componentController);
+        mWatchGameLiveCommentPresenter.setView(mWatchGameLiveCommentView.getViewProxy());
+        mWatchGameLiveCommentView.setPresenter(mWatchGameLiveCommentPresenter);
+
+        mWatchGameChatTabPresenter = new WatchGameChatTabPresenter(componentController, componentController.getRoomBaseDataModel());
+        mWatchGameChatTabPresenter.setView(this.getViewProxy());
+        this.setPresenter(mWatchGameChatTabPresenter);
+
+        $click(mAnchorAvatarIv, this);
+        $click(mFocusBtn, this);
     }
 
     @Override
@@ -86,7 +107,16 @@ public class WatchGameChatTabView extends RelativeLayout implements
 
     @Override
     public void onClick(View v) {
-
+        if (mWatchGameChatTabPresenter == null) {
+            MyLog.w(TAG, "onClick, mPresenter is null！");
+            return;
+        }
+        int id = v.getId();
+        if (id == R.id.anchor_avatar_iv) {
+            mWatchGameChatTabPresenter.getAnchorInfo();
+        } else if (id == R.id.focus_btn) {
+            mWatchGameChatTabPresenter.followAnchor();
+        }
     }
 
     @Override
@@ -94,8 +124,43 @@ public class WatchGameChatTabView extends RelativeLayout implements
         return new IView() {
 
             @Override
+            public void showFollowBtn(boolean needShow, boolean isFollow) {
+                mEnableFollow = needShow;
+                if (needShow) {
+                    mFocusBtn.setVisibility(VISIBLE);
+                    if (isFollow) {
+                        mFocusBtn.setText(R.string.followed);
+                        mFocusBtn.setEnabled(false);
+                    } else {
+                        mFocusBtn.setText(R.string.follow);
+                        mFocusBtn.setEnabled(true);
+                    }
+                } else {
+                    mFocusBtn.setVisibility(INVISIBLE);
+                }
+
+            }
+
+            @Override
+            public void onFollowResult(int resultCode) {
+                if (mEnableFollow) {
+                    if (resultCode == ErrorCode.CODE_RELATION_BLACK) {
+                        ToastUtils.showToast(getResources().getString(R.string.setting_black_follow_hint));
+                    } else if (resultCode == 0) {
+                        ToastUtils.showToast(getResources().getString(R.string.follow_success));
+                        mFocusBtn.setText(R.string.followed);
+                        mFocusBtn.setEnabled(false);
+                    } else if (resultCode == -1) {
+                        ToastUtils.showToast(getResources().getString(R.string.follow_failed));
+                    } else {
+                        ToastUtils.showToast("关注失败 code:" + resultCode);
+                    }
+                }
+            }
+
+
+            @Override
             public void updateViewerNum(int viewerCnt) {
-                viewerCnt = 11222;
                 String info;
                 if (viewerCnt > 9999) {
                     info = String.format("%.1f万人", viewerCnt / 10000.0);
@@ -106,6 +171,34 @@ public class WatchGameChatTabView extends RelativeLayout implements
             }
 
             @Override
+            public void updateAnchorInfo(long uid, long avatarTs, int certificationType, int level, String nickName) {
+                AvatarUtils.loadAvatarByUidTs(mAnchorAvatarIv, uid, avatarTs, true);
+                mUserBadgeIv.setVisibility(View.VISIBLE);
+                LayoutParams badgeLp = (LayoutParams) mUserBadgeIv.getLayoutParams();
+                if (certificationType > 0) {
+                    badgeLp.width = ANCHOR_BADGE_CERT;
+                    badgeLp.height = ANCHOR_BADGE_CERT;
+                    mUserBadgeIv.setImageDrawable(ItemDataFormatUtils.getCertificationImgSource(certificationType));
+                } else {
+                    badgeLp.width = ANCHOR_BADGE_UN_CERT;
+                    badgeLp.height = ANCHOR_BADGE_UN_CERT;
+                    mUserBadgeIv.setImageDrawable(ItemDataFormatUtils.getLevelSmallImgSource(level));
+                }
+                mUserBadgeIv.setLayoutParams(badgeLp);
+                if (!TextUtils.isEmpty(nickName)) {
+                    CommonUtils.setMaxEclipse(mAnchorNameTv, DisplayUtils.dip2px(75), nickName);
+                } else if (uid > 0) {
+                    CommonUtils.setMaxEclipse(mAnchorNameTv, DisplayUtils.dip2px(75), String.valueOf(uid));
+                } else {
+                    mAnchorNameTv.setText(R.string.watch_owner_name_default);
+                }
+
+                mAnchorRoomTv.setText(String.format(getResources().getString(R.string.anchor_room_id), String.valueOf(uid)));
+
+            }
+
+
+            @Override
             public <T extends View> T getRealView() {
                 return (T) WatchGameChatTabView.this;
             }
@@ -114,13 +207,47 @@ public class WatchGameChatTabView extends RelativeLayout implements
 
     @Override
     public void setPresenter(IPresenter iPresenter) {
-
+        iPresenter.syncData();
     }
 
     public interface IView extends IViewProxy {
+        /**
+         * 初始化关注
+         */
+        void showFollowBtn(boolean needShow, boolean isFollow);
+
+        /**
+         * 关注主播结果
+         */
+        void onFollowResult(int resultCode);
+
+        /**
+         * 更新观众人数
+         */
         void updateViewerNum(int viewerCnt);
+
+        /**
+         * 更新主播信息
+         */
+        void updateAnchorInfo(long uid, long avatarTs, int certificationType, int level, String nickName);
     }
 
     public interface IPresenter {
+
+        /**
+         * 打开主播信息框
+         */
+        void getAnchorInfo();
+
+        /**
+         * 关注主播
+         */
+        void followAnchor();
+
+        /**
+         * 更新数据
+         */
+        void syncData();
+
     }
 }
