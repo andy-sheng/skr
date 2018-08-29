fdab0a498bbc4ca128f9a799ee87a9d3e50b02f9
liuting <liuting@xiaomi.com>
2018-08-29 11:30:20 +0800

    [bugfix][水印位置调整][直播间]
    N/A
    
    Change-Id: I15c9130e8c433fcc33854a7db180eca953b488fc
    Signed-off-by: liuting <liuting@xiaomi.com>

--- watchclient/src/main/java/com/wali/live/watchsdk/watch/view/watchgameview/WatchGameWaterMarkView.java
+++ watchclient/src/main/java/com/wali/live/watchsdk/watch/view/watchgameview/WatchGameWaterMarkView.java
@@ -24,6 +24,25 @@ public class WatchGameWaterMarkView extends WatchWaterMarkView {
     }
 
     @Override
+    protected void init(Context context, AttributeSet attrs, int defStyleAttr) {
+        super.init(context, attrs, defStyleAttr);
+
+        LayoutParams layoutParams = (LayoutParams) mIvHuYaLogo.getLayoutParams();
+        layoutParams.removeRule(ALIGN_PARENT_TOP);
+        layoutParams.removeRule(ALIGN_PARENT_RIGHT);
+        layoutParams.addRule(ALIGN_PARENT_BOTTOM);
+        layoutParams.addRule(ALIGN_PARENT_LEFT);
+        mIvHuYaLogo.setLayoutParams(layoutParams);
+
+        layoutParams = (LayoutParams) mMiLogoArea.getLayoutParams();
+        layoutParams.removeRule(ALIGN_PARENT_TOP);
+        layoutParams.removeRule(ALIGN_PARENT_RIGHT);
+        layoutParams.addRule(ALIGN_PARENT_BOTTOM);
+        layoutParams.addRule(ALIGN_PARENT_LEFT);
+        mMiLogoArea.setLayoutParams(layoutParams);
+    }
+
+    @Override
     public void onOrientation(boolean isLandscape) {
         //防止父类父类调用方式发生改变,手动给他重写下
     }
