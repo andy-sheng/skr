30ce559396f72d7bafae5a2957ebd16951819b1a
chengsimin <chengsimin@xiaomi.com>
2018-11-07 21:48:59 +0800

    channel完善
    N/A
    
    Change-Id: I36dd582b27cd669f7631c72c78f5ec795da3f821
    Signed-off-by: chengsimin <chengsimin@xiaomi.com>

--- baseLibrary/commonsdk/src/main/java/com/common/utils/ChannelUtils.java
+++ baseLibrary/commonsdk/src/main/java/com/common/utils/ChannelUtils.java
@@ -1,39 +1,59 @@
 package com.common.utils;
 
-import com.common.log.MyLog;
+import android.text.TextUtils;
+
 import com.pgyersdk.crash.PgyCrashManager;
 
 import java.lang.reflect.Field;
 
 public class ChannelUtils {
     public final static String TAG = "ChannelUtils";
-    private String channelName = "DEFAULT";
+    private static final String PREF_KEY_CHANNEL = "key_channel";
+    private String channelNameFromBuildConfig = "DEFAULT";
+    private String channelNameFromPref;
 
     ChannelUtils() {
         try {
             Class ct = Class.forName(U.getAppInfoUtils().getPackageName() + ".BuildConfig");
             Field field = ct.getField("CHANNEL_NAME");
-            channelName = (String) field.get(null);
+            channelNameFromBuildConfig = (String) field.get(null);
         } catch (Exception e) {
             PgyCrashManager.reportCaughtException(e);
         }
     }
 
     public String getChannelNameFromBuildConfig() {
-        return channelName;
+        return channelNameFromBuildConfig;
     }
 
     /**
      * 这里以后会修改，返回真正渠道号。
      * 原理： DEFAULT 为自升级渠道，不能覆盖原本的渠道号
+     *
      * @return
      */
     public String getChannel() {
-        return channelName;
+        if ("TEST".equals(channelNameFromBuildConfig)) {
+            return "TEST";
+        }
+        if (TextUtils.isEmpty(channelNameFromPref)) {
+            channelNameFromPref = U.getPreferenceUtils().getSettingString(PREF_KEY_CHANNEL, "DEFAULT");
+        }
+        if (!channelNameFromBuildConfig.equals(channelNameFromPref)) {
+            if (channelNameFromBuildConfig.equals("DEFAULT")) {
+                // 如果是自升级渠道
+                return channelNameFromPref;
+            } else {
+                // 如果不是自升级渠道
+                channelNameFromPref = channelNameFromBuildConfig;
+                U.getPreferenceUtils().setSettingString(PREF_KEY_CHANNEL, channelNameFromPref);
+            }
+        }
+        return channelNameFromPref;
     }
 
-    public boolean isTestChannel(){
-        return "TEST".equals(channelName);
+    public boolean isTestChannel() {
+        return getChannel().equals(channelNameFromBuildConfig);
     }
 
 }
