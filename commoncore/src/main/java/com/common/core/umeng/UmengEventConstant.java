b79e6529c7e8d26cb917d4ce3fd723384c98f0ae
xuzhiyong <xuzhiyong@xuzhiyongdeMacBook-Pro.local>
2018-12-04 21:14:29 +0800

    删掉无用代码
    Signed-off-by: xuzhiyong <xuzhiyong@xuzhiyongdeMacBook-Pro.local>

--- commoncore/src/main/java/com/common/core/umeng/UmengEventConstant.java
+++
@@ -1,6 +0,0 @@
-package com.common.core.umeng;
-
-public class UmengEventConstant {
-    public static final String PLAY_EVENT = "play_music";
-    public static final String ENTER_ROOM_EVENT = "enter_room";
-}
