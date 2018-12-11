dfa1019daf24712f656fb4787c21a3b02074b428
xuzhiyong <xuzhiyong@xuzhiyongdeMacBook-Pro.local>
2018-12-11 14:59:27 +0800

    ApiMethod添加一个函数
    Signed-off-by: xuzhiyong <xuzhiyong@xuzhiyongdeMacBook-Pro.local>

--- baseLibrary/commonsdk/src/main/java-rxretrofit/com/common/rxretrofit/ApiMethods.java
+++ baseLibrary/commonsdk/src/main/java-rxretrofit/com/common/rxretrofit/ApiMethods.java
@@ -2,7 +2,6 @@ package com.common.rxretrofit;
 
 import com.common.base.BaseActivity;
 import com.common.base.BaseFragment;
-import com.common.mvp.Presenter;
 import com.common.mvp.PresenterEvent;
 import com.common.mvp.RxLifeCyclePresenter;
 import com.trello.rxlifecycle2.android.ActivityEvent;
@@ -11,6 +10,7 @@ import com.trello.rxlifecycle2.android.FragmentEvent;
 import io.reactivex.Observable;
 import io.reactivex.ObservableTransformer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 public class ApiMethods {
@@ -80,5 +80,52 @@ public class ApiMethods {
      */
     public static <T> void subscribe(Observable<T> observable, ApiObserver<T> apiObserver, RxLifeCyclePresenter presenter) {
         innerSubscribe(observable, apiObserver, presenter.bindUntilEvent(PresenterEvent.DESTROY));
+    }
+
+    /**
+     * 订阅数据，并绑定RxLifeCyclePresenter的声明周期
+     *
+     * @param observable
+     * @param apiObserver
+     * @param presenter
+     * @param <T>
+     */
+    public static <T> Disposable subscribeWith(Observable<T> observable, ApiObserver<T> apiObserver, RxLifeCyclePresenter presenter) {
+        return innerSubscribeWith(observable, apiObserver, presenter.bindUntilEvent(PresenterEvent.DESTROY));
+    }
+
+    private static <T> Disposable innerSubscribeWith(Observable<T> observable, ApiObserver<T> apiObserver, ObservableTransformer transformer) {
+        Observable<T> ob1 = observable.subscribeOn(Schedulers.io())
+                .unsubscribeOn(Schedulers.io());
+        if (transformer != null) {
+            ob1 = ob1.compose(transformer);
+        }
+
+        ob1 = ob1.observeOn(AndroidSchedulers.mainThread());
+        if (apiObserver == null) {
+            // 给个默认的实现，防止崩溃
+            ApiObserver observer = new ApiObserver() {
+                @Override
+                public void process(Object obj) {
+
+                }
+            };
+
+            return ob1.subscribe(t -> {
+                observer.onNext(t);
+            }, throwable -> {
+                observer.onError(throwable);
+            });
+        } else {
+            return ob1.subscribe(t -> {
+                apiObserver.onNext(t);
+            }, throwable -> {
+                apiObserver.onError(throwable);
+            }, () ->{
+                apiObserver.onComplete();
+            }, disposable -> {
+                apiObserver.onSubscribe(disposable);
+            });
+        }
     }
 }
