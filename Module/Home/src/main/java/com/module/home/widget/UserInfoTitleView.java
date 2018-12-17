25ef80ff3243c17784a266c02f6d1966d902f6b0
xuzhiyong <xuzhiyong@xuzhiyongdeMacBook-Pro.local>
2018-12-17 16:17:43 +0800

    首页ui
    Signed-off-by: xuzhiyong <xuzhiyong@xuzhiyongdeMacBook-Pro.local>

--- Module/Home/src/main/java/com/module/home/widget/UserInfoTitleView.java
+++ Module/Home/src/main/java/com/module/home/widget/UserInfoTitleView.java
@@ -1,10 +1,20 @@
 package com.module.home.widget;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.widget.RelativeLayout;
+
+import com.common.core.avatar.AvatarUtils;
+import com.common.core.myinfo.MyUserInfoManager;
+import com.common.utils.U;
+import com.common.view.ex.ExTextView;
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.module.home.R;
 public class UserInfoTitleView extends RelativeLayout {
+    SimpleDraweeView mIvUserIcon;
+    ExTextView mTvUserName;
+    ExTextView mTvUserLevel;
     public UserInfoTitleView(Context context) {
         this(context, null);
     }
@@ -20,5 +30,23 @@ public class UserInfoTitleView extends RelativeLayout {
 
     private void init(){
         inflate(getContext(), R.layout.user_info_title_layout, this);
+
+        mIvUserIcon = findViewById(R.id.iv_user_icon);
+        mTvUserName = findViewById(R.id.tv_user_name);
+        mTvUserLevel = findViewById(R.id.tv_user_level);
+
+        AvatarUtils.loadAvatarByUrl(mIvUserIcon,
+                AvatarUtils.newParamsBuilder(MyUserInfoManager.getInstance().getAvatar())
+                        .setWebpFormat(true)
+                        .setCircle(true)
+                        .setCornerRadius(200)
+                        .setBorderWidth(U.getDisplayUtils().dip2px(3))
+                        .setBorderColor(Color.WHITE)
+                        .build());
+
+        mTvUserName.setText(MyUserInfoManager.getInstance().getNickName());
+        mTvUserLevel.setText("铂金唱将");
     }
+
+
 }
