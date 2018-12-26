b33368c5c30f5160c6808cc03f0abd853a1fa9a1
long jiang <longjiang@longdeMacBook-Pro.local>
2018-12-26 17:23:53 +0800

    [个人主页初步]
    N/A
    
    Signed-off-by: long jiang <longjiang@longdeMacBook-Pro.local>

--- Module/Home/src/main/java/com/module/home/fragment/PersonFragment.java
+++ Module/Home/src/main/java/com/module/home/fragment/PersonFragment.java
@@ -2,48 +2,49 @@ package com.module.home.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.Gravity;
-import android.view.View;
+
 import android.widget.RelativeLayout;
 
-import com.alibaba.android.arouter.launcher.ARouter;
 import com.common.base.BaseFragment;
-import com.common.base.FragmentDataListener;
-import com.common.core.account.UserAccountManager;
+
 import com.common.core.avatar.AvatarUtils;
 import com.common.core.myinfo.MyUserInfoManager;
 import com.common.core.myinfo.event.MyUserInfoEvent;
 import com.common.image.fresco.BaseImageView;
-import com.common.upload.UploadCallback;
-import com.common.upload.UploadParams;
-import com.common.upload.UploadTask;
+
 import com.common.utils.FragmentUtils;
 import com.common.utils.U;
+import com.common.view.ex.ExImageView;
 import com.common.view.ex.ExTextView;
-import com.dialog.view.EditTextWithOkBtnView;
-import com.imagepicker.ImagePicker;
-import com.imagepicker.fragment.ImagePickerFragment;
-import com.imagepicker.model.ImageItem;
-import com.imagepicker.view.CropImageView;
-import com.module.RouterConstants;
+
+import com.jakewharton.rxbinding2.view.RxView;
 import com.module.home.R;
-import com.orhanobut.dialogplus.DialogPlus;
-import com.orhanobut.dialogplus.ViewHolder;
 
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
-import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.functions.Consumer;
+
 
 public class PersonFragment extends BaseFragment {
 
+    RelativeLayout mPersonMainContainner;
     BaseImageView mAvatarIv;
+    ExTextView mShareTv;
+    ExTextView mSettingTv;
     ExTextView mNameTv;
-    ExTextView mUserIdTv;
-    ExTextView mUpdateNicknameBtn;
-    RelativeLayout mClearCacheBtn;
-    RelativeLayout mLogoutBtn;
-    RelativeLayout mSetting;
+    ExTextView mSignTv;
+    RelativeLayout mFriends;
+    ExTextView mFriendsNumTv;
+    RelativeLayout mFans;
+    ExTextView mFansNumTv;
+    RelativeLayout mFollows;
+    ExTextView mFollowsNumTv;
+    RelativeLayout mMedalLayout;
+    ExImageView mAuditionRoomTv;
+    ExImageView mMusicTestTv;
 
     @Override
     public int initView() {
@@ -52,126 +53,138 @@ public class PersonFragment extends BaseFragment {
 
     @Override
     public void initData(@Nullable Bundle savedInstanceState) {
+        mPersonMainContainner = (RelativeLayout) mRootView.findViewById(R.id.person_main_containner);
         mAvatarIv = (BaseImageView) mRootView.findViewById(R.id.avatar_iv);
+        mShareTv = (ExTextView) mRootView.findViewById(R.id.share_tv);
+        mSettingTv = (ExTextView) mRootView.findViewById(R.id.setting_tv);
         mNameTv = (ExTextView) mRootView.findViewById(R.id.name_tv);
-        mUpdateNicknameBtn = (ExTextView) mRootView.findViewById(R.id.update_nickname_btn);
-        mClearCacheBtn = (RelativeLayout) mRootView.findViewById(R.id.clear_cache_btn);
-        mLogoutBtn = (RelativeLayout) mRootView.findViewById(R.id.logout_btn);
-        mUserIdTv = mRootView.findViewById(R.id.user_id_tv);
-        mUserIdTv.setText("用户id:" + UserAccountManager.getInstance().getUuid());
-        mSetting = (RelativeLayout) mRootView.findViewById(R.id.setting);
+        mSignTv = (ExTextView) mRootView.findViewById(R.id.sign_tv);
+        mFriends = (RelativeLayout) mRootView.findViewById(R.id.friends);
+        mFriendsNumTv = (ExTextView) mRootView.findViewById(R.id.friends_num_tv);
+        mFans = (RelativeLayout) mRootView.findViewById(R.id.fans);
+        mFansNumTv = (ExTextView) mRootView.findViewById(R.id.fans_num_tv);
+        mFollows = (RelativeLayout) mRootView.findViewById(R.id.follows);
+        mFollowsNumTv = (ExTextView) mRootView.findViewById(R.id.follows_num_tv);
+        mMedalLayout = (RelativeLayout) mRootView.findViewById(R.id.medal_layout);
+        mAuditionRoomTv = (ExImageView) mRootView.findViewById(R.id.audition_room_tv);
+        mMusicTestTv = (ExImageView) mRootView.findViewById(R.id.music_test_tv);
+
+        initViewData();
 
 
+        RxView.clicks(mSettingTv)
+                .throttleFirst(500, TimeUnit.MILLISECONDS)
+                .subscribe(new Consumer<Object>() {
+                    @Override
+                    public void accept(Object o) {
+                        U.getFragmentUtils().addFragment(
+                                FragmentUtils.newAddParamsBuilder(getActivity(), SettingFragment.class)
+                                        .setAddToBackStack(true)
+                                        .setHasAnimation(true)
+                                        .build());
+                    }
+                });
+
+//        mUpdateNicknameBtn.setOnClickListener(new View.OnClickListener() {
+//            DialogPlus mDialogPlus;
+//
+//            @Override
+//            public void onClick(View v) {
+//                EditTextWithOkBtnView editText = new EditTextWithOkBtnView(getContext());
+//                editText.getContentEt().setText(MyUserInfoManager.getInstance().getNickName());
+//                editText.getContentEt().setHint("输入昵称");
+//                editText.getOkBtn().setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        if (mDialogPlus != null) {
+//                            mDialogPlus.dismiss();
+//                        }
+//                        MyUserInfoManager.getInstance().updateInfo(
+//                                MyUserInfoManager.newMyInfoUpdateParamsBuilder()
+//                                        .setNickName(editText.getContentEt().getText().toString())
+//                                        .build()
+//                                );
+//                    }
+//                });
+//                mDialogPlus = DialogPlus.newDialog(getContext())
+//                        .setGravity(Gravity.CENTER)
+//                        .setContentHolder(new ViewHolder(editText))
+//                        .create();
+//                mDialogPlus.show();
+//            }
+//        });
+//
+//        mAvatarIv.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                ImagePicker.getInstance().setParams(ImagePicker.newParamsBuilder()
+//                        .setSelectLimit(1)
+//                        .setCropStyle(CropImageView.Style.CIRCLE)
+//                        .build()
+//                );
+//
+//                U.getFragmentUtils().addFragment(FragmentUtils.newAddParamsBuilder(getActivity(), ImagePickerFragment.class)
+//                        .setContainerViewId(R.id.person_main_containner)
+//                        .setAddToBackStack(true)
+//                        .setHasAnimation(true)
+//                        .setFragmentDataListener(new FragmentDataListener() {
+//                            @Override
+//                            public void onFragmentResult(int requestCode, int resultCode, Bundle bundle, Object object) {
+//                                List<ImageItem> list = ImagePicker.getInstance().getSelectedImages();
+//                                if (list.size() > 0) {
+//                                    ImageItem imageItem = list.get(0);
+//                                    UploadTask uploadTask = UploadParams.newBuilder(imageItem.getPath())
+//                                            .setNeedCompress(true)
+//                                            .startUploadAsync(new UploadCallback() {
+//                                                @Override
+//                                                public void onProgress(long currentSize, long totalSize) {
+//
+//                                                }
+//
+//                                                @Override
+//                                                public void onSuccess(String url) {
+//                                                    U.getToastUtil().showShort("上传成功 url:" + url);
+//                                                    MyUserInfoManager.getInstance().updateInfo(  MyUserInfoManager.newMyInfoUpdateParamsBuilder()
+//                                                            .setAvatar(url)
+//                                                            .build());
+//                                                }
+//
+//                                                @Override
+//                                                public void onFailure(String msg) {
+//
+//                                                }
+//
+//                                            });
+//                                }
+//                            }
+//                        })
+//                        .build());
+//            }
+//        });
+//
+//        mLogoutBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                UserAccountManager.getInstance().logoff();
+//                ARouter.getInstance().build(RouterConstants.ACTIVITY_LOGIN).navigation();
+//            }
+//        });
+//
+
+    }
+
+    private void initViewData() {
         AvatarUtils.loadAvatarByUrl(mAvatarIv, AvatarUtils.newParamsBuilder(MyUserInfoManager.getInstance()
                 .getAvatar())
                 .build());
         mNameTv.setText(MyUserInfoManager.getInstance().getNickName());
-
-        mUpdateNicknameBtn.setOnClickListener(new View.OnClickListener() {
-            DialogPlus mDialogPlus;
-
-            @Override
-            public void onClick(View v) {
-                EditTextWithOkBtnView editText = new EditTextWithOkBtnView(getContext());
-                editText.getContentEt().setText(MyUserInfoManager.getInstance().getNickName());
-                editText.getContentEt().setHint("输入昵称");
-                editText.getOkBtn().setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if (mDialogPlus != null) {
-                            mDialogPlus.dismiss();
-                        }
-                        MyUserInfoManager.getInstance().updateInfo(
-                                MyUserInfoManager.newMyInfoUpdateParamsBuilder()
-                                        .setNickName(editText.getContentEt().getText().toString())
-                                        .build()
-                                );
-                    }
-                });
-                mDialogPlus = DialogPlus.newDialog(getContext())
-                        .setGravity(Gravity.CENTER)
-                        .setContentHolder(new ViewHolder(editText))
-                        .create();
-                mDialogPlus.show();
-            }
-        });
-
-        mAvatarIv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                ImagePicker.getInstance().setParams(ImagePicker.newParamsBuilder()
-                        .setSelectLimit(1)
-                        .setCropStyle(CropImageView.Style.CIRCLE)
-                        .build()
-                );
-
-                U.getFragmentUtils().addFragment(FragmentUtils.newAddParamsBuilder(getActivity(), ImagePickerFragment.class)
-                        .setContainerViewId(R.id.person_main_containner)
-                        .setAddToBackStack(true)
-                        .setHasAnimation(true)
-                        .setFragmentDataListener(new FragmentDataListener() {
-                            @Override
-                            public void onFragmentResult(int requestCode, int resultCode, Bundle bundle, Object object) {
-                                List<ImageItem> list = ImagePicker.getInstance().getSelectedImages();
-                                if (list.size() > 0) {
-                                    ImageItem imageItem = list.get(0);
-                                    UploadTask uploadTask = UploadParams.newBuilder(imageItem.getPath())
-                                            .setNeedCompress(true)
-                                            .startUploadAsync(new UploadCallback() {
-                                                @Override
-                                                public void onProgress(long currentSize, long totalSize) {
-
-                                                }
-
-                                                @Override
-                                                public void onSuccess(String url) {
-                                                    U.getToastUtil().showShort("上传成功 url:" + url);
-                                                    MyUserInfoManager.getInstance().updateInfo(  MyUserInfoManager.newMyInfoUpdateParamsBuilder()
-                                                            .setAvatar(url)
-                                                            .build());
-                                                }
-
-                                                @Override
-                                                public void onFailure(String msg) {
-
-                                                }
-
-                                            });
-                                }
-                            }
-                        })
-                        .build());
-            }
-        });
-
-        mLogoutBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UserAccountManager.getInstance().logoff();
-                ARouter.getInstance().build(RouterConstants.ACTIVITY_LOGIN).navigation();
-            }
-        });
-
-        mSetting.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                U.getFragmentUtils().addFragment(
-                        FragmentUtils.newAddParamsBuilder(getActivity(), SettingFragment.class)
-                                .setAddToBackStack(true)
-                                .setHasAnimation(true)
-                                .build());
-            }
-        });
-
+        mSignTv.setText(MyUserInfoManager.getInstance().getSignature());
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onEvnet(MyUserInfoEvent.UserInfoChangeEvent userInfoChangeEvent) {
-        AvatarUtils.loadAvatarByUrl(mAvatarIv, AvatarUtils.newParamsBuilder(MyUserInfoManager.getInstance()
-                .getAvatar())
-                .build());
-        mNameTv.setText(MyUserInfoManager.getInstance().getNickName());
+        initViewData();
     }
 
     @Override
