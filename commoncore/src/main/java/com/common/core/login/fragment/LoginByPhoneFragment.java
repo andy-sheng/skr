a15f287fd4d3b9fd009576be0db234a5e8eb0199
long jiang <longjiang@longdeMacBook-Pro.local>
2018-12-28 12:33:02 +0800

    [完善登录页面]
    N/A
    
    Signed-off-by: long jiang <longjiang@longdeMacBook-Pro.local>

--- commoncore/src/main/java/com/common/core/login/fragment/LoginByPhoneFragment.java
+++ commoncore/src/main/java/com/common/core/login/fragment/LoginByPhoneFragment.java
@@ -1,19 +1,31 @@
 package com.common.core.login.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
-import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.common.base.BaseFragment;
 import com.common.core.R;
-import com.common.utils.FragmentUtils;
+import com.common.core.account.UserAccountManager;
+import com.common.core.account.UserAccountServerApi;
+import com.common.log.MyLog;
+import com.common.rxretrofit.ApiManager;
+import com.common.rxretrofit.ApiMethods;
+import com.common.rxretrofit.ApiObserver;
+import com.common.rxretrofit.ApiResult;
+import com.common.utils.ToastUtils;
 import com.common.utils.U;
-import com.common.view.ex.ExButton;
+import com.common.view.ex.ExImageView;
 import com.common.view.ex.ExTextView;
 import com.common.view.ex.NoLeakEditText;
-import com.common.view.titlebar.CommonTitleBar;
+import com.jakewharton.rxbinding2.view.RxView;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.functions.Consumer;
 
 /**
  * 手机方式登陆界面
@@ -21,13 +33,14 @@ import com.common.view.titlebar.CommonTitleBar;
 public class LoginByPhoneFragment extends BaseFragment {
 
     RelativeLayout mMainActContainer;
-    CommonTitleBar mTitlebar;
-    RelativeLayout mMainContainer;
-    ExTextView mLogoTv;
-    NoLeakEditText mInputPhoneEt;
-    ExTextView mPhoneHintTv;
-    ExButton mNextBtn;
+    ExImageView mIvBack;
+    NoLeakEditText mPhoneInputTv;
+    NoLeakEditText mCodeInputTv;
+    ExTextView mGetCodeTv;
+    ExTextView mLoginTv;
 
+    String mPhoneNumber; //发送验证码的电话号码
+    String mCode; //验证码
 
     @Override
     public int initView() {
@@ -37,32 +50,65 @@ public class LoginByPhoneFragment extends BaseFragment {
     @Override
     public void initData(@Nullable Bundle savedInstanceState) {
         mMainActContainer = (RelativeLayout) mRootView.findViewById(R.id.main_act_container);
-        mTitlebar = (CommonTitleBar) mRootView.findViewById(R.id.titlebar);
-        mMainContainer = (RelativeLayout) mRootView.findViewById(R.id.main_container);
-        mLogoTv = (ExTextView) mRootView.findViewById(R.id.logo_tv);
-        mInputPhoneEt = (NoLeakEditText) mRootView.findViewById(R.id.input_phone_et);
-        mPhoneHintTv = (ExTextView) mRootView.findViewById(R.id.phone_hint_tv);
-        mNextBtn = (ExButton) mRootView.findViewById(R.id.next_btn);
+        mIvBack = (ExImageView) mRootView.findViewById(R.id.iv_back);
+        mPhoneInputTv = (NoLeakEditText) mRootView.findViewById(R.id.phone_input_tv);
+        mCodeInputTv = (NoLeakEditText) mRootView.findViewById(R.id.code_input_tv);
+        mGetCodeTv = (ExTextView) mRootView.findViewById(R.id.get_code_tv);
+        mLoginTv = (ExTextView) mRootView.findViewById(R.id.login_tv);
 
-        mNextBtn.setOnClickListener(new View.OnClickListener() {
+        RxView.clicks(mGetCodeTv)
+                .throttleFirst(500, TimeUnit.MILLISECONDS)
+                .subscribe(new Consumer<Object>() {
+                    @Override
+                    public void accept(Object o) {
+                        mPhoneNumber = mPhoneInputTv.getText().toString().trim();
+                        if (checkPhoneNumber(mPhoneNumber)) {
+                            sendSmsVerifyCode(mPhoneNumber);
+                        }
+                    }
+                });
+
+        RxView.clicks(mLoginTv)
+                .throttleFirst(500, TimeUnit.MILLISECONDS)
+                .subscribe(new Consumer<Object>() {
+                    @Override
+                    public void accept(Object o) {
+                        U.getKeyBoardUtils().hideSoftInputKeyBoard(getActivity());
+                        mPhoneNumber = mPhoneInputTv.getText().toString().trim();
+                        mCode = mCodeInputTv.getText().toString().trim();
+                        if (checkPhoneNumber(mPhoneNumber)) {
+                            if (!TextUtils.isEmpty(mCode)) {
+                                UserAccountManager.getInstance().loginByPhoneNum(mPhoneNumber, mCode);
+                            } else {
+                                U.getToastUtil().showShort("验证码为空");
+                            }
+                        } else {
+                            U.getToastUtil().showShort("手机号有误");
+                        }
+
+                    }
+                });
+    }
+
+
+    private void sendSmsVerifyCode(final String phoneNumber) {
+        MyLog.d(TAG, "sendSmsVerifyCode" + " phoneNumber=" + phoneNumber);
+        if (TextUtils.isEmpty(phoneNumber)) {
+            MyLog.d(TAG, "sendSmsVerifyCode" + " phoneNumber=" + phoneNumber);
+            return;
+        }
+
+        UserAccountServerApi userAccountServerApi = ApiManager.getInstance().createService(UserAccountServerApi.class);
+        ApiMethods.subscribe(userAccountServerApi.sendSmsVerifyCode(phoneNumber), new ApiObserver<ApiResult>() {
             @Override
-            public void onClick(View view) {
-                if (U.getCommonUtils().isFastDoubleClick()) {
-                    return;
-                }
-                String phoneNumber = mInputPhoneEt.getText().toString().trim();
-                if (checkPhoneNumber(phoneNumber)) {
-                    Bundle bundle = new Bundle();
-                    bundle.putString(VerifyCodeFragment.EXTRA_PHONE_NUMBER, phoneNumber);
-                    U.getFragmentUtils().addFragment(FragmentUtils
-                            .newAddParamsBuilder(getActivity(), VerifyCodeFragment.class)
-                            .setAddToBackStack(true)
-                            .setHasAnimation(true)
-                            .setBundle(bundle)
-                            .build());
+            public void process(ApiResult result) {
+                if (result.getErrno() == 0) {
+                    // 发送验证码成功
+                    mGetCodeTv.setSelected(true);
+                    mGetCodeTv.setClickable(false);
                 }
             }
-        });
+        }, this);
     }
 
     /**
@@ -76,8 +122,6 @@ public class LoginByPhoneFragment extends BaseFragment {
             return true;
         }
 
-        mPhoneHintTv.setVisibility(View.VISIBLE);
-        mPhoneHintTv.setText("请输入正确的手机号");
         return false;
     }
 
