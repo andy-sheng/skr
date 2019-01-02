e088a059f8ef2eafde75c1913921972b524fe7d9
xuzhiyong <xuzhiyong@xuzhiyongdeMacBook-Pro.local>
2019-01-02 16:43:35 +0800

    试音房调整
    Signed-off-by: xuzhiyong <xuzhiyong@xuzhiyongdeMacBook-Pro.local>

--- Module/PlayWays/src/main/java-prepare/com/module/playways/rank/prepare/sence/AuditionFragment.java
+++ Module/PlayWays/src/main/java-prepare/com/module/playways/rank/prepare/sence/AuditionFragment.java
@@ -1,13 +1,21 @@
 package com.module.playways.rank.prepare.sence;
 
+import android.animation.AnimatorSet;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+
 import com.common.base.BaseFragment;
 import com.common.log.MyLog;
+import com.common.utils.HandlerTaskTimer;
 import com.common.utils.SongResUtils;
 import com.common.utils.U;
+import com.common.view.ex.ExTextView;
 import com.engine.EngineManager;
+import com.jakewharton.rxbinding2.view.RxView;
 import com.module.playways.rank.prepare.model.PrepareData;
+import com.module.playways.rank.prepare.view.VoiceControlPanelView;
 import com.module.playways.rank.song.model.SongModel;
 import com.module.rank.R;
 import com.trello.rxlifecycle2.android.FragmentEvent;
@@ -15,12 +23,17 @@ import com.zq.lyrics.LyricsManager;
 import com.zq.lyrics.event.LrcEvent;
 import com.zq.lyrics.widget.AbstractLrcView;
 import com.zq.lyrics.widget.ManyLyricsView;
+
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
+
 import java.io.File;
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
+
 import static com.zq.lyrics.widget.AbstractLrcView.LRCPLAYERSTATUS_PLAY;
 
 public class AuditionFragment extends BaseFragment {
@@ -28,9 +41,15 @@ public class AuditionFragment extends BaseFragment {
 
     ManyLyricsView mManyLyricsView;
 
+    ExTextView mTvDown;
+
+    ExTextView mTvUp;
+
     PrepareData mPrepareData;
 
     SongModel songModel;
+
+    VoiceControlPanelView mVoiceControlPanelView;
 
     @Override
     public int initView() {
@@ -42,6 +61,21 @@ public class AuditionFragment extends BaseFragment {
         if (!EventBus.getDefault().isRegistered(this)) {
             EventBus.getDefault().register(this);
         }
+
+        mTvDown = mRootView.findViewById(R.id.tv_down);
+        mTvUp = mRootView.findViewById(R.id.tv_up);
+
+        mVoiceControlPanelView = mRootView.findViewById(R.id.voice_control_view);
+
+        RxView.clicks(mTvDown).throttleFirst(500, TimeUnit.MILLISECONDS)
+                .subscribe(o -> {
+                    showVoicePanelView(false);
+        });
+
+        RxView.clicks(mTvUp).throttleFirst(500, TimeUnit.MILLISECONDS)
+                .subscribe(o -> {
+                    showVoicePanelView(true);
+                });
 
         mManyLyricsView = mRootView.findViewById(R.id.many_lyrics_view);
         mManyLyricsView.setOnLrcClickListener(new ManyLyricsView.OnLrcClickListener() {
@@ -80,6 +114,37 @@ public class AuditionFragment extends BaseFragment {
 
         playMusic(songModel);
         playLyrics(songModel);
+    }
+
+    private boolean mIsVoiceShow = true;
+
+    private void showVoicePanelView(boolean show){
+        mVoiceControlPanelView.clearAnimation();
+        mVoiceControlPanelView.setTranslationY(show ? mVoiceControlPanelView.getMeasuredHeight() + U.getDisplayUtils().dip2px(20) : 0);
+
+        mIsVoiceShow = show;
+        int startY = show ? mVoiceControlPanelView.getMeasuredHeight() + U.getDisplayUtils().dip2px(20) : 0;
+        int endY = show ? 0 : mVoiceControlPanelView.getMeasuredHeight() + U.getDisplayUtils().dip2px(20);
+
+        ValueAnimator creditValueAnimator = ValueAnimator.ofInt(startY, endY);
+        creditValueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                mVoiceControlPanelView.setTranslationY((int)animation.getAnimatedValue());
+            }
+        });
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.setDuration(300);
+        animatorSet.play(creditValueAnimator);
+        animatorSet.start();
+
+        HandlerTaskTimer.newBuilder().delay(300).start(new HandlerTaskTimer.ObserverW() {
+            @Override
+            public void onNext(Integer integer) {
+                mTvUp.setVisibility(show ? View.GONE : View.VISIBLE);
+            }
+        });
     }
 
     @Override
