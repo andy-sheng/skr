e6628cdefe901d37cb29773b3bd0cfe65f9f80eb
xuzhiyong <xuzhiyong@xuzhiyongdeMacBook-Pro.local>
2019-01-03 18:52:42 +0800

    匹配成功背景先加上，不知道会不会用
    Signed-off-by: xuzhiyong <xuzhiyong@xuzhiyongdeMacBook-Pro.local>

---
+++ Module/PlayWays/src/main/java-prepare/com/module/playways/rank/prepare/view/MatchSucessBottomView.java
@@ -0,0 +1,51 @@
+package com.module.playways.rank.prepare.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.common.utils.U;
+
+public class MatchSucessBottomView extends View {
+
+    int[] mCenterPosition = new int[]{U.getDisplayUtils().getScreenWidth() / 2, U.getDisplayUtils().getScreenHeight() / 2};
+
+    public MatchSucessBottomView(Context context) {
+        this(context, null);
+    }
+
+    public MatchSucessBottomView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public MatchSucessBottomView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Paint p = new Paint();
+
+        p.setColor(Color.BLACK);
+        //实例化路径
+        Path path = new Path();
+        //第一个点
+        path.moveTo(mCenterPosition[0], mCenterPosition[1]);// 此点为多边形的起点
+        int[] point2 = new int[]{0, mCenterPosition[1] + U.getDisplayUtils().getScreenWidth() / 2};
+        path.lineTo(point2[0], point2[1]);
+        int[] point3 = new int[]{0, U.getDisplayUtils().getScreenHeight()};
+        path.lineTo(point3[0], point3[1]);
+        int[] point4 = new int[]{U.getDisplayUtils().getScreenWidth(), U.getDisplayUtils().getScreenHeight()};
+        path.lineTo(point4[0], point4[1]);
+        int[] point5 = new int[]{U.getDisplayUtils().getScreenWidth(), U.getDisplayUtils().getScreenHeight() - U.getDisplayUtils().getScreenWidth() / 2};
+        path.lineTo(point5[0], point5[1]);
+//        int[] point6 = new int[]{U.getDisplayUtils().getScreenWidth(), U.getDisplayUtils().getScreenHeight() - U.getDisplayUtils().getScreenWidth() / 2};
+//        path.lineTo(point5[0], point5[1]);
+        path.close(); // 使这些点构成封闭的多边形
+        canvas.drawPath(path, p);
+    }
+}
