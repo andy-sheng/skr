04b2ab193ee5713de51bbe0da430c7d730a8dcf7
long jiang <longjiang@longdeMacBook-Pro.local>
2019-01-20 18:53:14 +0800

    更新加入演唱结果失败时的svga动画
    Signed-off-by: long jiang <longjiang@longdeMacBook-Pro.local>

--- Module/PlayWays/src/main/java-room-grab/com/module/playways/grab/room/view/RoundOverCardView.java
+++ Module/PlayWays/src/main/java-room-grab/com/module/playways/grab/room/view/RoundOverCardView.java
@@ -1,6 +1,8 @@
 package com.module.playways.grab.room.view;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.widget.RelativeLayout;
 
@@ -8,9 +10,15 @@ import com.common.view.ex.ExTextView;
 import com.module.playways.grab.room.listener.SVGAListener;
 import com.module.rank.R;
 import com.opensource.svgaplayer.SVGACallback;
+import com.opensource.svgaplayer.SVGADrawable;
+import com.opensource.svgaplayer.SVGADynamicEntity;
 import com.opensource.svgaplayer.SVGAImageView;
+import com.opensource.svgaplayer.SVGAParser;
+import com.opensource.svgaplayer.SVGAVideoEntity;
 import com.zq.live.proto.Room.EQRoundOverReason;
 import com.zq.live.proto.Room.EQRoundResultType;
+
+import org.greenrobot.greendao.annotation.NotNull;
 
 /**
  * 轮次结束
@@ -26,6 +34,7 @@ public class RoundOverCardView extends RelativeLayout {
     public final static int SING_ENOUGH_END = 6;           // 有种可惜叫觉得你行
 
     SVGAImageView mNoneSingSvga;
+    SVGAImageView mSingResultSvga;
 
     public RoundOverCardView(Context context) {
         super(context);
@@ -45,6 +54,7 @@ public class RoundOverCardView extends RelativeLayout {
     private void init() {
         inflate(getContext(), R.layout.grab_round_over_card_layout, this);
         mNoneSingSvga = (SVGAImageView) findViewById(R.id.none_sing_svga);
+        mSingResultSvga = (SVGAImageView) findViewById(R.id.sing_result_svga);
     }
 
     public void bindData(int reason, int resultType, SVGAListener listener) {
@@ -60,7 +70,7 @@ public class RoundOverCardView extends RelativeLayout {
             case SING_NO_PASS_END:
             case SING_PASS_END:
             case SING_ENOUGH_END:
-                startNotPerfect(listener);
+                startFailed(mode, listener);
                 break;
             default:
                 if (listener != null) {
@@ -92,7 +102,25 @@ public class RoundOverCardView extends RelativeLayout {
 
     private void startNoneSing(SVGAListener listener) {
         mNoneSingSvga.setVisibility(VISIBLE);
-        mNoneSingSvga.startAnimation();
+        mNoneSingSvga.setLoops(1);
+        SVGAParser parser = new SVGAParser(getContext());
+        try {
+            parser.parse("none_sing_end.svga", new SVGAParser.ParseCompletion() {
+                @Override
+                public void onComplete(@NotNull SVGAVideoEntity videoItem) {
+                    SVGADrawable drawable = new SVGADrawable(videoItem);
+                    mNoneSingSvga.setImageDrawable(drawable);
+                    mNoneSingSvga.startAnimation();
+                }
+
+                @Override
+                public void onError() {
+
+                }
+            });
+        } catch (Exception e) {
+            System.out.print(true);
+        }
 
         mNoneSingSvga.setCallback(new SVGACallback() {
             @Override
@@ -134,10 +162,71 @@ public class RoundOverCardView extends RelativeLayout {
     }
 
     // 不够优秀，换字即可，目前缺动画
-    private void startNotPerfect(SVGAListener listener) {
-        if (listener != null) {
-            listener.onFinished();
+    private void startFailed(int model, SVGAListener listener) {
+        mSingResultSvga.setVisibility(VISIBLE);
+        mSingResultSvga.setLoops(1);
+        SVGAParser parser = new SVGAParser(getContext());
+        try {
+            parser.parse("sing_fail_end.svga", new SVGAParser.ParseCompletion() {
+                @Override
+                public void onComplete(@NotNull SVGAVideoEntity videoItem) {
+                    SVGADrawable drawable = new SVGADrawable(videoItem, requestDynamicItem(model));
+                    mSingResultSvga.setImageDrawable(drawable);
+                    mSingResultSvga.startAnimation();
+                }
+
+                @Override
+                public void onError() {
+
+                }
+            });
+        } catch (Exception e) {
+            System.out.print(true);
         }
+
+        mSingResultSvga.setCallback(new SVGACallback() {
+            @Override
+            public void onPause() {
+
+            }
+
+            @Override
+            public void onFinished() {
+                if (mSingResultSvga != null) {
+                    mSingResultSvga.stopAnimation(true);
+                }
+
+                if (listener != null) {
+                    listener.onFinished();
+                }
+            }
+
+            @Override
+            public void onRepeat() {
+                if (mSingResultSvga != null && mSingResultSvga.isAnimating()) {
+                    mSingResultSvga.stopAnimation(false);
+                }
+            }
+
+            @Override
+            public void onStep(int i, double v) {
+
+            }
+        });
+    }
+
+    private SVGADynamicEntity requestDynamicItem(int model) {
+        SVGADynamicEntity dynamicEntity = new SVGADynamicEntity();
+        if (model == SING_MOMENT_END) {
+            dynamicEntity.setDynamicImage(BitmapFactory.decodeResource(getResources(), R.drawable.fankuizi_3), "text");
+        } else if (model == SING_NO_PASS_END) {
+            dynamicEntity.setDynamicImage(BitmapFactory.decodeResource(getResources(), R.drawable.fankuizi_1), "text");
+        } else if (model == SING_PASS_END) {
+            dynamicEntity.setDynamicImage(BitmapFactory.decodeResource(getResources(), R.drawable.fankuizi_2), "text");
+        } else if (model == SING_ENOUGH_END) {
+            dynamicEntity.setDynamicImage(BitmapFactory.decodeResource(getResources(), R.drawable.fankuizi_4), "text");
+        }
+        return dynamicEntity;
     }
 
     @Override
