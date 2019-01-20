173b115252db538344072ae39ca747a746e89c74
long jiang <longjiang@longdeMacBook-Pro.local>
2019-01-21 00:19:23 +0800

    选择专场加入错误页面，加载页和空页面
    Signed-off-by: long jiang <longjiang@longdeMacBook-Pro.local>

--- Module/PlayWays/src/main/java-room-grab/com/module/playways/grab/songselect/SpecialSelectFragment.java
+++ Module/PlayWays/src/main/java-room-grab/com/module/playways/grab/songselect/SpecialSelectFragment.java
@@ -1,6 +1,7 @@
 package com.module.playways.grab.songselect;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -17,13 +18,22 @@ import com.common.rxretrofit.ApiResult;
 import com.common.view.ex.ExImageView;
 import com.common.view.recyclerview.RecyclerOnItemClickListener;
 import com.common.view.titlebar.CommonTitleBar;
+import com.component.busilib.callback.EmptyCallback;
+import com.component.busilib.callback.ErrorCallback;
+import com.component.busilib.callback.LoadingCallback;
 import com.component.busilib.constans.GameModeType;
 import com.jakewharton.rxbinding2.view.RxView;
 
+import com.kingja.loadsir.callback.Callback;
+import com.kingja.loadsir.core.LoadService;
+import com.kingja.loadsir.core.LoadSir;
 import com.module.RouterConstants;
 
 import com.module.playways.rank.prepare.model.PrepareData;
 import com.module.rank.R;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnRefreshLoadMoreListener;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -35,9 +45,15 @@ public class SpecialSelectFragment extends BaseFragment {
     RelativeLayout mMainActContainer;
     CommonTitleBar mTitleView;
     ExImageView mSelectLogoIv;
+
+    SmartRefreshLayout mRefreshLayout;
     RecyclerView mContentRv;
     SpecialSelectAdapter mSpecialSelectAdapter;
-    LinearLayoutManager mLinearLayoutManager;
+
+    LoadService mLoadService;
+
+    int offset = 0;          //偏移量
+    int DEFAULT_COUNT = 10;  // 每次拉去列表数目
 
     @Override
     public int initView() {
@@ -49,9 +65,26 @@ public class SpecialSelectFragment extends BaseFragment {
         mMainActContainer = (RelativeLayout) mRootView.findViewById(R.id.main_act_container);
         mTitleView = (CommonTitleBar) mRootView.findViewById(R.id.title_view);
         mSelectLogoIv = (ExImageView) mRootView.findViewById(R.id.select_logo_iv);
+        mRefreshLayout = (SmartRefreshLayout) mRootView.findViewById(R.id.refreshLayout);
         mContentRv = (RecyclerView) mRootView.findViewById(R.id.content_rv);
-        mLinearLayoutManager = new LinearLayoutManager(getContext());
-        mContentRv.setLayoutManager(mLinearLayoutManager);
+
+        mRefreshLayout.setEnableRefresh(false);
+        mRefreshLayout.setEnableLoadMore(true);
+        mRefreshLayout.setEnableLoadMoreWhenContentNotFull(true);
+        mRefreshLayout.setEnableOverScrollDrag(false);
+        mRefreshLayout.setOnRefreshLoadMoreListener(new OnRefreshLoadMoreListener() {
+            @Override
+            public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+                loadData(offset, DEFAULT_COUNT);
+            }
+
+            @Override
+            public void onRefresh(@NonNull RefreshLayout refreshLayout) {
+                mRefreshLayout.finishRefresh();
+            }
+        });
+        mContentRv.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+
         mSpecialSelectAdapter = new SpecialSelectAdapter(new RecyclerOnItemClickListener<SpecialModel>() {
             @Override
             public void onItemClicked(View view, int position, SpecialModel model) {
@@ -59,7 +92,7 @@ public class SpecialSelectFragment extends BaseFragment {
             }
         });
         mContentRv.setAdapter(mSpecialSelectAdapter);
-        loadData();
+        loadData(offset, DEFAULT_COUNT);
 
         RxView.clicks(mTitleView.getLeftImageButton())
                 .throttleFirst(500, TimeUnit.MILLISECONDS)
@@ -69,21 +102,52 @@ public class SpecialSelectFragment extends BaseFragment {
                         getActivity().finish();
                     }
                 });
+
+
+        LoadSir mLoadSir = new LoadSir.Builder()
+                .addCallback(new LoadingCallback(R.drawable.wulishigedan, "数据正在努力加载中..."))
+                .addCallback(new EmptyCallback(R.drawable.wulishigedan, "你敢不敢唱首歌？"))
+                .addCallback(new ErrorCallback(R.drawable.wulishigedan, "请求出错了..."))
+                .setDefaultCallback(LoadingCallback.class)
+                .build();
+        mLoadService = mLoadSir.register(mRefreshLayout, new Callback.OnReloadListener() {
+            @Override
+            public void onReload(View v) {
+                loadData(offset, DEFAULT_COUNT);
+            }
+        });
+
     }
 
-    private void loadData() {
+    private void loadData(int offset, int count) {
         GrabSongApi grabSongApi = ApiManager.getInstance().createService(GrabSongApi.class);
-        ApiMethods.subscribe(grabSongApi.getSepcialList(0, 10), new ApiObserver<ApiResult>() {
+        ApiMethods.subscribe(grabSongApi.getSepcialList(offset, count), new ApiObserver<ApiResult>() {
             @Override
             public void process(ApiResult obj) {
                 if (obj.getErrno() == 0) {
                     List<SpecialModel> list = JSON.parseArray(obj.getData().getString("tags"), SpecialModel.class);
-                    if (list != null) {
-                        mSpecialSelectAdapter.setDataList(list);
-                    }
+                    int offset = obj.getData().getIntValue("offset");
+                    refreshView(list, offset);
+                } else {
+                    mLoadService.showCallback(ErrorCallback.class);
                 }
             }
         });
+    }
+
+    private void refreshView(List<SpecialModel> list, int offset) {
+        this.offset = offset;
+        mRefreshLayout.finishLoadMore();
+        if (list != null) {
+            mSpecialSelectAdapter.getDataList().addAll(list);
+            mSpecialSelectAdapter.notifyDataSetChanged();
+        }
+
+        if (mSpecialSelectAdapter.getDataList() != null && mSpecialSelectAdapter.getDataList().size() > 0) {
+            mLoadService.showSuccess();
+        } else {
+            mLoadService.showCallback(EmptyCallback.class);
+        }
     }
 
     @Override
