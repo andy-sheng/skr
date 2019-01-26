6eecfc6086753ed5f53e6f8bc2020ca1f3ecdba9
chengsimin <chengsimin@xiaomi.com>
2019-01-26 16:41:48 +0800

    bug 修复 防止崩溃

--- Module/PlayWays/src/main/java-room-rank/com/module/playways/rank/room/presenter/DownLoadScoreFilePresenter.java
+++ Module/PlayWays/src/main/java-room-rank/com/module/playways/rank/room/presenter/DownLoadScoreFilePresenter.java
@@ -8,6 +8,7 @@ import com.common.utils.HttpUtils;
 import com.common.utils.SongResUtils;
 import com.common.utils.U;
 import com.module.playways.rank.prepare.model.PlayerInfoModel;
+import com.module.playways.rank.prepare.model.ResourceInfoModel;
 import com.zq.lyrics.model.UrlRes;
 import com.zq.lyrics.utils.ZipUrlResourceManager;
 
@@ -21,26 +22,28 @@ public class DownLoadScoreFilePresenter extends RxLifeCyclePresenter {
     ZipUrlResourceManager mZipUrlResourceManager;
     HttpUtils.OnDownloadProgress mOnDownloadProgress;
 
-    List<PlayerInfoModel>  mPlayerInfoModels;
+    List<PlayerInfoModel> mPlayerInfoModels;
 
-    public DownLoadScoreFilePresenter(@NotNull HttpUtils.OnDownloadProgress onDownloadProgress, List<PlayerInfoModel> playerInfoList){
+    public DownLoadScoreFilePresenter(@NotNull HttpUtils.OnDownloadProgress onDownloadProgress, List<PlayerInfoModel> playerInfoList) {
 
         mOnDownloadProgress = onDownloadProgress;
         mPlayerInfoModels = playerInfoList;
     }
 
     public void prepareRes() {
-        if(mPlayerInfoModels==null){
+        if (mPlayerInfoModels == null) {
             return;
         }
         LinkedList<UrlRes> songResList = new LinkedList<>();
         for (PlayerInfoModel playerInfo : mPlayerInfoModels) {
-            if(playerInfo.isSkrer()){
-                String midiUrl = playerInfo.getResourceInfoList().get(0).getMidiURL();
-
-                if (!TextUtils.isEmpty(midiUrl)) {
-                    UrlRes midi = new UrlRes(midiUrl, SongResUtils.getScoreDir(), U.getFileUtils().getSuffixFromUrl(midiUrl, SongResUtils.SUFF_JSON));
-                    songResList.add(midi);
+            if (playerInfo.isSkrer()) {
+                List<ResourceInfoModel> resourceInfoModelList = playerInfo.getResourceInfoList();
+                if (resourceInfoModelList.size() > 0) {
+                    String midiUrl = resourceInfoModelList.get(0).getMidiURL();
+                    if (!TextUtils.isEmpty(midiUrl)) {
+                        UrlRes midi = new UrlRes(midiUrl, SongResUtils.getScoreDir(), U.getFileUtils().getSuffixFromUrl(midiUrl, SongResUtils.SUFF_JSON));
+                        songResList.add(midi);
+                    }
                 }
             }
         }
