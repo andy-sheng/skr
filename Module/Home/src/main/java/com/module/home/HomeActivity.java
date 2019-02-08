7e6c03b3486a6d2a856647510db8c1d87da005a0
long jiang <longjiang@longdeMacBook-Pro.local>
2019-02-08 12:00:51 +0800

    修复异常
    Signed-off-by: long jiang <longjiang@longdeMacBook-Pro.local>

--- Module/Home/src/main/java/com/module/home/HomeActivity.java
+++ Module/Home/src/main/java/com/module/home/HomeActivity.java
@@ -53,6 +53,7 @@ public class HomeActivity extends BaseActivity implements IHomeActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         if (!UserAccountManager.getInstance().hasAccount()) {
             ARouter.getInstance().build(RouterConstants.ACTIVITY_LOGIN).navigation();
         }
@@ -63,7 +64,6 @@ public class HomeActivity extends BaseActivity implements IHomeActivity {
                 return;
             }
         }
-        super.onCreate(savedInstanceState);
     }
 
     @Override
