f5d6c3a3d0b8620f32d17f91e98d4ead80153320
long jiang <longjiang@longdeMacBook-Pro.local>
2019-02-11 15:45:41 +0800

    接入邀请码的接口
    Signed-off-by: long jiang <longjiang@longdeMacBook-Pro.local>

--- Module/Home/src/main/java-setting/com/module/home/setting/fragment/InviteCodeFragment.java
+++ Module/Home/src/main/java-setting/com/module/home/setting/fragment/InviteCodeFragment.java
@@ -2,21 +2,34 @@ package com.module.home.setting.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.widget.RelativeLayout;
 
+import com.alibaba.fastjson.JSON;
 import com.common.base.BaseFragment;
+import com.common.core.account.UserAccountManager;
+import com.common.core.account.UserAccountServerApi;
 import com.common.core.login.view.SeparatedEditText;
+import com.common.log.MyLog;
+import com.common.rxretrofit.ApiManager;
+import com.common.rxretrofit.ApiMethods;
+import com.common.rxretrofit.ApiObserver;
+import com.common.rxretrofit.ApiResult;
+import com.common.utils.HandlerTaskTimer;
 import com.common.utils.U;
 import com.common.view.ex.ExTextView;
 import com.common.view.ex.NoLeakEditText;
 import com.common.view.titlebar.CommonTitleBar;
 import com.jakewharton.rxbinding2.view.RxView;
 import com.module.home.R;
-import com.module.home.feedback.FeedbackFragment;
+import com.module.home.setting.InviteServerApi;
 
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.functions.Consumer;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
 
 public class InviteCodeFragment extends BaseFragment {
 
@@ -28,6 +41,12 @@ public class InviteCodeFragment extends BaseFragment {
     ExTextView mGetCodeTv;
     ExTextView mErrorHint;
     ExTextView mSubmitTv;
+
+    String mPhoneNumber; //发送验证码的电话号码
+    String mSmsCode;     //验证码
+    String mInviteCode;  //邀请码
+
+    HandlerTaskTimer mTaskTimer; // 倒计时验证码
 
     @Override
     public int initView() {
@@ -45,6 +64,19 @@ public class InviteCodeFragment extends BaseFragment {
         mErrorHint = (ExTextView) mRootView.findViewById(R.id.error_hint);
         mSubmitTv = (ExTextView) mRootView.findViewById(R.id.submit_tv);
 
+
+        mInviteCodeSpet.setTextChangedListener(new SeparatedEditText.TextChangedListener() {
+            @Override
+            public void textChanged(CharSequence changeText) {
+                mInviteCode = changeText.toString();
+            }
+
+            @Override
+            public void textCompleted(CharSequence text) {
+                mInviteCode = text.toString();
+            }
+        });
+
         RxView.clicks(mTitlebar.getLeftTextView())
                 .throttleFirst(500, TimeUnit.MILLISECONDS)
                 .subscribe(new Consumer<Object>() {
@@ -54,10 +86,190 @@ public class InviteCodeFragment extends BaseFragment {
                         U.getFragmentUtils().popFragment(InviteCodeFragment.this);
                     }
                 });
+
+        RxView.clicks(mGetCodeTv)
+                .throttleFirst(500, TimeUnit.MILLISECONDS)
+                .subscribe(new Consumer<Object>() {
+                    @Override
+                    public void accept(Object o) {
+                        mPhoneNumber = mPhoneInputTv.getText().toString().trim();
+                        if (check(mPhoneNumber, mInviteCode)) {
+                            getInviteSmsCode(mPhoneNumber, mInviteCode);
+                        }
+                    }
+                });
+
+        RxView.clicks(mSubmitTv)
+                .throttleFirst(500, TimeUnit.MILLISECONDS)
+                .subscribe(new Consumer<Object>() {
+                    @Override
+                    public void accept(Object o) {
+                        sumbitInvite();
+                    }
+                });
+
+
+        mSubmitTv.setClickable(false);
+    }
+
+    private void sumbitInvite() {
+        mPhoneNumber = mPhoneInputTv.getText().toString().trim();
+        mSmsCode = mCodeInputTv.getText().toString().trim();
+
+        if (!check(mPhoneNumber, mInviteCode)) {
+            return;
+        }
+
+        if (TextUtils.isEmpty(mSmsCode)) {
+            U.getToastUtil().showShort("验证码为空");
+            return;
+        }
+
+        InviteServerApi inviteServerApi = ApiManager.getInstance().createService(InviteServerApi.class);
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("phoneNum", mPhoneNumber);
+        map.put("smsCode", mSmsCode);
+        map.put("inviteCode", mInviteCode);
+        RequestBody body = RequestBody.create(MediaType.parse(ApiManager.APPLICATION_JSOIN), JSON.toJSONString(map));
+        ApiMethods.subscribe(inviteServerApi.submitInviteCode(body), new ApiObserver<ApiResult>() {
+            @Override
+            public void process(ApiResult result) {
+                if (result.getErrno() == 0) {
+                    boolean isSucess = result.getData().getBoolean("isSuccess");
+                    if (isSucess) {
+                        U.getKeyBoardUtils().hideSoftInputKeyBoard(getActivity());
+                        U.getToastUtil().showShort("提交邀请码成功");
+                        U.getFragmentUtils().popFragment(InviteCodeFragment.this);
+                    } else {
+                        String failedMsg = result.getData().getString("failedMsg");
+                        U.getToastUtil().showShort(failedMsg);
+                    }
+                }
+            }
+
+            @Override
+            public void onNetworkError(ErrorType errorType) {
+                super.onNetworkError(errorType);
+                U.getToastUtil().showShort("网络异常，请检查网络后重试!");
+            }
+        }, this);
+
+    }
+
+    private void getInviteSmsCode(final String phoneNumber, String inviteCode) {
+        MyLog.d(TAG, "getInviteSmsCode" + " phoneNumber=" + phoneNumber + " inviteCode=" + inviteCode);
+        if (!U.getNetworkUtils().hasNetwork()) {
+            U.getToastUtil().showShort("网络异常，请检查网络后重试!");
+            return;
+        }
+
+        InviteServerApi inviteServerApi = ApiManager.getInstance().createService(InviteServerApi.class);
+
+        long timeMs = System.currentTimeMillis();
+        String sign = U.getMD5Utils().MD5_32("skrer|invite|" +
+                String.valueOf(phoneNumber) + "|" +
+                String.valueOf(timeMs));
+
+        ApiMethods.subscribe(inviteServerApi.getInviteSmsCode(phoneNumber, String.valueOf(timeMs), sign), new ApiObserver<ApiResult>() {
+            @Override
+            public void process(ApiResult result) {
+                if (result.getErrno() == 0) {
+                    // 发送验证码成功
+                    U.getToastUtil().showShort("验证码发送成功");
+                    mGetCodeTv.setSelected(true);
+                    mGetCodeTv.setClickable(false);
+                    mCodeInputTv.setFocusable(true);
+                    mCodeInputTv.setFocusableInTouchMode(true);
+                    mCodeInputTv.requestFocus();
+                    mSubmitTv.setClickable(true);
+                    mSubmitTv.setBackgroundResource(com.common.core.R.drawable.img_btn_bg_yellow);
+                    startTimeTask();
+                } else {
+                    U.getToastUtil().showShort(result.getErrmsg());
+                }
+            }
+        }, this);
+    }
+
+    /**
+     * 更新验证码倒计时
+     */
+    public void startTimeTask() {
+        mTaskTimer = HandlerTaskTimer.newBuilder()
+                .interval(1000)
+                .take(60)
+                .start(new HandlerTaskTimer.ObserverW() {
+                    @Override
+                    public void onNext(Integer integer) {
+                        mGetCodeTv.setText("倒计时:" + (60 - integer) + "s");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        super.onComplete();
+                        mGetCodeTv.setText("获取验证码");
+                        mGetCodeTv.setSelected(false);
+                        mGetCodeTv.setClickable(true);
+                    }
+                });
+    }
+
+    public void stopTimeTask() {
+        if (mTaskTimer != null) {
+            mTaskTimer.dispose();
+        }
+    }
+
+    private boolean check(String phoneNumber, String inviteCode) {
+        if (TextUtils.isEmpty(inviteCode)) {
+            U.getToastUtil().showShort("邀请码为空");
+            return false;
+        }
+
+        if (inviteCode.length() != 6) {
+            U.getToastUtil().showShort("邀请码有误");
+            return false;
+        }
+
+        if (checkPhoneNumber(phoneNumber)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * 检查手机号是否正确
+     *
+     * @return
+     */
+    private boolean checkPhoneNumber(String phoneNumber) {
+        if (!TextUtils.isEmpty(phoneNumber) && TextUtils.isDigitsOnly(phoneNumber)
+                && phoneNumber.length() == 11 && phoneNumber.startsWith("1")) {
+            return true;
+        } else if (TextUtils.isEmpty(phoneNumber)) {
+            U.getToastUtil().showShort("手机号为空");
+        } else {
+            U.getToastUtil().showShort("手机号有误");
+        }
+
+        return false;
     }
 
     @Override
     public boolean useEventBus() {
         return false;
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        stopTimeTask();
+    }
+
+    @Override
+    protected boolean onBackPressed() {
+        stopTimeTask();
+        return super.onBackPressed();
     }
 }
