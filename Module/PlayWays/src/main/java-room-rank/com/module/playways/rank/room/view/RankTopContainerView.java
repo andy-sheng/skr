dcda31ed506adbf02a905f375f1f9d036ee1d2a3
xuzhiyong <xuzhiyong@xuzhiyongdeMacBook-Pro.local>
2019-02-20 12:47:49 +0800

    添加排位赛顶部view
    Signed-off-by: xuzhiyong <xuzhiyong@xuzhiyongdeMacBook-Pro.local>

---
+++ Module/PlayWays/src/main/java-room-rank/com/module/playways/rank/room/view/RankTopContainerView.java
@@ -0,0 +1,87 @@
+package com.module.playways.rank.room.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.common.view.ex.ExImageView;
+import com.module.playways.RoomData;
+import com.module.playways.rank.room.score.bar.EnergySlotView;
+import com.module.rank.R;
+
+public class RankTopContainerView extends RelativeLayout {
+    ExImageView mMoreBtn;
+    MoreOpView mMoreOpView;
+    ExImageView mIvLed;
+    ExImageView mIvLeft;
+    ExImageView mIvCenter;
+    ExImageView mIvRignt;
+    EnergySlotView mEnergySlotView;
+
+    TopContainerView.Listener mListener;
+    RoomData mRoomData;
+
+    public RankTopContainerView(Context context) {
+        super(context);
+        init();
+    }
+
+    public RankTopContainerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public void setListener(TopContainerView.Listener l) {
+        mListener = l;
+    }
+
+    public void setRoomData(RoomData roomData) {
+        mRoomData = roomData;
+    }
+
+    private void init() {
+        inflate(getContext(), R.layout.rank_top_container_view, this);
+        mMoreBtn = this.findViewById(R.id.more_btn);
+        mIvLed = (ExImageView) findViewById(R.id.iv_led);
+        mIvLeft = (ExImageView) findViewById(R.id.iv_left);
+        mIvCenter = (ExImageView) findViewById(R.id.iv_center);
+        mIvRignt = (ExImageView) findViewById(R.id.iv_rignt);
+        mEnergySlotView = (EnergySlotView)findViewById(R.id.energy_slot_view);
+
+        mMoreBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mMoreOpView == null) {
+                    mMoreOpView = new MoreOpView(getContext());
+                    mMoreOpView.setListener(new MoreOpView.Listener() {
+                        @Override
+                        public void onClostBtnClick() {
+                            if (mListener != null) {
+                                mListener.closeBtnClick();
+                            }
+                        }
+
+                        @Override
+                        public void onVoiceChange(boolean voiceOpen) {
+                            // 打开或者关闭声音 只是不听别人的声音
+                            if (mListener != null) {
+                                mListener.onVoiceChange(voiceOpen);
+                            }
+                        }
+                    });
+                    mMoreOpView.setRoomData(mRoomData);
+                }
+                mMoreOpView.showAt(mMoreBtn);
+            }
+        });
+    }
+
+    public void updateLight(boolean[] light){
+
+    }
+
+    public EnergySlotView getEnergySlotView(){
+        return mEnergySlotView;
+    }
+}
