b7854415e3abfa2da9c79f57ae10e60d610e262a
xuzhiyong <xuzhiyong@xuzhiyongdeMacBook-Pro.local>
2019-02-21 14:36:57 +0800

    js分享功能
    Signed-off-by: xuzhiyong <xuzhiyong@xuzhiyongdeMacBook-Pro.local>

--- commoncore/src/main/java/com/common/webview/JsBridgeImpl.java
+++ commoncore/src/main/java/com/common/webview/JsBridgeImpl.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.alibaba.fastjson.JSONObject;
 import com.common.base.BaseActivity;
 
+import com.common.core.share.SharePanel;
+import com.common.core.share.ShareType;
 import com.common.log.MyLog;
 import com.jsbridge.CallBackFunction;
 
@@ -17,23 +19,35 @@ public class JsBridgeImpl {
         mBaseActivity = baseActivity;
     }
 
-    public void share(String data, CallBackFunction function){
+    public void share(String data, CallBackFunction function) {
         MyLog.w(TAG, "share" + " data=" + data + " function=" + function);
         JSONObject jsonObject = JSONObject.parseObject(data);
 
-        if(jsonObject == null){
+        if (jsonObject == null) {
             MyLog.w(TAG, "share" + " jsonObject=null");
             return;
         }
 
-        String url = jsonObject.getString("url");
-        if(TextUtils.isEmpty(url)){
-            MyLog.w(TAG, "share" + " url=empty");
+        String type = jsonObject.getString("type");
+        if (TextUtils.isEmpty(type)) {
+            MyLog.w(TAG, "share" + " type=empty");
             return;
         }
 
-//        SharePanel sharePanel = new SharePanel(mBaseActivity);
-//        sharePanel.setShareContent(url);
-//        sharePanel.show(ShareType.URL);
+        String url = jsonObject.getString("url");
+
+        if ("image".equals(type)) {
+            String icon = jsonObject.getString("icon");
+            String des = jsonObject.getString("des");
+            String title = jsonObject.getString("title");
+
+            SharePanel sharePanel = new SharePanel(mBaseActivity);
+            sharePanel.setShareContent(icon, title, des, url);
+            sharePanel.show(ShareType.URL);
+        } else if ("url".equals(type)) {
+            SharePanel sharePanel = new SharePanel(mBaseActivity);
+            sharePanel.setShareContent(url);
+            sharePanel.show(ShareType.IMAGE_RUL);
+        }
     }
 }
