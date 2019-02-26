6865f1baee499c2f9eece58906bf8cd296bb3e3c
long jiang <longjiang@longdeMacBook-Pro.local>
2019-02-26 18:09:40 +0800

    加入语音房头像点击
    Signed-off-by: long jiang <longjiang@longdeMacBook-Pro.local>

--- Module/PlayWays/src/main/java-room-voice/com/module/playways/voice/view/VoiceUserStatusView.java
+++ Module/PlayWays/src/main/java-room-voice/com/module/playways/voice/view/VoiceUserStatusView.java
@@ -1,18 +1,24 @@
 package com.module.playways.voice.view;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.app.FragmentActivity;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.RelativeLayout;
 
 import com.common.core.avatar.AvatarUtils;
 import com.common.core.userinfo.model.UserInfoModel;
 import com.common.image.fresco.BaseImageView;
+import com.common.utils.FragmentUtils;
 import com.common.utils.U;
+import com.common.view.DebounceViewClickListener;
 import com.common.view.ex.ExImageView;
 import com.common.view.ex.ExTextView;
 import com.module.rank.R;
+import com.zq.person.fragment.OtherPersonFragment;
 
 public class VoiceUserStatusView extends RelativeLayout {
 
@@ -56,6 +62,20 @@ public class VoiceUserStatusView extends RelativeLayout {
         mAvatarIv = (BaseImageView) this.findViewById(R.id.avatar_iv);
         mMuteMicIv = (ExImageView) this.findViewById(R.id.mute_mic_iv);
         mTestTipsView = (ExTextView) this.findViewById(R.id.test_tips_view);
+
+        mAvatarIv.setOnClickListener(new DebounceViewClickListener() {
+            @Override
+            public void clickValid(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putSerializable(OtherPersonFragment.BUNDLE_USER_MODEL, mUserInfoModel);
+                U.getFragmentUtils().addFragment(FragmentUtils
+                        .newAddParamsBuilder((FragmentActivity) getContext(), OtherPersonFragment.class)
+                        .setBundle(bundle)
+                        .setAddToBackStack(true)
+                        .setHasAnimation(true)
+                        .build());
+            }
+        });
     }
 
     public void bindData(UserInfoModel model) {
