3e348b028a5c4108291e02cf54e200dca19f12e1
long jiang <longjiang@longdeMacBook-Pro.local>
2019-03-03 21:35:33 +0800

    加入导唱板子动画
    Signed-off-by: long jiang <longjiang@longdeMacBook-Pro.local>

--- Module/PlayWays/src/main/java-room-grab/com/module/playways/grab/room/view/SongInfoCardView.java
+++ Module/PlayWays/src/main/java-room-grab/com/module/playways/grab/room/view/SongInfoCardView.java
@@ -1,34 +1,26 @@
 package com.module.playways.grab.room.view;
 
-import android.animation.Animator;
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
+
 import android.content.Context;
+import android.graphics.Color;
 import android.text.TextUtils;
 import android.util.AttributeSet;
-import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
-import com.common.image.model.HttpImage;
+import com.common.image.fresco.FrescoWorker;
 import com.common.image.model.ImageFactory;
-import com.common.image.model.oss.OssImgFactory;
 import com.common.log.MyLog;
-import com.common.utils.ImageUtils;
 import com.common.utils.U;
-import com.common.view.ex.ExImageView;
 import com.common.view.ex.ExTextView;
 
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.module.playways.rank.song.model.SongModel;
 import com.module.rank.R;
-import com.opensource.svgaplayer.SVGADrawable;
-import com.opensource.svgaplayer.SVGADynamicEntity;
-import com.opensource.svgaplayer.SVGAImageView;
-import com.opensource.svgaplayer.SVGAParser;
-import com.opensource.svgaplayer.SVGAVideoEntity;
-
-import org.jetbrains.annotations.NotNull;
 
 
 /**
@@ -38,15 +30,16 @@ public class SongInfoCardView extends RelativeLayout {
 
     public final static String TAG = "SongInfoCardView";
 
-    SVGAImageView mSongCover;
-    RelativeLayout mSongInfoArea;
-
+    SimpleDraweeView mSongCoverIv;
     ExTextView mSongNameTv;
-    ExTextView mSongOwnerTv;
-    ExImageView mBaibanIv;
+    ExTextView mSongSingerTv;
+    ExTextView mSongSeqTv;
+    ExTextView mSongLyrics;
+    ImageView mGrabCd;
 
-    AnimatorSet mAnimatorSet;  // 入场动画
-    TranslateAnimation mTranslateAnimation; // 飞出的离场动画
+    RotateAnimation mRotateAnimation;    // cd的旋转
+    TranslateAnimation mEnterTranslateAnimation; // 飞入的进场动画
+    TranslateAnimation mLeaveTranslateAnimation; // 飞出的离场动画
 
     public SongInfoCardView(Context context) {
         super(context);
@@ -65,11 +58,12 @@ public class SongInfoCardView extends RelativeLayout {
 
     private void init() {
         inflate(getContext(), R.layout.grab_song_info_card_layout, this);
-        mSongCover = (SVGAImageView) findViewById(R.id.song_cover);
-        mSongInfoArea = (RelativeLayout) findViewById(R.id.song_info_area);
+        mSongCoverIv = (SimpleDraweeView) findViewById(R.id.song_cover_iv);
         mSongNameTv = (ExTextView) findViewById(R.id.song_name_tv);
-        mSongOwnerTv = (ExTextView) findViewById(R.id.song_owner_tv);
-        mBaibanIv = (ExImageView) findViewById(R.id.baiban_iv);
+        mSongSingerTv = (ExTextView) findViewById(R.id.song_singer_tv);
+        mSongSeqTv = (ExTextView) findViewById(R.id.song_seq_tv);
+        mSongLyrics = (ExTextView) findViewById(R.id.song_lyrics);
+        mGrabCd = (ImageView) findViewById(R.id.grab_cd);
     }
 
     // 该动画需要循环播放
@@ -79,111 +73,52 @@ public class SongInfoCardView extends RelativeLayout {
             return;
         }
 
+        setVisibility(VISIBLE);
+        if (!TextUtils.isEmpty(songModel.getCover())) {
+            FrescoWorker.loadImage(mSongCoverIv,
+                    ImageFactory.newHttpImage(songModel.getCover())
+                            .setCornerRadius(U.getDisplayUtils().dip2px(6))
+                            .setBorderWidth(U.getDisplayUtils().dip2px(2))
+                            .setBorderColor(Color.parseColor("#202239")).build());
+        } else {
+            FrescoWorker.loadImage(mSongCoverIv,
+                    ImageFactory.newResImage(R.drawable.xuanzegequ_wufengmian)
+                            .setCornerRadius(U.getDisplayUtils().dip2px(6))
+                            .setBorderWidth(U.getDisplayUtils().dip2px(2))
+                            .setBorderColor(Color.parseColor("#202239")).build());
+        }
+        mSongNameTv.setText("《" + songModel.getItemName() + "》");
+        mSongSingerTv.setText(songModel.getOwner());
         // 入场动画
         animationGo();
-
-        if (songModel.isIsblank()) {
-            mSongNameTv.setVisibility(GONE);
-            mSongOwnerTv.setVisibility(GONE);
-            mBaibanIv.setVisibility(VISIBLE);
-        } else {
-            mSongNameTv.setVisibility(VISIBLE);
-            mSongOwnerTv.setVisibility(VISIBLE);
-            mBaibanIv.setVisibility(GONE);
-            mSongNameTv.setText("《" + songModel.getItemName() + "》");
-            mSongOwnerTv.setText(songModel.getOwner());
-        }
-
-        mSongCover.setVisibility(VISIBLE);
-        mSongCover.setLoops(0);
-        SVGAParser parser = new SVGAParser(U.app());
-        try {
-            parser.parse("grab_record_player.svga", new SVGAParser.ParseCompletion() {
-                @Override
-                public void onComplete(@NotNull SVGAVideoEntity svgaVideoEntity) {
-                    SVGADrawable drawable = new SVGADrawable(svgaVideoEntity, requestDynamicBitmapItem(songModel.getCover()));
-                    mSongCover.setImageDrawable(drawable);
-                    mSongCover.startAnimation();
-                }
-
-                @Override
-                public void onError() {
-
-                }
-            });
-        } catch (Exception e) {
-            System.out.print(true);
-        }
     }
 
     private void animationGo() {
-        MyLog.d(TAG,"animationGo" );
-        if (mAnimatorSet == null) {
-            ObjectAnimator animator1 = ObjectAnimator.ofFloat(this, View.ALPHA, 0f, 1f);
-            ObjectAnimator animator2 = ObjectAnimator.ofFloat(this, View.SCALE_X, 0.8f, 1f);
-            ObjectAnimator animator3 = ObjectAnimator.ofFloat(this, View.SCALE_Y, 0.8f, 1f);
-            ObjectAnimator animator4 = ObjectAnimator.ofFloat(mSongInfoArea, View.TRANSLATION_Y, -U.getDisplayUtils().dip2px(50), 0f);
-            mAnimatorSet = new AnimatorSet();
-            mAnimatorSet.playTogether(animator1, animator2, animator3, animator4);
-            mAnimatorSet.setDuration(200);
+        if (mEnterTranslateAnimation == null) {
+            mEnterTranslateAnimation = new TranslateAnimation(-U.getDisplayUtils().getScreenWidth(), 0.0F, 0.0F, 0.0F);
+            mEnterTranslateAnimation.setDuration(200);
         }
-        mAnimatorSet.removeAllListeners();
-        mAnimatorSet.addListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animator) {
-                setVisibility(VISIBLE);
-            }
+        this.startAnimation(mEnterTranslateAnimation);
 
-            @Override
-            public void onAnimationEnd(Animator animator) {
-
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animator) {
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animator) {
-
-            }
-        });
-        mAnimatorSet.start();
-    }
-
-    private SVGADynamicEntity requestDynamicBitmapItem(String cover) {
-        if (TextUtils.isEmpty(cover)) {
-            return null;
+        if (mRotateAnimation == null) {
+            mRotateAnimation = new RotateAnimation(0f, 360f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+            mRotateAnimation.setDuration(3000);
+            mRotateAnimation.setRepeatCount(Animation.INFINITE);
+            mRotateAnimation.setFillAfter(true);
+            mRotateAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
         }
-//        cover = "http://song-static.inframe.mobi/cover/23bd61971ae39700b4a66a6b15bb3338.jpg";
-        HttpImage httpImage = ImageFactory.newHttpImage(cover)
-                .addOssProcessors(OssImgFactory.newResizeBuilder()
-                                .setW(ImageUtils.SIZE.SIZE_160.getW())
-                                .build()
-                        , OssImgFactory.newCircleBuilder()
-                                .setR(500)
-                                .build()
-                )
-                .build();
-        String url = httpImage.getUrl();
-        MyLog.d(TAG, "requestDynamicBitmapItem" + " url=" + url);
-        SVGADynamicEntity dynamicEntity = new SVGADynamicEntity();
-        if (!TextUtils.isEmpty(url)) {
-            dynamicEntity.setDynamicImage(url, "cover");
-        }
-        return dynamicEntity;
+        mGrabCd.startAnimation(mRotateAnimation);
     }
 
     public void hide() {
         if (this != null && this.getVisibility() == VISIBLE) {
-            if (mTranslateAnimation == null) {
-                mTranslateAnimation = new TranslateAnimation(0.0F, U.getDisplayUtils().getScreenWidth(), 0.0F, 0.0F);
-                mTranslateAnimation.setDuration(200);
+            if (mLeaveTranslateAnimation == null) {
+                mLeaveTranslateAnimation = new TranslateAnimation(0.0F, U.getDisplayUtils().getScreenWidth(), 0.0F, 0.0F);
+                mLeaveTranslateAnimation.setDuration(200);
             }
 
-            this.startAnimation(mTranslateAnimation);
-            mTranslateAnimation.setAnimationListener(new Animation.AnimationListener() {
+            this.startAnimation(mLeaveTranslateAnimation);
+            mLeaveTranslateAnimation.setAnimationListener(new Animation.AnimationListener() {
                 @Override
                 public void onAnimationStart(Animation animation) {
 
@@ -191,10 +126,10 @@ public class SongInfoCardView extends RelativeLayout {
 
                 @Override
                 public void onAnimationEnd(Animation animation) {
-                    if (mSongCover != null) {
-                        mSongCover.stopAnimation(false);
-                    }
                     clearAnimation();
+                    if (mRotateAnimation != null) {
+                        mRotateAnimation.cancel();
+                    }
                     setVisibility(GONE);
                 }
 
@@ -204,8 +139,8 @@ public class SongInfoCardView extends RelativeLayout {
                 }
             });
         } else {
-            if (mSongCover != null) {
-                mSongCover.stopAnimation(false);
+            if (mRotateAnimation != null) {
+                mRotateAnimation.cancel();
             }
             clearAnimation();
             setVisibility(GONE);
@@ -215,15 +150,16 @@ public class SongInfoCardView extends RelativeLayout {
     @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
-        if (mSongCover != null) {
-            mSongCover.setCallback(null);
-            mSongCover.stopAnimation(true);
+        if (mEnterTranslateAnimation != null) {
+            mEnterTranslateAnimation.setAnimationListener(null);
+            mEnterTranslateAnimation.cancel();
         }
-        if (mAnimatorSet != null) {
-            mAnimatorSet.cancel();
+        if (mRotateAnimation != null) {
+            mRotateAnimation.cancel();
         }
-        if (mTranslateAnimation != null) {
-            mTranslateAnimation.cancel();
+        if (mLeaveTranslateAnimation != null) {
+            mLeaveTranslateAnimation.setAnimationListener(null);
+            mLeaveTranslateAnimation.cancel();
         }
     }
 }
