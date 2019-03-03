8415012db678ebc02025a2502631b85c000ae398
xuzhiyong <xuzhiyong@skrer.net>
2019-03-03 13:40:46 +0800

    一唱到底灭灯爆灯交互完善
    Signed-off-by: xuzhiyong <xuzhiyong@skrer.net>

--- Module/PlayWays/src/main/java-room-grab/com/module/playways/grab/room/view/GrabOpView.java
+++ Module/PlayWays/src/main/java-room-grab/com/module/playways/grab/room/view/GrabOpView.java
@@ -23,6 +23,7 @@ import com.module.playways.grab.room.event.SomeOneLightBurstEvent;
 import com.module.playways.grab.room.event.SomeOneLightOffEvent;
 import com.module.rank.R;
 
+import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
@@ -302,6 +303,7 @@ public class GrabOpView extends RelativeLayout {
             }
         });
 
+        mUiHandler.removeCallbacksAndMessages(null);
         mUiHandler.removeMessages(MSG_HIDE_FROM_END_GUIDE_AUDIO);
         mUiHandler.removeMessages(MSG_SHOW_BRUST_BTN);
         Message msg = mUiHandler.obtainMessage(MSG_HIDE);
@@ -367,8 +369,8 @@ public class GrabOpView extends RelativeLayout {
                         mIvLightOff.setEnabled(true);
                         Drawable drawable = new DrawableCreator.Builder().setCornersRadius(U.getDisplayUtils().dip2px(20))
                                 .setShape(DrawableCreator.Shape.Rectangle)
-                                .setPressedDrawable(U.getDrawable(R.drawable.grab_yanchang_miedeng))
-                                .setUnPressedDrawable(U.getDrawable(R.drawable.grab_miedeng_anxia))
+                                .setPressedDrawable(U.getDrawable(R.drawable.grab_miedeng_anxia))
+                                .setUnPressedDrawable(U.getDrawable(R.drawable.grab_yanchang_miedeng))
                                 .build();
 
                         mIvLightOff.setBackground(drawable);
@@ -399,8 +401,17 @@ public class GrabOpView extends RelativeLayout {
     }
 
     @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        if(!EventBus.getDefault().isRegistered(this)){
+            EventBus.getDefault().register(this);
+        }
+    }
+
+    @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
+        EventBus.getDefault().unregister(this);
         mBtnIv.clearAnimation();
         cancelCountDownTask();
         mUiHandler.removeCallbacksAndMessages(null);
