4fcf98c654d7291c58902f196f3ad3366eb3da16
chengsimin <chengsimin@xiaomi.com>
2019-03-07 00:44:40 +0800

    音效bug

--- Module/PlayWays/src/main/java-room-grab/com/module/playways/grab/room/fragment/GrabRoomFragment.java
+++ Module/PlayWays/src/main/java-room-grab/com/module/playways/grab/room/fragment/GrabRoomFragment.java
@@ -263,7 +263,7 @@ public class GrabRoomFragment extends BaseFragment implements IGrabView, IRedPkg
         U.getSoundUtils().preLoad(TAG, R.raw.grab_challengelose, R.raw.grab_challengewin,
                 R.raw.grab_gameover, R.raw.grab_iwannasingworks,
                 R.raw.grab_nobodywants, R.raw.grab_olight,
-                R.raw.grab_readygo, R.raw.grab_xlight, R.raw.grab_lightup);
+                R.raw.grab_readygo, R.raw.grab_xlight, R.raw.grab_lightup, R.raw.normal_click);
 
         MyLog.w(TAG, "gameid 是 " + mRoomData.getGameId() + " userid 是 " + MyUserInfoManager.getInstance().getUid());
 
@@ -518,7 +518,7 @@ public class GrabRoomFragment extends BaseFragment implements IGrabView, IRedPkg
         mGrabOpBtn.setListener(new GrabOpView.Listener() {
             @Override
             public void clickGrabBtn(int seq) {
-//                U.getSoundUtils().play(TAG, R.raw.iwannasing);
+                U.getSoundUtils().play(TAG, R.raw.normal_click);
                 mCorePresenter.grabThisRound(seq);
             }
 
