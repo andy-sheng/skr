010f75507ae3de13779f1a82f00eec2a7f0dcfa2
xuzhiyong <xuzhiyong@skrer.net>
2019-03-11 16:20:11 +0800

    红包首页提示只会展示一次
    Signed-off-by: xuzhiyong <xuzhiyong@skrer.net>

--- Module/Home/src/main/java/com/module/home/persenter/RedPkgPresenter.java
+++ Module/Home/src/main/java/com/module/home/persenter/RedPkgPresenter.java
@@ -8,6 +8,7 @@ import com.common.rxretrofit.ApiManager;
 import com.common.rxretrofit.ApiMethods;
 import com.common.rxretrofit.ApiObserver;
 import com.common.rxretrofit.ApiResult;
+import com.common.utils.U;
 import com.module.home.MainPageSlideApi;
 import com.module.home.model.RedPkgTaskModel;
 import com.module.home.view.IRedPkgView;
@@ -17,10 +18,12 @@ public class RedPkgPresenter extends RxLifeCyclePresenter {
     public final static String TAG = "RedPkgPresenter";
     public final static String RED_PKG_TASK_ID = "1";
 
+    public static final String PREF_KEY_RED_PKG_SHOW = "red_pkg_show";
+
     MainPageSlideApi mMainPageSlideApi;
     IRedPkgView view;
 
-    boolean mIsHasShow = false;
+    boolean mIsHasReq = false;
 
     public RedPkgPresenter(IRedPkgView view) {
         this.view = view;
@@ -28,7 +31,7 @@ public class RedPkgPresenter extends RxLifeCyclePresenter {
     }
 
     public void checkRedPkg() {
-        if (mIsHasShow) {
+        if (mIsHasReq) {
             MyLog.d(TAG, "has checkRedPkg");
             return;
         }
@@ -38,18 +41,24 @@ public class RedPkgPresenter extends RxLifeCyclePresenter {
             return;
         }
 
+        if(U.getPreferenceUtils().getSettingBoolean(PREF_KEY_RED_PKG_SHOW, false)){
+            MyLog.w(TAG, "checkRedPkg 展示过一次就不展示了");
+            return;
+        }
+
         ApiMethods.subscribe(mMainPageSlideApi.checkRedPkg(), new ApiObserver<ApiResult>() {
             @Override
             public void process(ApiResult result) {
                 MyLog.d(TAG, "process" + " result=" + result.getErrno());
                 if (result.getErrno() == 0) {
-                    mIsHasShow = true;
+                    mIsHasReq = true;
                     List<RedPkgTaskModel> redPkgTaskModelList = JSONArray.parseArray(result.getData().getString("tasks"), RedPkgTaskModel.class);
                     if (redPkgTaskModelList != null) {
                         for (RedPkgTaskModel model :
                                 redPkgTaskModelList) {
                             if (RED_PKG_TASK_ID.equals(model.getTaskID()) && !model.isDone()) {
                                 view.showGetCashView(Float.parseFloat(model.getRedbagExtra().getCash()), model.getDeepLink());
+                                U.getPreferenceUtils().setSettingBoolean(PREF_KEY_RED_PKG_SHOW, true);
                             }
                         }
                     } else {
