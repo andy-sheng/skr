4b14a56f4ecc66c2d6742667a9d7ff2c45a91f62
long jiang <longjiang@longdeMacBook-Pro.local>
2019-03-24 17:26:06 +0800

    消息和好友页面完善
    Signed-off-by: long jiang <longjiang@longdeMacBook-Pro.local>

---
+++ Module/IMKit/src/main/java-msg/com/module/msg/friend/FriendAdapter.java
@@ -0,0 +1,91 @@
+package com.module.msg.friend;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.common.core.avatar.AvatarUtils;
+import com.common.utils.U;
+import com.common.view.DebounceViewClickListener;
+import com.common.view.ex.ExTextView;
+import com.common.view.recyclerview.DiffAdapter;
+import com.common.view.recyclerview.RecyclerOnItemClickListener;
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.zq.live.proto.Common.ESex;
+
+import io.rong.imkit.R;
+
+public class FriendAdapter extends DiffAdapter<FriendStatusModel, FriendAdapter.FriendViewHodler> {
+
+    RecyclerOnItemClickListener<FriendStatusModel> mItemClickListener;
+
+    public FriendAdapter(RecyclerOnItemClickListener<FriendStatusModel> itemClickListener) {
+        this.mItemClickListener = itemClickListener;
+    }
+
+    @NonNull
+    @Override
+    public FriendAdapter.FriendViewHodler onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.friend_item_holder, parent, false);
+        FriendViewHodler viewHolder = new FriendViewHodler(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FriendAdapter.FriendViewHodler holder, int position) {
+        FriendStatusModel friendStatusModel = mDataList.get(position);
+        holder.bindData(position, friendStatusModel);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataList.size();
+    }
+
+    class FriendViewHodler extends RecyclerView.ViewHolder {
+
+        SimpleDraweeView mAvatarIv;
+        ExTextView mNameTv;
+        ExTextView mStatusTv;
+
+        FriendStatusModel mFriendStatusModel;
+        int position;
+
+        public FriendViewHodler(View itemView) {
+            super(itemView);
+
+            mAvatarIv = (SimpleDraweeView) itemView.findViewById(R.id.avatar_iv);
+            mNameTv = (ExTextView) itemView.findViewById(R.id.name_tv);
+            mStatusTv = (ExTextView) itemView.findViewById(R.id.status_tv);
+
+            itemView.setOnClickListener(new DebounceViewClickListener() {
+                @Override
+                public void clickValid(View v) {
+                    if (mItemClickListener != null) {
+                        mItemClickListener.onItemClicked(v, position, mFriendStatusModel);
+                    }
+                }
+            });
+        }
+
+        public void bindData(int position, FriendStatusModel mFriendStatusModel) {
+            this.position = position;
+            this.mFriendStatusModel = mFriendStatusModel;
+
+            AvatarUtils.loadAvatarByUrl(mAvatarIv,
+                    AvatarUtils.newParamsBuilder(mFriendStatusModel.getAvatar())
+                            .setBorderColorBySex(mFriendStatusModel.getSex() == ESex.SX_MALE.getValue())
+                            .setBorderWidth(U.getDisplayUtils().dip2px(2))
+                            .setCircle(true)
+                            .build());
+            mNameTv.setText(mFriendStatusModel.getNickname());
+            if (mFriendStatusModel.getStatus() == FriendStatusModel.EF_OnLine) {
+                mStatusTv.setCompoundDrawablePadding(U.getDisplayUtils().dip2px(3));
+                mStatusTv.setCompoundDrawablesWithIntrinsicBounds(R.drawable.greendot, 0, 0, 0);
+            }
+            mStatusTv.setText(mFriendStatusModel.getStatusDesc());
+        }
+    }
+}
---
+++ Module/IMKit/src/main/java-msg/com/module/msg/friend/FriendFragment.java
@@ -0,0 +1,165 @@
+package com.module.msg.friend;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.alibaba.fastjson.JSON;
+import com.common.base.BaseFragment;
+import com.common.core.userinfo.UserInfoLocalApi;
+import com.common.core.userinfo.UserInfoServerApi;
+import com.common.core.userinfo.cache.BuddyCache;
+import com.common.core.userinfo.model.UserInfoModel;
+import com.common.log.MyLog;
+import com.common.rxretrofit.ApiManager;
+import com.common.rxretrofit.ApiMethods;
+import com.common.rxretrofit.ApiObserver;
+import com.common.rxretrofit.ApiResult;
+import com.common.view.recyclerview.RecyclerOnItemClickListener;
+import com.kingja.loadsir.callback.Callback;
+import com.kingja.loadsir.core.LoadService;
+import com.kingja.loadsir.core.LoadSir;
+import com.module.ModuleServiceManager;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnRefreshLoadMoreListener;
+import com.zq.relation.callback.FriendsEmptyCallback;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.schedulers.Schedulers;
+import io.rong.imkit.R;
+
+public class FriendFragment extends BaseFragment {
+
+    SmartRefreshLayout mRefreshLayout;
+    RecyclerView mContentRv;
+
+    FriendAdapter mFriendAdapter;
+
+    LoadService mLoadService;
+
+    private int mOffset = 0; // 偏移量
+    private int DEFAULT_COUNT = 15; // 每次拉去最大值
+    private boolean hasMore = true; // 是否还有数据
+
+    @Override
+    public int initView() {
+        return R.layout.friend_fragment_layout;
+    }
+
+    @Override
+    public void initData(@Nullable Bundle savedInstanceState) {
+        mRefreshLayout = (SmartRefreshLayout) mRootView.findViewById(R.id.refreshLayout);
+        mContentRv = (RecyclerView) mRootView.findViewById(R.id.content_rv);
+
+        mRefreshLayout.setEnableRefresh(false);
+        mRefreshLayout.setEnableLoadMore(true);
+        mRefreshLayout.setEnableLoadMoreWhenContentNotFull(false);
+        mRefreshLayout.setEnableOverScrollDrag(false);
+        mRefreshLayout.setOnRefreshLoadMoreListener(new OnRefreshLoadMoreListener() {
+            @Override
+            public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+                loadData(mOffset, DEFAULT_COUNT);
+            }
+
+            @Override
+            public void onRefresh(@NonNull RefreshLayout refreshLayout) {
+                mRefreshLayout.finishRefresh();
+            }
+        });
+
+        mFriendAdapter = new FriendAdapter(new RecyclerOnItemClickListener<FriendStatusModel>() {
+            @Override
+            public void onItemClicked(View view, int position, FriendStatusModel model) {
+                ModuleServiceManager.getInstance().getMsgService().startPrivateChat(getContext(),
+                        String.valueOf(model.getUserID()), model.getNickname());
+            }
+        });
+        mContentRv.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        mContentRv.setAdapter(mFriendAdapter);
+
+        LoadSir mLoadSir = new LoadSir.Builder()
+                .addCallback(new FriendsEmptyCallback())
+                .build();
+        mLoadService = mLoadSir.register(mRefreshLayout, new Callback.OnReloadListener() {
+            @Override
+            public void onReload(View v) {
+                loadData(mOffset, DEFAULT_COUNT);
+            }
+        });
+
+        loadData(mOffset, DEFAULT_COUNT);
+    }
+
+    private void loadData(int offset, int limit) {
+        UserInfoServerApi userInfoServerApi = ApiManager.getInstance().createService(UserInfoServerApi.class);
+        ApiMethods.subscribe(userInfoServerApi.getFriendStatusList(offset, limit), new ApiObserver<ApiResult>() {
+            @Override
+            public void process(ApiResult result) {
+                if (result.getErrno() == 0) {
+                    List<FriendStatusModel> list = JSON.parseArray(result.getData().getString("contacts"), FriendStatusModel.class);
+                    int offset = result.getData().getIntValue("offset");
+                    refreshView(list, offset);
+
+                    if (list != null && list.size() > 0) {
+                        updateDBAndCache(list);
+                    }
+                }
+            }
+        }, this);
+    }
+
+    private void updateDBAndCache(List<FriendStatusModel> list) {
+        List<UserInfoModel> userInfoModels = new ArrayList<>();
+        for (FriendStatusModel friendStatusModel : list) {
+            userInfoModels.add(friendStatusModel.toUserInfoModel());
+        }
+        Observable.create(new ObservableOnSubscribe<List<UserInfoModel>>() {
+            @Override
+            public void subscribe(ObservableEmitter<List<UserInfoModel>> emitter) throws Exception {
+                if (userInfoModels != null && userInfoModels.size() > 0) {
+                    UserInfoLocalApi.insertOrUpdate(userInfoModels);
+
+                    List<BuddyCache.BuddyCacheEntry> buddyCacheEntries = new ArrayList<>();
+                    for (UserInfoModel userInfoModel : userInfoModels) {
+                        buddyCacheEntries.add(new BuddyCache.BuddyCacheEntry(userInfoModel));
+                    }
+                    BuddyCache.getInstance().putBuddyList(buddyCacheEntries);
+                }
+            }
+        }).subscribeOn(Schedulers.io())
+                .subscribe();
+
+
+    }
+
+    private void refreshView(List<FriendStatusModel> list, int offset) {
+        this.mOffset = offset;
+        if (list != null && list.size() != 0) {
+            mRefreshLayout.finishLoadMore();
+            mLoadService.showSuccess();
+            mFriendAdapter.getDataList().addAll(list);
+            mFriendAdapter.notifyDataSetChanged();
+            hasMore = true;
+        } else {
+            hasMore = false;
+            mRefreshLayout.finishLoadMoreWithNoMoreData();
+            if (mOffset == 0) {
+                mLoadService.showCallback(FriendsEmptyCallback.class);
+            }
+        }
+    }
+
+    @Override
+    public boolean useEventBus() {
+        return false;
+    }
+}
