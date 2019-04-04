54a3618188b01680e0d34a81694ee60236300008
long jiang <longjiang@longdeMacBook-Pro.local>
2019-04-04 15:38:46 +0800

    修复数据库查不到的问题
    Signed-off-by: long jiang <longjiang@longdeMacBook-Pro.local>

--- commoncore/src/main/java/com/common/core/userinfo/UserInfoDB.java
+++ commoncore/src/main/java/com/common/core/userinfo/UserInfoDB.java
@@ -21,161 +21,162 @@ public class UserInfoDB implements Serializable {
     private static final long serialVersionUID = -5809782578272943999L;
 
     @Id
-    private long id;
+    private Long id;
     @NonNull
-    private long userId;
+    private Long userId;
     private String avatar;   // 头像时间戳
     private String userNickname;    // 昵称
     private String userDisplayname; // 备注
     private String letter;          // 昵称或备注的首字母
-    private long updateTime = -1;        //更新时间，水位
+    private Long updateTime;        //更新时间，水位
 
-    private int sex = -1;         // 性别
+    private Integer sex = -1;         // 性别
     private String birthday;      // 生日
     private String signature;     // 签名
     // 关系
-    private int relative = -1;  // 0,未关注  1,已关注  2,互相关注 (关注层面关系，粉丝不用管)
-    private int block = -1;  // 是否拉黑,1为拉黑 0为没有
-    private int isSystem = -1; // 是否是系统
+    private Integer relative = -1;  // 0,未关注  1,已关注  2,互相关注 (关注层面关系，粉丝不用管)
+    private Integer block = -1;  // 是否拉黑,1为拉黑 0为没有
+    private Integer isSystem = -1; // 是否是系统
 
     private String ext; //待扩展
 
-@Generated(hash = 960044444)
-public UserInfoDB(long id, long userId, String avatar, String userNickname,
-        String userDisplayname, String letter, long updateTime, int sex,
-        String birthday, String signature, int relative, int block,
-        int isSystem, String ext) {
-    this.id = id;
-    this.userId = userId;
-    this.avatar = avatar;
-    this.userNickname = userNickname;
-    this.userDisplayname = userDisplayname;
-    this.letter = letter;
-    this.updateTime = updateTime;
-    this.sex = sex;
-    this.birthday = birthday;
-    this.signature = signature;
-    this.relative = relative;
-    this.block = block;
-    this.isSystem = isSystem;
-    this.ext = ext;
-}
+    @Generated(hash = 1624474751)
+    public UserInfoDB(Long id, @NonNull Long userId, String avatar,
+                      String userNickname, String userDisplayname, String letter,
+                      Long updateTime, Integer sex, String birthday, String signature,
+                      Integer relative, Integer block, Integer isSystem, String ext) {
+        this.id = id;
+        this.userId = userId;
+        this.avatar = avatar;
+        this.userNickname = userNickname;
+        this.userDisplayname = userDisplayname;
+        this.letter = letter;
+        this.updateTime = updateTime;
+        this.sex = sex;
+        this.birthday = birthday;
+        this.signature = signature;
+        this.relative = relative;
+        this.block = block;
+        this.isSystem = isSystem;
+        this.ext = ext;
+    }
 
-@Generated(hash = 1831215638)
-public UserInfoDB() {
-}
+    @Generated(hash = 1831215638)
+    public UserInfoDB() {
+    }
 
-public long getId() {
-    return this.id;
-}
+    public Long getId() {
+        return this.id;
+    }
 
-public void setId(long id) {
-    this.id = id;
-}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-public long getUserId() {
-    return this.userId;
-}
+    public Long getUserId() {
+        return this.userId;
+    }
 
-public void setUserId(long userId) {
-    this.userId = userId;
-}
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 
-public String getAvatar() {
-    return this.avatar;
-}
+    public String getAvatar() {
+        return this.avatar;
+    }
 
-public void setAvatar(String avatar) {
-    this.avatar = avatar;
-}
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
 
-public String getUserNickname() {
-    return this.userNickname;
-}
+    public String getUserNickname() {
+        return this.userNickname;
+    }
 
-public void setUserNickname(String userNickname) {
-    this.userNickname = userNickname;
-}
+    public void setUserNickname(String userNickname) {
+        this.userNickname = userNickname;
+    }
 
-public String getUserDisplayname() {
-    return this.userDisplayname;
-}
+    public String getUserDisplayname() {
+        return this.userDisplayname;
+    }
 
-public void setUserDisplayname(String userDisplayname) {
-    this.userDisplayname = userDisplayname;
-}
+    public void setUserDisplayname(String userDisplayname) {
+        this.userDisplayname = userDisplayname;
+    }
 
-public String getLetter() {
-    return this.letter;
-}
+    public String getLetter() {
+        return this.letter;
+    }
 
-public void setLetter(String letter) {
-    this.letter = letter;
-}
+    public void setLetter(String letter) {
+        this.letter = letter;
+    }
 
-public long getUpdateTime() {
-    return this.updateTime;
-}
+    public Long getUpdateTime() {
+        return this.updateTime;
+    }
 
-public void setUpdateTime(long updateTime) {
-    this.updateTime = updateTime;
-}
+    public void setUpdateTime(Long updateTime) {
+        this.updateTime = updateTime;
+    }
 
-public int getSex() {
-    return this.sex;
-}
+    public Integer getSex() {
+        return this.sex;
+    }
 
-public void setSex(int sex) {
-    this.sex = sex;
-}
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
 
-public String getBirthday() {
-    return this.birthday;
-}
+    public String getBirthday() {
+        return this.birthday;
+    }
 
-public void setBirthday(String birthday) {
-    this.birthday = birthday;
-}
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
 
-public String getSignature() {
-    return this.signature;
-}
+    public String getSignature() {
+        return this.signature;
+    }
 
-public void setSignature(String signature) {
-    this.signature = signature;
-}
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
 
-public int getRelative() {
-    return this.relative;
-}
+    public Integer getRelative() {
+        return this.relative;
+    }
 
-public void setRelative(int relative) {
-    this.relative = relative;
-}
+    public void setRelative(Integer relative) {
+        this.relative = relative;
+    }
 
-public int getBlock() {
-    return this.block;
-}
+    public Integer getBlock() {
+        return this.block;
+    }
 
-public void setBlock(int block) {
-    this.block = block;
-}
+    public void setBlock(Integer block) {
+        this.block = block;
+    }
 
-public int getIsSystem() {
-    return this.isSystem;
-}
+    public Integer getIsSystem() {
+        return this.isSystem;
+    }
 
-public void setIsSystem(int isSystem) {
-    this.isSystem = isSystem;
-}
+    public void setIsSystem(Integer isSystem) {
+        this.isSystem = isSystem;
+    }
 
-public String getExt() {
-    return this.ext;
-}
+    public String getExt() {
+        return this.ext;
+    }
 
-public void setExt(String ext) {
-    this.ext = ext;
-}
+    public void setExt(String ext) {
+        this.ext = ext;
+    }
+
 
 }
 
