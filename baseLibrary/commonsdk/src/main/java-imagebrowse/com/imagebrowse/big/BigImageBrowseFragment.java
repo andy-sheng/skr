00c6572eb6ccd38033e78f658d8e789ca122e052
chengsimin <chengsimin@xiaomi.com>
2019-04-08 13:28:01 +0800

    看大图空间完善

--- baseLibrary/commonsdk/src/main/java-imagebrowse/com/imagebrowse/big/BigImageBrowseFragment.java
+++ baseLibrary/commonsdk/src/main/java-imagebrowse/com/imagebrowse/big/BigImageBrowseFragment.java
@@ -16,6 +16,15 @@ import com.common.utils.U;
 import com.common.view.DebounceViewClickListener;
 import com.common.view.titlebar.CommonTitleBar;
 import com.imagebrowse.ImageBrowseView;
+import com.trello.rxlifecycle2.android.FragmentEvent;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * 看大图的Fragment
@@ -28,6 +37,48 @@ public class BigImageBrowseFragment extends BaseFragment {
     ViewPager mImagesVp;
     //ArrayList<IMAGE_DATA> mDataList = new ArrayList<>();
     Loader mLoader;
+    int mLastPostion = 0;
+
+
+    PagerAdapter mPagerAdapter = new PagerAdapter() {
+
+        @NonNull
+        @Override
+        public Object instantiateItem(@NonNull ViewGroup container, int position) {
+            ImageBrowseView imageBrowseView = new ImageBrowseView(container.getContext());
+            if (mLoader != null) {
+                mLoader.load(imageBrowseView, position);
+            } else {
+                //imageBrowseView.load(data);
+            }
+            container.addView(imageBrowseView);
+            return imageBrowseView;
+        }
+
+        @Override
+        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+            container.removeView((View) object);
+        }
+
+        @Override
+        public int getCount() {
+            if (mLoader != null) {
+                return mLoader.getCount();
+            } else {
+                return 0;
+            }
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+            return view == object;
+        }
+
+        @Override
+        public int getItemPosition(Object object) {
+            return POSITION_NONE;
+        }
+    };
 
     @Override
     public int initView() {
@@ -50,49 +101,75 @@ public class BigImageBrowseFragment extends BaseFragment {
             }
         });
 
-        mImagesVp.setAdapter(new PagerAdapter() {
-
-            @NonNull
+        mImagesVp.setAdapter(mPagerAdapter);
+        mImagesVp.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
-            public Object instantiateItem(@NonNull ViewGroup container, int position) {
-                ImageBrowseView imageBrowseView = new ImageBrowseView(container.getContext());
-                if (mLoader != null) {
-                    mLoader.load(imageBrowseView, position);
-                } else {
-                    //imageBrowseView.load(data);
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position > mLastPostion) {
+                    // 往后滑动
+                    if (position >= mLoader.getCount() - 2) {
+                        if (mLoader.hasMore(true)) {
+                            // 需要往后加载更多了
+                            loadMore(true, mLoader.getCount() - 1);
+                        }
+                    }
+                } else if (position < mLastPostion) {
+                    if (position <= 1) {
+                        // 需要往后加载更多了
+                        if (mLoader.hasMore(false)) {
+                            loadMore(false, 0);
+                        }
+                    }
                 }
-                container.addView(imageBrowseView);
-                return imageBrowseView;
+                mLastPostion = position;
             }
 
             @Override
-            public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
-                container.removeView((View) object);
-            }
+            public void onPageScrollStateChanged(int state) {
 
-            @Override
-            public int getCount() {
-                if (mLoader != null) {
-                    return mLoader.getCount();
-                } else {
-                    return 0;
-                }
             }
-
-            @Override
-            public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-                return view == object;
-            }
-
-            @Override
-            public int getItemPosition(Object object) {
-                return POSITION_NONE;
-            }
-
         });
         if (mLoader == null) {
             finish();
         }
+    }
+
+    void loadMore(boolean backward, int postion) {
+        Observable.create(new ObservableOnSubscribe<Object>() {
+            @Override
+            public void subscribe(ObservableEmitter<Object> emitter) throws Exception {
+                mLoader.loadMore(backward, postion);
+                emitter.onComplete();
+            }
+        }).subscribeOn(Schedulers.io())
+                .compose(BigImageBrowseFragment.this.bindUntilEvent(FragmentEvent.DESTROY))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Object>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Object o) {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        mPagerAdapter.notifyDataSetChanged();
+                    }
+                });
     }
 
     @Override
@@ -104,6 +181,7 @@ public class BigImageBrowseFragment extends BaseFragment {
     public void destroy() {
         super.destroy();
         U.getSoundUtils().release(TAG);
+        mImagesVp.clearOnPageChangeListeners();
     }
 
     @Override
@@ -149,6 +227,16 @@ public class BigImageBrowseFragment extends BaseFragment {
             public int getCount() {
                 return 1;
             }
+
+            @Override
+            public void loadMore(boolean backward, int position) {
+
+            }
+
+            @Override
+            public boolean hasMore(boolean backward) {
+                return false;
+            }
         });
     }
 
@@ -156,5 +244,17 @@ public class BigImageBrowseFragment extends BaseFragment {
         void load(ImageBrowseView imageBrowseView, int position);
 
         int getCount();
+
+
+        /**
+         * backward 为 true，表示要向后加载更多，position 为当前最后一个元素的索引
+         * backward 为 false，表示要向前加载更多，position 为当前最前一个元素的索引
+         *
+         * @param backward
+         * @param position
+         */
+        void loadMore(boolean backward, int position);
+
+        boolean hasMore(boolean backward);
     }
 }
