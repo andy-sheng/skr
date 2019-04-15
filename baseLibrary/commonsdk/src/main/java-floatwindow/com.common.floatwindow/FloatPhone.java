afefdf41583b321b10084277b3fbada6a6549162
chengsimin <chengsimin@xiaomi.com>
2019-04-15 14:36:43 +0800

    浮动弹窗优化

--- baseLibrary/commonsdk/src/main/java-floatwindow/com.common.floatwindow/FloatPhone.java
+++ baseLibrary/commonsdk/src/main/java-floatwindow/com.common.floatwindow/FloatPhone.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.PixelFormat;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.WindowManager;
@@ -23,6 +25,22 @@ class FloatPhone extends FloatView {
     private WindowManager mWindowManager;
     private final WindowManager.LayoutParams mLayoutParams;
     private boolean isRemove = true;
+
+    private int MSG_CHECK = 1;
+    Handler mUiHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if(msg.what == MSG_CHECK){
+                if(isShow()){
+                    MyLog.d(TAG,"handleMessage 浮窗可见");
+                }else{
+                    MyLog.d(TAG,"handleMessage 浮窗不可见");
+                    attachView(true);
+                }
+            }
+        }
+    };
 
     FloatPhone(FloatWindow.B b) {
         mB = b;
@@ -51,6 +69,16 @@ class FloatPhone extends FloatView {
         boolean hasFloatWindowPermission = U.getPermissionUtils().checkFloatWindow(mB.mApplicationContext);
         MyLog.d(TAG, "init hasFloatWindowPermission=" + hasFloatWindowPermission + " reqPermissionIfNeed=" + reqPermissionIfNeed);
         if (hasFloatWindowPermission) {
+            /**
+             * 4.4以前不用判断权限可以直接使用 4.4 以前 hasFloatWindowPermission为true
+             * 在4.4到6.0之前，google没有提供方法让我们用于判断悬浮窗权限，同时也没有跳转到设置界面进行开启的方法，
+             * 因为此权限是默认开启的，但是有一些产商会修改它，所以在使用之前最好进行判断，以免使用时出现崩溃，
+             * 判断方法是用反射的方式获取出是否开启了悬浮窗权限。
+             *
+             * 在6.0以及以后的版本中，google为我们提供了判断方法和跳转界面的方法，
+             * 直接使用Settings.canDrawOverlays(context) 就可以判断是否开启了悬浮窗权限，
+             * 没有开启可以跳转到设置界面让用户开启
+             */
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 mLayoutParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
             } else {
@@ -62,58 +90,93 @@ class FloatPhone extends FloatView {
                 //TODO 这里不好加悬浮窗权限是否获得成功的回调
                 U.getPermissionUtils().requestFloatWindow(U.getActivityUtils().getTopActivity());
             } else {
-                mLayoutParams.type = WindowManager.LayoutParams.TYPE_TOAST;
-                mLayoutParams.packageName = U.app().getPackageName();
 
-                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N_MR1) {
-                    mLayoutParams.type = WindowManager.LayoutParams.TYPE_TOAST;
-                } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.N_MR1) {
-                    Context topActivityOrApp = U.getActivityUtils().getTopActivity();
-                    if (!(topActivityOrApp instanceof Activity)) {
-                        Log.e("ToastUtils", "Couldn't get top Activity.");
-                        return;
-                    }
-                    Activity topActivity = (Activity) topActivityOrApp;
-                    if (topActivity.isFinishing() || topActivity.isDestroyed()) {
-                        Log.e("ToastUtils", topActivity + " is useless");
-                        return;
-                    }
-                    mWindowManager = topActivity.getWindowManager();
-                    mLayoutParams.type = WindowManager.LayoutParams.LAST_APPLICATION_WINDOW;
-                    //U.getActivityUtils().addOnActivityDestroyedListener(topActivity, LISTENER);
-                } else {
-                    //mWM = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-                    mLayoutParams.type = WindowManager.LayoutParams.FIRST_SYSTEM_WINDOW + 37;
+                /**
+                 * mLayoutParams.type android 窗口类型
+                 *
+                 *         有3种主要类型：
+                 *
+                 *         Applicationwindows：
+                 *         取值在 FIRST_APPLICATION_WINDOW 和 LAST_APPLICATION_WINDOW 之间。  1-99
+                 *         是通常的、顶层的应用程序窗口。必须将 token 设置成 activity 的 token 。
+                 *
+                 *        Sub_windows：
+                 *         取值在 FIRST_SUB_WINDOW 和 LAST_SUB_WINDOW 之间。  1000-1999
+                 *         与顶层窗口相关联，token 必须设置为它所附着的宿主窗口的 token。
+                 *
+                 *         Systemwindows：
+                 *         取值在 FIRST_SYSTEM_WINDOW 和 LAST_SYSTEM_WINDOW 之间。 2000 - 2999
+                 *         用于特定的系统功能。它不能用于应用程序，使用时需要特殊权限。
+                 *
+                 * TYPE_STATUS_BAR=2000 状态栏
+                 * TYPE_PHONE=2002 它用来提供与用户交互的界面(特别是接电话的界面)
+                 * TYPE_SYSTEM_ALERT = 2003 低电量警告
+                 * TYPE_TOAST = 2005 短暂的通知
+                 * TYPE_SYSTEM_OVERLAY = 2006 系统用来覆盖屏幕用的
+                 *
+                 * 总得来说有个原则,type值越大则显示的越靠上层,
+                 * 上面的这些type常量都是系统中各种UI默认的使用的值
+                 *
+                 * 如果要达到你想要达到的效果甚至可以自己设置想要的int值
+                 *
+                 * 比如想要覆盖在状态栏之上,就设置个大于2001且小于2999的值就行
+                 *
+                 * 有一点要注意,api>=23之后type要是>=2000则需要一些权限才能使用
+                 *
+                 * 而且api>=23之后,要正确设置token值才能使用,要注意哦
+                 */
+
+                attachView(false);
+                mUiHandler.sendEmptyMessageDelayed(MSG_CHECK,200);
+            }
+        }
+    }
+
+    void attachView(boolean attachActivity){
+        MyLog.d(TAG,"attachView" + " attachActivity=" + attachActivity);
+        if(attachActivity) {
+            Activity topActivityOrApp = U.getActivityUtils().getTopActivity();
+            if (topActivityOrApp != null) {
+                if (!topActivityOrApp.isFinishing() && !topActivityOrApp.isDestroyed()) {
+                    mWindowManager = topActivityOrApp.getWindowManager();
                 }
+            }
+            mLayoutParams.type = WindowManager.LayoutParams.LAST_APPLICATION_WINDOW;
+        }else{
+            mLayoutParams.type = WindowManager.LayoutParams.FIRST_SYSTEM_WINDOW + 37;
+        }
+//        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.N_MR1) {
+//            mLayoutParams.type = WindowManager.LayoutParams.TYPE_PHONE;
+//        } else {
+//            //mWM = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+//
+//        }
+        final Configuration config = U.app().getResources().getConfiguration();
+        final int gravity = Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1
+                ? Gravity.getAbsoluteGravity(mB.gravity, config.getLayoutDirection())
+                : mB.gravity;
 
-                final Configuration config = U.app().getResources().getConfiguration();
-                final int gravity = Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1
-                        ? Gravity.getAbsoluteGravity(mB.gravity, config.getLayoutDirection())
-                        : mB.gravity;
+        mLayoutParams.y = mB.yOffset;
+        mLayoutParams.height = mB.mHeight;
+        mLayoutParams.width = mB.mWidth;
+        mLayoutParams.format = PixelFormat.TRANSLUCENT;
+        mLayoutParams.windowAnimations = android.R.style.Animation_Toast;
 
-                mLayoutParams.y = mB.yOffset;
-                mLayoutParams.height = mB.mHeight;
-                mLayoutParams.width = mB.mWidth;
-                mLayoutParams.format = PixelFormat.TRANSLUCENT;
-                mLayoutParams.windowAnimations = android.R.style.Animation_Toast;
-
-                mLayoutParams.setTitle("ToastWithoutNotification");
+        mLayoutParams.setTitle("ToastWithoutNotification");
 //                mLayoutParams.flags = WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
 //                        | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
 //                        | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE;
-                mLayoutParams.gravity = gravity;
-                if ((gravity & Gravity.HORIZONTAL_GRAVITY_MASK) == Gravity.FILL_HORIZONTAL) {
-                    mLayoutParams.horizontalWeight = 1.0f;
-                }
-                if ((gravity & Gravity.VERTICAL_GRAVITY_MASK) == Gravity.FILL_VERTICAL) {
-                    mLayoutParams.verticalWeight = 1.0f;
-                }
-                mLayoutParams.x = mB.xOffset;
-                mLayoutParams.packageName = U.app().getPackageName();
-
-                addView();
-            }
+        mLayoutParams.gravity = gravity;
+        if ((gravity & Gravity.HORIZONTAL_GRAVITY_MASK) == Gravity.FILL_HORIZONTAL) {
+            mLayoutParams.horizontalWeight = 1.0f;
         }
+        if ((gravity & Gravity.VERTICAL_GRAVITY_MASK) == Gravity.FILL_VERTICAL) {
+            mLayoutParams.verticalWeight = 1.0f;
+        }
+        mLayoutParams.x = mB.xOffset;
+        mLayoutParams.packageName = U.app().getPackageName();
+
+        addView();
     }
 
     public void addView() {
@@ -123,20 +186,28 @@ class FloatPhone extends FloatView {
                 mWindowManager.addView(mB.mView, mLayoutParams);
                 isRemove = false;
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             MyLog.e(TAG, e);
         }
+    }
+
+    public boolean isShow(){
+        if(mB.mView!=null && mB.mView.isShown()){
+            return true;
+        }
+        return false;
     }
 
     @Override
     public void dismiss() {
         MyLog.d(TAG, "dismiss isRemove=" + isRemove);
+        mUiHandler.removeCallbacksAndMessages(null);
         try {
             if (!isRemove) {
                 isRemove = true;
                 mWindowManager.removeView(mB.mView);
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             MyLog.e(TAG, e);
         }
 
@@ -144,7 +215,7 @@ class FloatPhone extends FloatView {
 
     @Override
     public void updateXY(int x, int y) {
-        MyLog.d(TAG,"updateXY" + " x=" + x + " y=" + y);
+        MyLog.d(TAG, "updateXY" + " x=" + x + " y=" + y);
         if (isRemove) return;
         mB.xOffset = x;
         mB.yOffset = y;
