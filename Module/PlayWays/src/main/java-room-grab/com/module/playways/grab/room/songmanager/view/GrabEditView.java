b141230f7b72e33f9cac9f294323b67fa4949400
long jiang <longjiang@longdeMacBook-Pro.local>
2019-04-18 21:38:13 +0800

    房主改房间名
    Signed-off-by: long jiang <longjiang@longdeMacBook-Pro.local>

---
+++ Module/PlayWays/src/main/java-room-grab/com/module/playways/grab/room/songmanager/view/GrabEditView.java
@@ -0,0 +1,76 @@
+package com.module.playways.grab.room.songmanager.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.common.view.DebounceViewClickListener;
+import com.common.view.ex.NoLeakEditText;
+import com.dialog.view.StrokeTextView;
+import com.module.rank.R;
+
+/**
+ * 修改房间名字
+ */
+public class GrabEditView extends RelativeLayout {
+
+    NoLeakEditText mRoomName;
+    StrokeTextView mCancelTv;
+    StrokeTextView mSaveTv;
+
+    Listener mListener;
+
+
+    public GrabEditView(Context context) {
+        super(context);
+        init();
+    }
+
+    public GrabEditView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public GrabEditView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        inflate(getContext(), R.layout.grab_edit_view_layout, this);
+        mRoomName = (NoLeakEditText) this.findViewById(R.id.room_name);
+        mCancelTv = (StrokeTextView) this.findViewById(R.id.cancel_tv);
+        mSaveTv = (StrokeTextView) this.findViewById(R.id.save_tv);
+
+        mCancelTv.setOnClickListener(new DebounceViewClickListener() {
+            @Override
+            public void clickValid(View v) {
+                if (mListener != null) {
+                    mListener.onClickCancel();
+                }
+            }
+        });
+
+        mSaveTv.setOnClickListener(new DebounceViewClickListener() {
+            @Override
+            public void clickValid(View v) {
+                String name = mRoomName.getText().toString().trim();
+                if (mListener != null) {
+                    mListener.onClickSave(name);
+                }
+            }
+        });
+    }
+
+    public void setListener(Listener listener) {
+        this.mListener = listener;
+    }
+
+    public interface Listener {
+        void onClickCancel();
+
+        void onClickSave(String roomName);
+    }
+
+}
