c132867dd91447bf70bda7369315a41e6ca70d61
long jiang <longjiang@longdeMacBook-Pro.local>
2019-04-22 19:26:46 +0800

    调整结束
    Signed-off-by: long jiang <longjiang@longdeMacBook-Pro.local>

--- Module/PlayWays/src/main/java-room-grab/com/module/playways/grab/room/view/normal/NormalRoundOverCardView.java
+++ Module/PlayWays/src/main/java-room-grab/com/module/playways/grab/room/view/normal/NormalRoundOverCardView.java
@@ -18,6 +18,7 @@ import com.opensource.svgaplayer.SVGAImageView;
 import com.opensource.svgaplayer.SVGAParser;
 import com.opensource.svgaplayer.SVGAVideoEntity;
 import com.zq.live.proto.Room.EQRoundOverReason;
+import com.zq.live.proto.Room.EQRoundResultType;
 
 
 import java.util.HashMap;
@@ -29,25 +30,11 @@ public class NormalRoundOverCardView extends RelativeLayout {
 
     public final static String TAG = "RoundOverCardView";
 
-    public final static int UNKNOW_END = 0;                // 未知原因
-    public final static int NONE_SING_END = -1;             // 无人想唱
-    public final static int SING_PERFECT_END = 1;          // 有种优秀叫一唱到底
-    public final static int SING_MOMENT_END = 2;           // 有种结束叫刚刚开始
-    public final static int SING_NO_PASS_END = 3;          // 有种悲伤叫都没及格
-    public final static int SING_PASS_END = 4;             // 有种遗憾叫明明可以
-    public final static int SING_ENOUGH_END = 5;           // 有种可惜叫觉得你行
-    public final static int SING_ABANDON_END = 6;          // 有种装x叫抢了不唱
-
-    //0未知
-    //1有种优秀叫一唱到底（全部唱完）
-    //2有种结束叫刚刚开始（t<30%）
-    //3有份悲伤叫都没及格(30%<=t <60%)
-    //4有种遗憾叫明明可以（60%<=t<90%）
-    //5有种可惜叫我觉得你行（90%<=t<=100%)
-
     SVGAImageView mSingResultSvga;
 
     SVGAListener mSVGAListener;
+
+    String assetsName = "";
 
     public NormalRoundOverCardView(Context context) {
         super(context);
@@ -72,196 +59,137 @@ public class NormalRoundOverCardView extends RelativeLayout {
     public void bindData(int songId, int reason, int resultType, SVGAListener listener) {
         this.mSVGAListener = listener;
         setVisibility(VISIBLE);
-        int mode = getRoundOver(reason, resultType);
-        switch (mode) {
-            case NONE_SING_END: {
-                LayoutParams lp = (LayoutParams) mSingResultSvga.getLayoutParams();
-                lp.height = U.getDisplayUtils().dip2px(560);
-                lp.topMargin = 0;
-                mSingResultSvga.setLayoutParams(lp);
-                startNoneSing(songId);
-            }
-            break;
-            case SING_PERFECT_END: {
-                LayoutParams lp = (LayoutParams) mSingResultSvga.getLayoutParams();
-                lp.height = U.getDisplayUtils().dip2px(180);
-                lp.topMargin = U.getDisplayUtils().dip2px(139);
-                mSingResultSvga.setLayoutParams(lp);
+
+        if (reason == EQRoundOverReason.ROR_NO_ONE_SING.getValue()) {
+            // 无人想唱
+            assetsName = "grab_none_sing_end.svga";
+            startNoneSing(songId);
+        } else if (reason == EQRoundOverReason.ROR_SELF_GIVE_UP.getValue()) {
+            // 自己放弃演唱
+            assetsName = "grab_sing_abandon_end.svga";
+            startFailed(songId);
+        } else if (reason == EQRoundOverReason.ROR_CHO_SUCCESS.getValue()) {
+            // 合唱成功
+            assetsName = "grab_chorus_sucess.svga";
+            startChorusSucess(songId);
+        } else if (reason == EQRoundOverReason.ROR_CHO_FAILED.getValue()) {
+            // 合唱失败
+            assetsName = "grab_chorus_failed.svga";
+            startChorusFailed(songId);
+        } else if (reason == EQRoundOverReason.ROR_CHO_NOT_ENOUTH_PLAYER.getValue()) {
+            // 合唱人数不够失败
+            // TODO: 2019/4/22  缺svga动画
+        } else {
+            // 放弃不用单独处理，看在哪个阶段点击放弃的
+            if (resultType == EQRoundResultType.ROT_TYPE_1.getValue()) {
+                //有种优秀叫一唱到底（全部唱完）
+                assetsName = "grab_sing_perfect_end.svga";
                 startPerfect(songId);
-            }
-            break;
-            case SING_MOMENT_END:
-            case SING_NO_PASS_END:
-            case SING_PASS_END:
-            case SING_ENOUGH_END:
-            case SING_ABANDON_END: {
-                LayoutParams lp = (LayoutParams) mSingResultSvga.getLayoutParams();
-                lp.height = U.getDisplayUtils().dip2px(180);
-                lp.topMargin = U.getDisplayUtils().dip2px(139);
-                mSingResultSvga.setLayoutParams(lp);
-                startFailed(mode, songId);
-            }
-            break;
-            default:
+            } else if (resultType == EQRoundResultType.ROT_TYPE_2.getValue()) {
+                //有种结束叫刚刚开始（t<30%）
+                assetsName = "grab_sing_moment_end.svga";
+                startFailed(songId);
+            } else if (resultType == EQRoundResultType.ROT_TYPE_3.getValue()) {
+                //有份悲伤叫都没及格(30%<=t <60%)
+                assetsName = "grab_sing_no_pass_end.svga";
+                startFailed(songId);
+            } else if (resultType == EQRoundResultType.ROT_TYPE_4.getValue()) {
+                //有种遗憾叫明明可以（60%<=t<90%）
+                assetsName = "grab_sing_pass_end.svga";
+                startFailed(songId);
+            } else if (resultType == EQRoundResultType.ROT_TYPE_5.getValue()) {
+                //有种可惜叫我觉得你行（90%<=t<=100%)
+                assetsName = "grab_sing_enough_end.svga";
+                startFailed(songId);
+            } else if (resultType == EQRoundResultType.ROT_TYPE_6.getValue()) {
+                //自己放弃演唱
+                assetsName = "grab_sing_abandon_end.svga";
+                startFailed(songId);
+            } else {
                 if (mSVGAListener != null) {
                     mSVGAListener.onFinished();
                 }
-                break;
+            }
         }
-    }
 
-    private int getRoundOver(int reason, int resultType) {
-        if (reason == EQRoundOverReason.ROR_NO_ONE_SING.getValue()) {
-            return NONE_SING_END;
-        } else if (reason == EQRoundOverReason.ROR_SELF_GIVE_UP.getValue()) {
-            return SING_ABANDON_END;
-        } else {
-            // 放弃不用单独处理，看在哪个阶段点击放弃的
-            return resultType;
-        }
     }
 
     private void startNoneSing(int songId) {
+        LayoutParams lp = (LayoutParams) mSingResultSvga.getLayoutParams();
+        lp.height = U.getDisplayUtils().dip2px(560);
+        lp.topMargin = 0;
+        mSingResultSvga.setLayoutParams(lp);
+
 //        U.getSoundUtils().play(GrabRoomFragment.TAG, R.raw.grab_nobodywants);
         HashMap map = new HashMap();
         map.put("songId2", String.valueOf(songId));
         StatisticsAdapter.recordCountEvent(UserAccountManager.getInstance().getGategory(StatConstants.CATEGORY_GRAB),
                 StatConstants.KEY_SONG_NO_ONE, map);
-        mSingResultSvga.setVisibility(VISIBLE);
-        mSingResultSvga.setLoops(1);
-            SvgaParserAdapter.parse("grab_none_sing_end.svga", new SVGAParser.ParseCompletion() {
-                @Override
-                public void onComplete( SVGAVideoEntity videoItem) {
-                    SVGADrawable drawable = new SVGADrawable(videoItem);
-                    mSingResultSvga.setImageDrawable(drawable);
-                    mSingResultSvga.startAnimation();
-                }
+        playAnimation();
+    }
 
-                @Override
-                public void onError() {
+    private void startChorusSucess(int songId){
+        LayoutParams lp = (LayoutParams) mSingResultSvga.getLayoutParams();
+        lp.height = U.getDisplayUtils().dip2px(180);
+        lp.topMargin = U.getDisplayUtils().dip2px(150);
+        mSingResultSvga.setLayoutParams(lp);
 
-                }
-            });
+        // TODO: 2019/4/22 音效和打点？？？
 
-        mSingResultSvga.setCallback(new SVGACallback() {
-            @Override
-            public void onPause() {
+        playAnimation();
+    }
 
-            }
+    private void startChorusFailed(int songId){
+        LayoutParams lp = (LayoutParams) mSingResultSvga.getLayoutParams();
+        lp.height = U.getDisplayUtils().dip2px(180);
+        lp.topMargin = U.getDisplayUtils().dip2px(139);
+        mSingResultSvga.setLayoutParams(lp);
 
-            @Override
-            public void onFinished() {
-                if (mSingResultSvga != null) {
-                    mSingResultSvga.setCallback(null);
-                    mSingResultSvga.stopAnimation(true);
-                    mSingResultSvga.setVisibility(GONE);
-                }
+        // TODO: 2019/4/22 音效和打点？？？
 
-                if (mSVGAListener != null) {
-                    mSVGAListener.onFinished();
-                }
-            }
-
-            @Override
-            public void onRepeat() {
-                if (mSingResultSvga != null && mSingResultSvga.isAnimating()) {
-                    mSingResultSvga.stopAnimation(false);
-                }
-            }
-
-            @Override
-            public void onStep(int i, double v) {
-
-            }
-        });
+        playAnimation();
     }
 
     // 优秀, 目前缺动画
     private void startPerfect(int songId) {
+        LayoutParams lp = (LayoutParams) mSingResultSvga.getLayoutParams();
+        lp.height = U.getDisplayUtils().dip2px(180);
+        lp.topMargin = U.getDisplayUtils().dip2px(139);
+        mSingResultSvga.setLayoutParams(lp);
+
         U.getSoundUtils().play(GrabRoomFragment.TAG, R.raw.grab_challengewin);
         HashMap map = new HashMap();
         map.put("song_id2", String.valueOf(songId));
         StatisticsAdapter.recordCountEvent(UserAccountManager.getInstance().getGategory(StatConstants.CATEGORY_GRAB),
                 StatConstants.KEY_SONG_SUCCESS, map);
-        mSingResultSvga.setVisibility(VISIBLE);
-        mSingResultSvga.setLoops(1);
-        SvgaParserAdapter.parse( "grab_sing_perfect_end.svga", new SVGAParser.ParseCompletion() {
-            @Override
-            public void onComplete( SVGAVideoEntity videoItem) {
-                SVGADrawable drawable = new SVGADrawable(videoItem);
-                mSingResultSvga.setImageDrawable(drawable);
-                mSingResultSvga.startAnimation();
-            }
 
-            @Override
-            public void onError() {
-
-            }
-        });
-
-        mSingResultSvga.setCallback(new SVGACallback() {
-            @Override
-            public void onPause() {
-
-            }
-
-            @Override
-            public void onFinished() {
-                if (mSingResultSvga != null) {
-                    mSingResultSvga.setCallback(null);
-                    mSingResultSvga.stopAnimation(true);
-                    mSingResultSvga.setVisibility(GONE);
-                }
-
-                if (mSVGAListener != null) {
-                    mSVGAListener.onFinished();
-                }
-            }
-
-            @Override
-            public void onRepeat() {
-                if (mSingResultSvga != null && mSingResultSvga.isAnimating()) {
-                    mSingResultSvga.stopAnimation(false);
-                }
-            }
-
-            @Override
-            public void onStep(int i, double v) {
-
-            }
-        });
+        playAnimation();
     }
 
     // 不够优秀，换字即可，目前缺动画
-    private void startFailed(int model, int songId) {
+    private void startFailed(int songId) {
+
+        LayoutParams lp = (LayoutParams) mSingResultSvga.getLayoutParams();
+        lp.height = U.getDisplayUtils().dip2px(180);
+        lp.topMargin = U.getDisplayUtils().dip2px(139);
+        mSingResultSvga.setLayoutParams(lp);
+
         U.getSoundUtils().play(GrabRoomFragment.TAG, R.raw.grab_challengelose);
         HashMap map = new HashMap();
         map.put("song_id2", String.valueOf(songId));
         StatisticsAdapter.recordCountEvent(UserAccountManager.getInstance().getGategory(StatConstants.CATEGORY_GRAB),
                 StatConstants.KEY_SONG_FAIL, map);
+
+        playAnimation();
+    }
+
+
+    private void playAnimation() {
         mSingResultSvga.setVisibility(VISIBLE);
         mSingResultSvga.setLoops(1);
-        String assetsName = "";
-        switch (model) {
-            case SING_MOMENT_END:
-                assetsName = "grab_sing_moment_end.svga";
-                break;
-            case SING_NO_PASS_END:
-                assetsName = "grab_sing_no_pass_end.svga";
-                break;
-            case SING_PASS_END:
-                assetsName = "grab_sing_pass_end.svga";
-                break;
-            case SING_ENOUGH_END:
-                assetsName = "grab_sing_enough_end.svga";
-                break;
-            case SING_ABANDON_END:
-                assetsName = "grab_sing_abandon_end.svga";
-                break;
-        }
-        SvgaParserAdapter.parse( assetsName, new SVGAParser.ParseCompletion() {
+        SvgaParserAdapter.parse(assetsName, new SVGAParser.ParseCompletion() {
             @Override
-            public void onComplete( SVGAVideoEntity videoItem) {
+            public void onComplete(SVGAVideoEntity videoItem) {
                 SVGADrawable drawable = new SVGADrawable(videoItem);
                 mSingResultSvga.setImageDrawable(drawable);
                 mSingResultSvga.startAnimation();
