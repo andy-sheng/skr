c28faff3b30c0089b22e0ddddb7f00ad27b04e0c
chengsimin <chengsimin@xiaomi.com>
2019-05-06 18:42:31 +0800

    接入bugly

--- baseLibrary/commonsdk/src/main/java/com/common/bugly/BuglyInit.java
+++ baseLibrary/commonsdk/src/main/java/com/common/bugly/BuglyInit.java
@@ -6,7 +6,11 @@ import com.tencent.bugly.crashreport.CrashReport;
 
 public class BuglyInit {
     public static void init(boolean coreProess){
+        CrashReport.setIsDevelopmentDevice(U.app(), MyLog.isDebugLogOpen());
         CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(U.app());
+        strategy.setAppChannel(U.getChannelUtils().getChannel());  //设置渠道
+//        strategy.setAppVersion(U.getAppInfoUtils().getVersionName());      //App的版本
+//        strategy.setAppPackageName(U.getAppInfoUtils().getVersionName());  //App的包名
         strategy.setUploadProcess(coreProess);
         CrashReport.initCrashReport(U.app(), "75917797f3", MyLog.isDebugLogOpen());
     }
