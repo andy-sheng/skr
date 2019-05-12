ffece17a615cdaa45c879ebac9c722ef3063cb85
chengsimin <chengsimin@xiaomi.com>
2019-05-12 16:06:55 +0800

    增加log拉取工具

--- log_cat.sh
+++ log_cat.sh
@@ -1,1 +1,26 @@
-python decode_mars_nocrypt_log_file.py $1
+if [[ $1 == "" ]]; then
+	rm -rf logs/
+	adb pull /sdcard/ZQ_LIVE/logs ./
+
+	for file in ./logs/*
+	do
+	    if test -f $file
+	    then
+	        python decode_mars_nocrypt_log_file.py $file
+	    fi
+	done
+	echo sublime ./logs/
+	sublime ./logs/
+else
+	for file in $1/*
+	do
+	    if test -f $file
+	    then
+	        python decode_mars_nocrypt_log_file.py $file
+	    fi
+	done
+	echo sublime $1
+	sublime $1
+fi
+
+echo 请使用 UTF-8 的编码，打开日志文件查看
