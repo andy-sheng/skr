44b0b113784b4606ecfb63c94f74d77af21a422e
long jiang <longjiang@longdeMacBook-Pro.local>
2019-05-15 09:57:07 +0800

    加入反馈
    Signed-off-by: long jiang <longjiang@longdeMacBook-Pro.local>

--- Module/Home/src/main/java-feedback/com.module.home.feedback/FeedbackFragment.java
+++ Module/Home/src/main/java-feedback/com.module.home.feedback/FeedbackFragment.java
@@ -10,6 +10,7 @@ import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.alibaba.fastjson.JSON;
 import com.common.base.BaseFragment;
 import com.common.base.BuildConfig;
 import com.common.core.account.UserAccountManager;
@@ -18,27 +19,41 @@ import com.common.core.share.SharePanel;
 import com.common.core.share.SharePlatform;
 import com.common.core.share.ShareType;
 import com.common.log.MyLog;
+import com.common.rxretrofit.ApiManager;
+import com.common.rxretrofit.ApiMethods;
+import com.common.rxretrofit.ApiObserver;
+import com.common.rxretrofit.ApiResult;
 import com.common.statistics.TimeStatistics;
 import com.common.utils.LogUploadUtils;
 import com.common.utils.U;
+import com.common.view.DebounceViewClickListener;
 import com.common.view.ex.ExTextView;
 import com.common.view.ex.NoLeakEditText;
 import com.common.view.titlebar.CommonTitleBar;
 import com.dialog.view.StrokeTextView;
 import com.jakewharton.rxbinding2.view.RxView;
 import com.module.home.R;
+import com.module.home.musictest.model.Answer;
 import com.xiaomi.mistatistic.sdk.MiStatInterface;
 import com.zq.toast.CommonToastView;
 
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import io.agora.rtc.RtcEngine;
 import io.reactivex.functions.Consumer;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
 
 public class FeedbackFragment extends BaseFragment {
+
+    private static int FEEDBACK_ERRO = 1;  // 反馈问题
+    private static int FEEDBACK_SUGGEST = 2; // 功能建议
 
     CommonTitleBar mTitlebar;
     RadioGroup mButtonArea;
@@ -51,6 +66,7 @@ public class FeedbackFragment extends BaseFragment {
     ProgressBar mUploadProgressBar;
 
     int mBefore;  // 记录之前的位置
+    int mType = FEEDBACK_ERRO;
 
     @Override
     public int initView() {
@@ -70,16 +86,26 @@ public class FeedbackFragment extends BaseFragment {
         mUploadProgressBar = (ProgressBar) mRootView.findViewById(R.id.upload_progress_bar);
 
 
-        RxView.clicks(mTitlebar.getLeftTextView())
-                .throttleFirst(500, TimeUnit.MILLISECONDS)
-                .subscribe(new Consumer<Object>() {
-                    @Override
-                    public void accept(Object o) {
-                        //U.getSoundUtils().play(TAG, R.raw.normal_back, 500);
-                        U.getKeyBoardUtils().hideSoftInputKeyBoard(getActivity());
-                        U.getFragmentUtils().popFragment(FeedbackFragment.this);
-                    }
-                });
+        mTitlebar.getLeftTextView().setOnClickListener(new DebounceViewClickListener() {
+            @Override
+            public void clickValid(View v) {
+                //U.getSoundUtils().play(TAG, R.raw.normal_back, 500);
+                U.getKeyBoardUtils().hideSoftInputKeyBoard(getActivity());
+                U.getFragmentUtils().popFragment(FeedbackFragment.this);
+            }
+        });
+
+        mButtonArea.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.error_back) {
+                    mType = FEEDBACK_ERRO;
+                } else if (checkedId == R.id.feed_back) {
+                    mType = FEEDBACK_SUGGEST;
+                }
+            }
+        });
+
 
         mFeedbackContent.addTextChangedListener(new TextWatcher() {
             @Override
@@ -113,11 +139,80 @@ public class FeedbackFragment extends BaseFragment {
                     public void accept(Object o) {
                         U.getKeyBoardUtils().hideSoftInputKeyBoard(getActivity());
                         mUploadProgressBar.setVisibility(View.VISIBLE);
-                        U.getLogUploadUtils().upload(MyUserInfoManager.getInstance().getUid(),null,true);
+                        if (mType == FEEDBACK_ERRO) {
+                            U.getLogUploadUtils().upload(MyUserInfoManager.getInstance().getUid(), new LogUploadUtils.Callback() {
+                                @Override
+                                public void onSuccess(String url) {
+                                    feedback(mType, url);
+                                }
+
+                                @Override
+                                public void onFailed() {
+                                    mUploadProgressBar.setVisibility(View.GONE);
+                                    U.getToastUtil().showSkrCustomShort(new CommonToastView.Builder(U.app())
+                                            .setImage(R.drawable.touxiangshezhishibai_icon)
+                                            .setText("反馈失败")
+                                            .build());
+
+                                    U.getFragmentUtils().popFragment(FeedbackFragment.this);
+                                }
+                            }, true);
+                        } else if (mType == FEEDBACK_SUGGEST) {
+                            feedback(mType, null);
+                        } else {
+                            // donothing
+                        }
                     }
                 });
 
         U.getSoundUtils().preLoad(TAG, R.raw.normal_back);
+    }
+
+    private void feedback(int type, String logUrl) {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("createAt", System.currentTimeMillis());
+        map.put("appVer", U.getAppInfoUtils().getVersionName());
+        map.put("channel", U.getChannelUtils().getChannel());
+        map.put("source", 2);
+        map.put("type", type);
+        String content = mFeedbackContent.getText().toString().trim();
+        map.put("content", content);
+        map.put("appLog", logUrl);
+
+        FeedbackServerApi feedbackServerApi = ApiManager.getInstance().createService(FeedbackServerApi.class);
+        RequestBody body = RequestBody.create(MediaType.parse(ApiManager.APPLICATION_JSON), JSON.toJSONString(map));
+        ApiMethods.subscribe(feedbackServerApi.feedback(body), new ApiObserver<ApiResult>() {
+            @Override
+            public void process(ApiResult result) {
+                if (result.getErrno() == 0) {
+                    mUploadProgressBar.setVisibility(View.GONE);
+                    U.getToastUtil().showSkrCustomShort(new CommonToastView.Builder(U.app())
+                            .setImage(R.drawable.touxiangshezhichenggong_icon)
+                            .setText("反馈成功")
+                            .build());
+
+                    U.getFragmentUtils().popFragment(FeedbackFragment.this);
+                } else {
+                    mUploadProgressBar.setVisibility(View.GONE);
+                    U.getToastUtil().showSkrCustomShort(new CommonToastView.Builder(U.app())
+                            .setImage(R.drawable.touxiangshezhishibai_icon)
+                            .setText(result.getErrmsg())
+                            .build());
+
+                    U.getFragmentUtils().popFragment(FeedbackFragment.this);
+                }
+            }
+
+            @Override
+            public void onNetworkError(ErrorType errorType) {
+                super.onNetworkError(errorType);
+                mUploadProgressBar.setVisibility(View.GONE);
+                U.getToastUtil().showSkrCustomShort(new CommonToastView.Builder(U.app())
+                        .setImage(R.drawable.touxiangshezhishibai_icon)
+                        .setText("反馈失败！\n请检查网络之后重试")
+                        .build());
+            }
+        });
     }
 
     @Override
@@ -133,13 +228,6 @@ public class FeedbackFragment extends BaseFragment {
 
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onEvent(LogUploadUtils.UploadLogEvent event) {
-        mUploadProgressBar.setVisibility(View.GONE);
-        U.getToastUtil().showSkrCustomShort(new CommonToastView.Builder(U.app())
-                .setImage(event.mIsSuccess ? R.drawable.touxiangshezhichenggong_icon : R.drawable.touxiangshezhishibai_icon)
-                .setText(event.mIsSuccess ? "反馈成功" : "反馈失败")
-                .build());
-
-        U.getFragmentUtils().popFragment(FeedbackFragment.this);
         if (event.mIsSuccess && MyLog.isDebugLogOpen()) {
             // 尝试跳到微信
             U.getToastUtil().showLong("调试模式，请通过微信将反馈分享给研发");
