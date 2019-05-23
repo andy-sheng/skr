8efbaabde6d90a58309cc9575af5a4c590f6cfc9
xuzhiyong <xuzhiyong@skrer.net>
2019-05-23 19:06:56 +0800

    ui微调
    Signed-off-by: xuzhiyong <xuzhiyong@skrer.net>

--- Component/BusiLib/src/main/java-relation/com/zq/relation/activity/RelationActivity.java
+++ Component/BusiLib/src/main/java-relation/com/zq/relation/activity/RelationActivity.java
@@ -35,6 +35,11 @@ public class RelationActivity extends BaseActivity {
     }
 
     @Override
+    public boolean resizeLayoutSelfWhenKeybordShow() {
+        return true;
+    }
+
+    @Override
     public boolean useEventBus() {
         return false;
     }
