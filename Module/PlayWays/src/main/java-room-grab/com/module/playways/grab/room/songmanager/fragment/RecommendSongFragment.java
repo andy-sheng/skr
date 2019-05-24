7ca3e6d210d48e62c0b2350572107e5c111fb083
xuzhiyong <xuzhiyong@skrer.net>
2019-05-24 17:25:03 +0800

    推荐分页加载
    Signed-off-by: xuzhiyong <xuzhiyong@skrer.net>

--- Module/PlayWays/src/main/java-room-grab/com/module/playways/grab/room/songmanager/fragment/RecommendSongFragment.java
+++ Module/PlayWays/src/main/java-room-grab/com/module/playways/grab/room/songmanager/fragment/RecommendSongFragment.java
@@ -1,6 +1,7 @@
 package com.module.playways.grab.room.songmanager.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -14,9 +15,12 @@ import com.common.rxretrofit.ApiResult;
 import com.common.utils.U;
 import com.module.playways.R;
 import com.module.playways.grab.room.GrabRoomServerApi;
-import com.module.playways.grab.room.songmanager.model.RecommendTagModel;
 import com.module.playways.grab.room.songmanager.adapter.RecommendSongAdapter;
+import com.module.playways.grab.room.songmanager.model.RecommendTagModel;
 import com.module.playways.room.song.model.SongModel;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnRefreshLoadMoreListener;
 
 import java.util.List;
 
@@ -26,8 +30,9 @@ public class RecommendSongFragment extends BaseFragment {
     private RecommendTagModel mRecommendTagModel;
     RecommendSongAdapter mRecommendSongAdapter;
     GrabRoomServerApi mGrabRoomServerApi;
-    int mTotalNum = 0;
-    int mLimit = 100;
+    SmartRefreshLayout mRefreshLayout;
+    int mOffset = 0;
+    int mLimit = 20;
 
     @Override
     public int initView() {
@@ -37,10 +42,29 @@ public class RecommendSongFragment extends BaseFragment {
     @Override
     public void initData(@Nullable Bundle savedInstanceState) {
         mRecyclerView = (RecyclerView) mRootView.findViewById(R.id.recycler_view);
+        mRefreshLayout = (SmartRefreshLayout) mRootView.findViewById(R.id.refreshLayout);
         mRecommendSongAdapter = new RecommendSongAdapter();
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         mRecyclerView.setAdapter(mRecommendSongAdapter);
         mGrabRoomServerApi = ApiManager.getInstance().createService(GrabRoomServerApi.class);
+
+        mRefreshLayout.setEnableRefresh(false);
+        mRefreshLayout.setEnableLoadMore(true);
+        mRefreshLayout.setEnableLoadMoreWhenContentNotFull(false);
+        mRefreshLayout.setEnableOverScrollDrag(false);
+
+        mRefreshLayout.setOnRefreshLoadMoreListener(new OnRefreshLoadMoreListener() {
+            @Override
+            public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+                getSongList();
+            }
+
+            @Override
+            public void onRefresh(@NonNull RefreshLayout refreshLayout) {
+
+            }
+        });
+
         getSongList();
     }
 
@@ -49,15 +73,21 @@ public class RecommendSongFragment extends BaseFragment {
     }
 
     private void getSongList() {
-        ApiMethods.subscribe(mGrabRoomServerApi.getListStandBoards(mRecommendTagModel.getType(), 0, 100), new ApiObserver<ApiResult>() {
+        ApiMethods.subscribe(mGrabRoomServerApi.getListStandBoards(mRecommendTagModel.getType(), mOffset, mLimit), new ApiObserver<ApiResult>() {
             @Override
             public void process(ApiResult result) {
+                mRefreshLayout.finishLoadMore();
+
                 if (result.getErrno() == 0) {
                     List<SongModel> recommendTagModelArrayList = JSONObject.parseArray(result.getData().getString("items"), SongModel.class);
-                    if (recommendTagModelArrayList != null) {
-                        mRecommendSongAdapter.setDataList(recommendTagModelArrayList);
+                    if (recommendTagModelArrayList == null || recommendTagModelArrayList.size() == 0) {
+                        mRefreshLayout.setEnableLoadMore(false);
+                        return;
                     }
 
+                    mRecommendSongAdapter.getDataList().addAll(recommendTagModelArrayList);
+                    mOffset = mRecommendSongAdapter.getDataList().size();
+                    mRecommendSongAdapter.notifyDataSetChanged();
                 } else {
                     U.getToastUtil().showShort(result.getErrmsg() + "");
                 }
