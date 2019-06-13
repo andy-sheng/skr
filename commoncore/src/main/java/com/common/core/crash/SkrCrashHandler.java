e77fb848c4a85e34a5494d9136bf804d3cd409fb
chengsimin <chengsimin@xiaomi.com>
2019-06-13 13:54:32 +0800

    none

--- commoncore/src/main/java/com/common/core/crash/SkrCrashHandler.java
+++ commoncore/src/main/java/com/common/core/crash/SkrCrashHandler.java
@@ -33,6 +33,7 @@ import java.text.SimpleDateFormat;
 import java.util.concurrent.TimeoutException;
 
 import io.reactivex.annotations.NonNull;
+import io.reactivex.exceptions.UndeliverableException;
 import io.reactivex.functions.Consumer;
 import io.reactivex.plugins.RxJavaPlugins;
 
@@ -63,22 +64,20 @@ public class SkrCrashHandler implements UncaughtExceptionHandler {
          * 其实没什么用，handler 已经被别的sdk注册了
          */
         Thread.setDefaultUncaughtExceptionHandler(this);
-        if (BuildConfig.DEBUG) {
-            //TODO 看的实现 RxJavaPlugins.onError
-            RxJavaPlugins.setErrorHandler(new Consumer<Throwable>() {
-                @Override
-                public void accept(Throwable throwable) throws Exception {
-                    MyLog.d(TAG, throwable);
-                    if (BuildConfig.DEBUG) {
-                        if (throwable instanceof IgnoreException || throwable.getCause() instanceof IgnoreException) {
+        //TODO 看的实现 RxJavaPlugins.onError
+        RxJavaPlugins.setErrorHandler(new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) {
+                MyLog.d(TAG, throwable);
+                if (BuildConfig.DEBUG) {
+                    if (throwable!=null && throwable.getCause() instanceof IgnoreException) {
 
-                        } else {
-                            uncaught(new Throwable("来自的rx的异常,不会导致崩溃，但要分析下原因是否合理", throwable));
-                        }
+                    } else {
+                        uncaught(new Throwable("来自的rx的异常,不会导致崩溃，但要分析下原因是否合理", throwable));
                     }
                 }
-            });
-        }
+            }
+        });
 
         try {
             if (U.getDeviceUtils().isOppo() || U.getDeviceUtils().isVivo()) {
