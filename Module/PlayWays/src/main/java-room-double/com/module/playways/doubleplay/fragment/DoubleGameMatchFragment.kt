55a634dc788b2c4f616a1603c56bc26b3ff0bb8f
xuzhiyong <xuzhiyong@skrer.net>
2019-06-24 14:10:20 +0800

    双人匹配添加取消
    Signed-off-by: xuzhiyong <xuzhiyong@skrer.net>

--- Module/PlayWays/src/main/java-room-double/com/module/playways/doubleplay/fragment/DoubleGameMatchFragment.kt
+++ Module/PlayWays/src/main/java-room-double/com/module/playways/doubleplay/fragment/DoubleGameMatchFragment.kt
@@ -1,23 +1,33 @@
 package com.module.playways.doubleplay.fragment
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import com.alibaba.android.arouter.launcher.ARouter
 import com.common.base.BaseFragment
 import com.common.core.userinfo.model.UserInfoModel
 import com.common.log.MyLog
 import com.common.notification.event.DoubleStartCombineRoomByMatchPushEvent
+import com.common.view.AnimateClickListener
+import com.common.view.DebounceViewClickListener
 import com.common.view.ex.ExImageView
+import com.common.view.ex.ExTextView
+import com.dialog.view.TipsDialogView
 import com.module.RouterConstants
+import com.module.playways.R
 import com.module.playways.doubleplay.DoubleRoomData
 import com.module.playways.doubleplay.inter.IMatchView
 import com.module.playways.doubleplay.pbLocalModel.LocalAgoraTokenInfo
 import com.module.playways.doubleplay.presenter.DoubleMatchPresenter
+import com.orhanobut.dialogplus.DialogPlus
+import com.orhanobut.dialogplus.ViewHolder
 
 
 class DoubleGameMatchFragment : BaseFragment(), IMatchView {
     val mTag = "DoubleGameMatchFragment"
     lateinit var mExImageView: ExImageView
+    lateinit var mCalcelMatchTv: ExTextView
+    var mDialogPlus: DialogPlus? = null
     lateinit var doubleMatchPresenter: DoubleMatchPresenter
 
     override fun initView(): Int {
@@ -26,10 +36,45 @@ class DoubleGameMatchFragment : BaseFragment(), IMatchView {
 
     override fun initData(savedInstanceState: Bundle?) {
         mExImageView = mRootView.findViewById<View>(com.module.playways.R.id.exImageView) as ExImageView
+        mCalcelMatchTv = mRootView.findViewById<View>(com.module.playways.R.id.calcel_match_tv) as ExTextView
+
+        mCalcelMatchTv.setOnClickListener(object : DebounceViewClickListener() {
+            override fun clickValid(v: View?) {
+                exitLogin()
+            }
+        })
 
         doubleMatchPresenter = DoubleMatchPresenter(this)
         addPresent(doubleMatchPresenter)
         doubleMatchPresenter.startMatch()
+    }
+
+    private fun exitLogin() {
+        val tipsDialogView = TipsDialogView.Builder(context)
+                .setMessageTip("确定取消匹配吗？")
+                .setConfirmTip("确定")
+                .setCancelTip("取消")
+                .setConfirmBtnClickListener(object : AnimateClickListener() {
+                    override fun click(view: View) {
+                        mDialogPlus?.dismiss()
+                        activity?.finish()
+                    }
+                })
+                .setCancelBtnClickListener(object : AnimateClickListener() {
+                    override fun click(view: View) {
+                        mDialogPlus?.dismiss()
+                    }
+                })
+                .build()
+
+        mDialogPlus = DialogPlus.newDialog(context!!)
+                .setContentHolder(ViewHolder(tipsDialogView))
+                .setGravity(Gravity.BOTTOM)
+                .setContentBackgroundResource(R.color.transparent)
+                .setOverlayBackgroundResource(R.color.black_trans_80)
+                .setExpanded(false)
+                .create()
+        mDialogPlus?.show()
     }
 
     override fun matchSuccessFromHttp(doubleRoomData: DoubleRoomData) {
@@ -67,6 +112,10 @@ class DoubleGameMatchFragment : BaseFragment(), IMatchView {
                 .navigation()
 
         activity?.finish()
+    }
+
+    override fun onBackPressed(): Boolean {
+        return true
     }
 
     override fun useEventBus(): Boolean {
