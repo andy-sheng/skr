c8152ea3884c705f058902c518fbda45ce483a23
xuzhiyong <xuzhiyong@skrer.net>
2019-06-25 19:17:09 +0800

    点歌时候房间名更改
    Signed-off-by: xuzhiyong <xuzhiyong@skrer.net>

--- Module/PlayWays/src/main/java-room-grab/com/module/playways/grab/room/songmanager/SongManageData.java
+++ Module/PlayWays/src/main/java-room-grab/com/module/playways/grab/room/songmanager/SongManageData.java
@@ -61,7 +61,7 @@ public class SongManageData implements Serializable {
         }
 
         if (mDoubleRoomData != null) {
-            return mDoubleRoomData.getConfig().getRoomSignature();
+            return "一个人的时间，两个人的空间";
         }
 
         return "";
