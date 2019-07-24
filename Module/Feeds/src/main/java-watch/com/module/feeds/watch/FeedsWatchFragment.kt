0ec0db86bee17a8b79fa55647144291189b778a5
long jiang <longjiang@longdeMacBook-Pro.local>
2019-07-24 19:22:09 +0800

    bugfix
    Signed-off-by: long jiang <longjiang@longdeMacBook-Pro.local>

--- Module/Feeds/src/main/java-watch/com/module/feeds/watch/FeedsWatchFragment.kt
+++ Module/Feeds/src/main/java-watch/com/module/feeds/watch/FeedsWatchFragment.kt
@@ -101,6 +101,11 @@ class FeedsWatchFragment : BaseFragment() {
             }
         }
 
+        mFeedVp.adapter = mTabPagerAdapter
+        mFeedTab.setViewPager(mFeedVp)
+        mTabPagerAdapter.notifyDataSetChanged()
+        mFeedVp.setCurrentItem(1, false)
+
         mFeedTab.setOnPageChangeListener(object : ViewPager.OnPageChangeListener {
             override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
 
@@ -115,12 +120,6 @@ class FeedsWatchFragment : BaseFragment() {
 
             }
         })
-
-        mFeedVp.offscreenPageLimit = 2
-        mFeedVp.adapter = mTabPagerAdapter
-        mFeedTab.setViewPager(mFeedVp)
-        mTabPagerAdapter.notifyDataSetChanged()
-        mFeedVp.setCurrentItem(1, false)
     }
 
     override fun onFragmentVisible() {
@@ -129,6 +128,9 @@ class FeedsWatchFragment : BaseFragment() {
     }
 
     fun onViewSelected(pos:Int){
+        if(!this.fragmentVisible){
+            return
+        }
         when (pos) {
             0 -> {
                 mFollowFeesView.stopPlay()
