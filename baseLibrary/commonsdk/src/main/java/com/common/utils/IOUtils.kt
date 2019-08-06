a8263325a4b629f7f0581b5f384c90855989c358
chengsimin <chengsimin@xiaomi.com>
2019-08-06 15:39:56 +0800

    none

--- baseLibrary/commonsdk/src/main/java/com/common/utils/IOUtils.kt
+++ baseLibrary/commonsdk/src/main/java/com/common/utils/IOUtils.kt
@@ -1,11 +1,15 @@
 package com.common.utils
 
+import android.text.TextUtils
 import okio.Okio
 import java.io.File
 import java.nio.charset.Charset
 
 class IOUtils {
     fun writeFile(content: String, file: File) {
+        if(TextUtils.isEmpty(content)){
+            return
+        }
         val bufferSink = Okio.buffer(Okio.sink(file))
         bufferSink.writeString(content, Charset.forName("utf-8"))
         bufferSink.close()
