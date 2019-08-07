40ce74b08f003edac3fb3b826e5e72c44be26f69
chengsimin <chengsimin@xiaomi.com>
2019-08-07 15:58:10 +0800

    add try catch

--- baseLibrary/commonsdk/src/main/java/com/common/utils/IOUtils.kt
+++ baseLibrary/commonsdk/src/main/java/com/common/utils/IOUtils.kt
@@ -1,24 +1,34 @@
 package com.common.utils
 
 import android.text.TextUtils
+import com.common.log.MyLog
 import okio.Okio
 import java.io.File
 import java.nio.charset.Charset
 
 class IOUtils {
     fun writeFile(content: String, file: File) {
-        if(TextUtils.isEmpty(content)){
+        if (TextUtils.isEmpty(content)) {
             return
         }
-        val bufferSink = Okio.buffer(Okio.sink(file))
-        bufferSink.writeString(content, Charset.forName("utf-8"))
-        bufferSink.close()
+        try {
+            val bufferSink = Okio.buffer(Okio.sink(file))
+            bufferSink.writeString(content, Charset.forName("utf-8"))
+            bufferSink.close()
+        } catch (e: Exception) {
+            MyLog.e(e)
+        }
     }
 
-    fun readFile(file: File):String {
-        val bufferSink = Okio.buffer(Okio.source(file))
-        val r = bufferSink.readUtf8()
-        bufferSink.close()
-        return r
+    fun readFile(file: File): String {
+        try {
+            val bufferSink = Okio.buffer(Okio.source(file))
+            val r = bufferSink.readUtf8()
+            bufferSink.close()
+            return r
+        } catch (e: Exception) {
+            MyLog.e(e)
+        }
+        return ""
     }
 }
