408474ab9ed579181b0a0ba538a6de525331a346
long jiang <longjiang@longdeMacBook-Pro.local>
2019-08-13 17:17:30 +0800

    去掉滑动的拖动效果
    Signed-off-by: long jiang <longjiang@longdeMacBook-Pro.local>

--- Module/Feeds/src/main/java-gen-db/com/module/feeds/db/DaoMaster.java
+++ Module/Feeds/src/main/java-gen-db/com/module/feeds/db/DaoMaster.java
@@ -21,14 +21,14 @@ public class DaoMaster extends AbstractDaoMaster {
 
     /** Creates underlying database table using DAOs. */
     public static void createAllTables(Database db, boolean ifNotExists) {
-        FeedsDraftDBDao.createTable(db, ifNotExists);
         FeedCollectDBDao.createTable(db, ifNotExists);
+        FeedsDraftDBDao.createTable(db, ifNotExists);
     }
 
     /** Drops underlying database table using DAOs. */
     public static void dropAllTables(Database db, boolean ifExists) {
-        FeedsDraftDBDao.dropTable(db, ifExists);
         FeedCollectDBDao.dropTable(db, ifExists);
+        FeedsDraftDBDao.dropTable(db, ifExists);
     }
 
     /**
@@ -47,8 +47,8 @@ public class DaoMaster extends AbstractDaoMaster {
 
     public DaoMaster(Database db) {
         super(db, SCHEMA_VERSION);
-        registerDaoClass(FeedsDraftDBDao.class);
         registerDaoClass(FeedCollectDBDao.class);
+        registerDaoClass(FeedsDraftDBDao.class);
     }
 
     public DaoSession newSession() {
