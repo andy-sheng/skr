bb02b17e2fb74613f8881f61d27bae0517ec8762
xuzhiyong <xuzhiyong@skrer.net>
2019-08-29 17:02:42 +0800

    第二个轮次开始的时候的ui问题
    Signed-off-by: xuzhiyong <xuzhiyong@skrer.net>

--- Module/PlayWays/src/main/java-room-race/com/module/playways/race/room/ui/RaceRoomFragment.kt
+++ Module/PlayWays/src/main/java-room-race/com/module/playways/race/room/ui/RaceRoomFragment.kt
@@ -454,6 +454,7 @@ class RaceRoomFragment : BaseFragment(), IRaceRoomView {
 
     override fun singBySelfSecondRound(songModel: SongModel?) {
         MyLog.d(TAG, "singBySelfSecondRound songModel = ${songModel?.toSimpleString()}")
+        hideAllSceneView()
         mRaceTopVsView.startSingBySelf {
             mRaceRightOpView.showGiveUp(false)
             mRaceSelfSingLyricView.startFly {
@@ -467,6 +468,7 @@ class RaceRoomFragment : BaseFragment(), IRaceRoomView {
 
     override fun singByOtherSecondRound(songModel: SongModel?, userModel: UserInfoModel?) {
         MyLog.d(TAG, "singByOtherSecondRound songModel = ${songModel?.toSimpleString()}, userModel = ${userModel?.toSimpleString()}")
+        hideAllSceneView()
         mRaceTopVsView.startSingByOther {
             if (mRoomData.realRoundInfo?.isSingerByUserId(MyUserInfoManager.getInstance().uid.toInt()) == true) {
                 mRaceRightOpView.visibility = View.GONE
