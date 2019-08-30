67a4c7eaaa52c54474dd7e27a2f2f151bf2d58d9
chengsimin <chengsimin@xiaomi.com>
2019-08-30 20:28:46 +0800

    none

---
+++ baseLibrary/commonsdk/src/main/java/com/common/utils/Ukt.kt
@@ -0,0 +1,9 @@
+package com.common.utils
+
+import com.common.image.fresco.FrescoWorker
+import com.common.image.model.BaseImage
+import com.common.log.MyLog
+import com.facebook.drawee.view.SimpleDraweeView
+
+fun <T> T.println() {
+}
