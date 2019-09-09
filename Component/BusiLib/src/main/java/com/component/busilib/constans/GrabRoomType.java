2a9ab58ffcd5ac66292ece0584b580017c15f4e8
chengsimin <chengsimin@xiaomi.com>
2019-09-09 19:57:06 +0800

    none

--- Component/BusiLib/src/main/java/com/component/busilib/constans/GrabRoomType.java
+++ Component/BusiLib/src/main/java/com/component/busilib/constans/GrabRoomType.java
@@ -10,4 +10,6 @@ public class GrabRoomType {
     public static final int ROOM_TYPE_PUBLIC = 3;   //公开房
 
     public static final int ROOM_TYPE_COMMON = 4;   //普通房
+
+    public static final int ROOM_TYPE_PLAYBOOK = 5;   //歌单战房间
 }
