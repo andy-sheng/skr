464889292ed4969ae4d7889c4abd642a36a0cf02
long jiang <longjiang@longdeMacBook-Pro.local>
2019-09-18 20:38:14 +0800

    加入评论的音频
    Signed-off-by: long jiang <longjiang@longdeMacBook-Pro.local>

--- Module/Posts/src/main/java/com/module/posts/view/PostsCommentAudioView.kt
+++ Module/Posts/src/main/java/com/module/posts/view/PostsCommentAudioView.kt
@@ -4,14 +4,13 @@ import android.content.Context
 import android.support.constraint.ConstraintLayout
 import android.util.AttributeSet
 import android.view.View
-import android.widget.ImageView
 import android.widget.TextView
 import com.common.view.ex.ExImageView
 import com.component.busilib.view.SpeakingTipsAnimationView
 import com.module.posts.R
 
-// 首页音频的view
-class PostsAudioView : ConstraintLayout {
+// 评论中音频的view
+class PostsCommentAudioView : ConstraintLayout {
 
     constructor(context: Context) : super(context) {}
 
@@ -20,23 +19,21 @@ class PostsAudioView : ConstraintLayout {
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
 
     private val audioBg: ExImageView
-    private val audioPlay: ImageView
     private val speakerAnimationIv: SpeakingTipsAnimationView
     private val durationTv: TextView
 
     var isPlaying = false
 
     init {
-        View.inflate(context, R.layout.post_audio_view_layout, this)
+        View.inflate(context, R.layout.post_comment_audio_view_layout, this)
 
         audioBg = this.findViewById(R.id.audio_bg)
-        audioPlay = this.findViewById(R.id.audio_play)
         speakerAnimationIv = this.findViewById(R.id.speaker_animation_iv)
         durationTv = this.findViewById(R.id.duration_tv)
     }
 
-    private fun binData(){
-        // todo
+    private fun bindData(){
+        
     }
 
     fun setPlay(isPlay: Boolean) {
