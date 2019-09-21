07b0345aaaf483284303111925498509586ebe3d
long jiang <longjiang@longdeMacBook-Pro.local>
2019-09-21 15:33:16 +0800

    红吧结构
    Signed-off-by: long jiang <longjiang@longdeMacBook-Pro.local>

--- Module/Posts/src/main/java-watch/com/module/posts/watch/model/PostsRedPkgModel.kt
+++ Module/Posts/src/main/java-watch/com/module/posts/watch/model/PostsRedPkgModel.kt
@@ -15,9 +15,24 @@ class PostsRedPkgModel : Serializable {
     var openStatus: Int = 0    // 红包的状态
     @JSONField(name = "redpacketID")
     var redpacketID: Long = 0L
+    @JSONField(name = "redpacketDesc")
+    var redpacketDesc: String = ""  // 金额描述
+    @JSONField(name = "redpacketNum")
+    var redpacketNum: Int = 0
+    @JSONField(name = "redpacketType")
+    var redpacketType: Int = 0    // 0未知 1金币红包 2钻石红包
+    @JSONField(name = "resTimeMs")
+    var resTimeMs: Long = 0    // 剩余时间
+    @JSONField(name = "status")
+    var status: Int = 0   // 0未知 1未审核 2有效期内 3已过期未领取完 4被领取完
 
     override fun toString(): String {
         return "PostsRedPkgModel(openStatus=$openStatus, redpacketID=$redpacketID)"
     }
+
+
+
+
+
 
 }
