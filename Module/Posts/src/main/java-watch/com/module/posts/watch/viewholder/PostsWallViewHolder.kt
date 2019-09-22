7339a60eb21bbe8a58560cc2e2f576b7cc35e244
long jiang <longjiang@longdeMacBook-Pro.local>
2019-09-22 16:18:11 +0800

    个人中心完成
    Signed-off-by: long jiang <longjiang@longdeMacBook-Pro.local>

--- Module/Posts/src/main/java-watch/com/module/posts/watch/viewholder/PostsWallViewHolder.kt
+++ Module/Posts/src/main/java-watch/com/module/posts/watch/viewholder/PostsWallViewHolder.kt
@@ -2,36 +2,25 @@ package com.module.posts.watch.viewholder
 
 import android.view.View
 import android.widget.TextView
-import com.common.core.view.setDebounceViewClickListener
-import com.common.log.MyLog
-import com.common.utils.U
-import com.component.busilib.view.AvatarView
 import com.module.posts.R
 import com.module.posts.watch.adapter.PostsWatchListener
+import com.module.posts.watch.model.PostsModel
 import com.module.posts.watch.model.PostsWatchModel
 
-class PostsWallViewHolder (item: View, listener: PostsWatchListener) : PostsViewHolder(item, listener){
+class PostsWallViewHolder(item: View, listener: PostsWatchListener) : PostsViewHolder(item, listener) {
 
-    val avatarIv: AvatarView = item.findViewById(R.id.avatar_iv)
-    val nicknameTv: TextView = item.findViewById(R.id.nickname_tv)
-    val timeTv: TextView = item.findViewById(R.id.time_tv)
-
-    init {
-        avatarIv.setDebounceViewClickListener { listener.onClickCommentAvatar(pos, mModel) }
-    }
+    val unauditTv: TextView = item.findViewById(R.id.unaudit_tv)
 
     override fun bindData(pos: Int, model: PostsWatchModel) {
         super.bindData(pos, model)
 
-        if (mModel?.user != null) {
-            avatarIv.bindData(mModel?.user!!)
-            nicknameTv.text = mModel?.user?.nicknameRemark
+        if (model.posts?.status == PostsModel.EPS_AUDIT_ACCEPT) {
+            // 审核成功
+            unauditTv.visibility = View.GONE
         } else {
-            MyLog.e("PostsWatchViewHolder", "bindData error pos = $pos, model = $model")
+            // 审核失败和未审核都算审核中
+            unauditTv.visibility = View.VISIBLE
         }
 
-        mModel?.posts?.let {
-            timeTv.text = U.getDateTimeUtils().formatHumanableDateForSkrFeed(it.createdAt, System.currentTimeMillis())
-        }
     }
 }
