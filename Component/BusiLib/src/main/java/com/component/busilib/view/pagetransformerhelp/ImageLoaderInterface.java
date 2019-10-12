13c1f902c717e3ffa2434c5da07d86ffb6370f02
xuzhiyong <xuzhiyong@skrer.net>
2019-10-12 14:20:02 +0800

    各种ViewPager滑动的效果添加
    Signed-off-by: xuzhiyong <xuzhiyong@skrer.net>

---
+++ Component/BusiLib/src/main/java/com/component/busilib/view/pagetransformerhelp/ImageLoaderInterface.java
@@ -0,0 +1,16 @@
+package com.component.busilib.view.pagetransformerhelp;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+/*******************************************************************
+ *    * * * *   * * * *   *     *       Created by OCN.Yang
+ *    *     *   *         * *   *       Time:2017/12/6 15:09.
+ *    *     *   *         *   * *       Email address:ocnyang@gmail.com
+ *    * * * *   * * * *   *     *.Yang  Web site:www.ocnyang.com
+ *******************************************************************/
+
+
+public interface ImageLoaderInterface {
+    void displayImage(Context context, Object imgPath, ImageView imageView);
+}
