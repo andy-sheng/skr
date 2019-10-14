2351e3b3d07f5702deed6da5634ca2af29cf5627
xuzhiyong <xuzhiyong@skrer.net>
2019-10-14 21:09:10 +0800

    投票添加动画
    Signed-off-by: xuzhiyong <xuzhiyong@skrer.net>

--- Module/PlayWays/src/main/java-room-race/com/module/playways/race/room/view/RaceRightOpView.kt
+++ Module/PlayWays/src/main/java-room-race/com/module/playways/race/room/view/RaceRightOpView.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.support.constraint.ConstraintLayout
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.ScaleAnimation
 import com.common.utils.U
 import com.common.view.DebounceViewClickListener
 import com.common.view.ex.ExImageView
@@ -22,14 +24,18 @@ class RaceRightOpView : ConstraintLayout {
 
     val voteIv: ExImageView
     val giveUpIv: ExImageView
+    val giveUpIvBg: ExImageView
 
     var mListener: RightOpListener? = null
+
+    var mScaleAnimation: ScaleAnimation? = null
 
     init {
         View.inflate(context, R.layout.race_right_op_view, this)
 
         voteIv = this.findViewById(R.id.vote_iv)
         giveUpIv = this.findViewById(R.id.give_up_iv)
+        giveUpIvBg = this.findViewById(R.id.vote_bg)
 
         voteIv.setOnClickListener(object : DebounceViewClickListener() {
             override fun clickValid(v: View?) {
@@ -61,8 +67,43 @@ class RaceRightOpView : ConstraintLayout {
         visibility = View.VISIBLE
         giveUpIv.visibility = View.GONE
         voteIv.visibility = View.VISIBLE
-        voteIv.isSelected = isSelected
-        voteIv.isClickable = !isSelected
+
+
+        if (isSelected) {
+            playBgAnim {
+                giveUpIvBg.visibility = View.INVISIBLE
+                voteIv.isSelected = isSelected
+                voteIv.isClickable = !isSelected
+            }
+        } else {
+            voteIv.isSelected = isSelected
+            voteIv.isClickable = !isSelected
+        }
+    }
+
+    fun playBgAnim(call: () -> Unit) {
+        giveUpIvBg.clearAnimation()
+        giveUpIvBg.visibility = View.VISIBLE
+        if (mScaleAnimation == null) {
+            mScaleAnimation = ScaleAnimation(0.8f, 1.3f, 0.8f, 1.3f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+            mScaleAnimation?.setDuration(300)
+        }
+
+        mScaleAnimation?.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                call.invoke()
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+        })
+
+        giveUpIvBg.startAnimation(mScaleAnimation)
     }
 
     fun showGiveUp(isSelected: Boolean) {
