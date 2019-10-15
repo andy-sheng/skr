8afb746ff8e91f03fb0c34dcf2a2d9de1d4cde8a
chengsimin <chengsimin@xiaomi.com>
2019-10-15 15:23:40 +0800

    bug fix

--- baseLibrary/arcCloud/src/main/java/com/engine/arccloud/RecognizeConfig.java
+++ baseLibrary/arcCloud/src/main/java/com/engine/arccloud/RecognizeConfig.java
@@ -31,7 +31,7 @@ public class RecognizeConfig {
     public void setSongName(String songName) {
         // 把不对的歌名处理下
         if (songName.contains("-清唱版")) {
-            songName = songName.replace("-清唱版", "");
+            songName = songName.replaceAll("-清唱版", "");
         }
         this.songName = songName;
     }
