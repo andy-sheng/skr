81f11b0743943dbe9b70b699f400dd4f2a3967f5
chengsimin <chengsimin@xiaomi.com>
2019-10-22 17:10:20 +0800

    step2

--- upload_to_local_maven.sh
+++ upload_to_local_maven.sh
@@ -1,2 +1,8 @@
 ./gradlew :baseLibrary:android-gif-drawable:uploadArchives
+./gradlew :baseLibrary:commonsdk:uploadArchives
+./gradlew :baseLibrary:doraemonkit:uploadArchives
+./gradlew :baseLibrary:arcCloud:uploadArchives
+./gradlew :baseLibrary:effectsdk:uploadArchives
+./gradlew :baseLibrary:enginesdk:uploadArchives
+./gradlew :baseLibrary:mediaengine:uploadArchives
 ./gradlew :commoncore:uploadArchives
