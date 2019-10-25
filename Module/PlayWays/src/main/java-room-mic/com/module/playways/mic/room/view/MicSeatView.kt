93825e16d8b182910a4272dbbc378aa13b8ef055
xuzhiyong <xuzhiyong@skrer.net>
2019-10-25 21:10:46 +0800

    麦席相关代码
    Signed-off-by: xuzhiyong <xuzhiyong@skrer.net>

--- Module/PlayWays/src/main/java-room-mic/com/module/playways/mic/room/view/MicSeatView.kt
+++ Module/PlayWays/src/main/java-room-mic/com/module/playways/mic/room/view/MicSeatView.kt
@@ -2,12 +2,20 @@ package com.module.playways.mic.room.view
 
 import android.content.Context
 import android.support.constraint.ConstraintLayout
+import android.support.v7.widget.RecyclerView
 import android.util.AttributeSet
+import android.view.View
+import com.common.core.view.setDebounceViewClickListener
+import com.common.view.ex.ExImageView
+import com.module.playways.R
 
 // 右边操作区域，投票
 class MicSeatView : ConstraintLayout {
 
     val TAG = "MicSeatView"
+
+    var bg: ExImageView
+    var recyclerView: RecyclerView
 
     constructor(context: Context) : super(context) {}
 
@@ -15,5 +23,21 @@ class MicSeatView : ConstraintLayout {
 
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
 
+    init {
+        View.inflate(context, R.layout.mic_seat_view_layout, this)
+        bg = rootView.findViewById(R.id.bg)
+        recyclerView = rootView.findViewById(R.id.recycler_view)
 
+        setDebounceViewClickListener {
+            hide()
+        }
+
+        bg.setDebounceViewClickListener {
+            //拦截
+        }
+    }
+
+    private fun hide() {
+
+    }
 }
