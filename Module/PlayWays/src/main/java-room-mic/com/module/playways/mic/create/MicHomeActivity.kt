a57ec720bfa6052ec5525d276b85fc28b96b2cf2
long jiang <longjiang@longdeMacBook-Pro.local>
2019-10-27 19:00:25 +0800

    加入新的入口
    Signed-off-by: long jiang <longjiang@longdeMacBook-Pro.local>

---
+++ Module/PlayWays/src/main/java-room-mic/com/module/playways/mic/create/MicHomeActivity.kt
@@ -0,0 +1,42 @@
+package com.module.playways.mic.create
+
+import android.os.Bundle
+import android.widget.ImageView
+import com.alibaba.android.arouter.facade.annotation.Route
+import com.alibaba.android.arouter.launcher.ARouter
+import com.common.base.BaseActivity
+import com.common.core.view.setAnimateDebounceViewClickListener
+import com.common.core.view.setDebounceViewClickListener
+import com.common.view.titlebar.CommonTitleBar
+import com.module.RouterConstants
+import com.module.playways.R
+
+@Route(path = RouterConstants.ACTIVITY_MIC_HOME)
+class MicHomeActivity : BaseActivity() {
+
+    lateinit var titlebar: CommonTitleBar
+    lateinit var quickBegin: ImageView
+    lateinit var createRoom: ImageView
+
+    override fun initView(savedInstanceState: Bundle?): Int {
+        return R.layout.mic_home_activity_layout
+    }
+
+    override fun initData(savedInstanceState: Bundle?) {
+        titlebar = findViewById(R.id.titlebar)
+        quickBegin = findViewById(R.id.quick_begin)
+        createRoom = findViewById(R.id.create_room)
+
+        titlebar.leftTextView.setDebounceViewClickListener { finish() }
+        quickBegin.setAnimateDebounceViewClickListener {
+            // todo 要去匹配
+        }
+        createRoom.setAnimateDebounceViewClickListener {
+            ARouter.getInstance().build(RouterConstants.ACTIVITY_CREATE_MIC_ROOM)
+                    .navigation()}
+    }
+
+    override fun useEventBus(): Boolean {
+        return false
+    }
+}
