b8af16ee05f840e4dec1165743444f5a7ab62fff
xuzhiyong <xuzhiyong@skrer.net>
2019-10-28 17:01:46 +0800

    麦席使用真数据
    Signed-off-by: xuzhiyong <xuzhiyong@skrer.net>

--- Module/PlayWays/src/main/java-room-mic/com/module/playways/mic/room/model/MicSeatModel.kt
+++ Module/PlayWays/src/main/java-room-mic/com/module/playways/mic/room/model/MicSeatModel.kt
@@ -1,16 +1,17 @@
 package com.module.playways.mic.room.model
 
 import com.alibaba.fastjson.annotation.JSONField
-import com.common.core.userinfo.model.UserInfoModel
 import java.io.Serializable
 
-class MicSeatModel() : Serializable {
+class MicSeatModel : Serializable {
     @JSONField(name = "music")
     var music: List<SeatMusicInfo>? = null
     @JSONField(name = "user")
-    var user: UserInfoModel? = null
+    var user: MicPlayerInfoModel? = null
 
-    class SeatMusicInfo() : Serializable {
+    class SeatState
+
+    class SeatMusicInfo : Serializable {
         @JSONField(name = "itemID")
         var itemID: Int? = null
         @JSONField(name = "itemName")
