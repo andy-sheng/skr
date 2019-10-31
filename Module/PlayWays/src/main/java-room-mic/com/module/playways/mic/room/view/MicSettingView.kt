49740a4ed44008d31b4f83e9c3264a7f6750e5ad
xuzhiyong <xuzhiyong@skrer.net>
2019-10-31 17:39:54 +0800

    继承房间属性
    Signed-off-by: xuzhiyong <xuzhiyong@skrer.net>

--- Module/PlayWays/src/main/java-room-mic/com/module/playways/mic/room/view/MicSettingView.kt
+++ Module/PlayWays/src/main/java-room-mic/com/module/playways/mic/room/view/MicSettingView.kt
@@ -5,26 +5,15 @@ import android.util.AttributeSet
 import android.view.Gravity
 import android.view.View
 import android.widget.CompoundButton
-import com.alibaba.fastjson.JSON
 import com.common.rxretrofit.ApiManager
-import com.common.rxretrofit.ControlType
-import com.common.rxretrofit.RequestControl
-import com.common.rxretrofit.subscribe
 import com.common.utils.U
 import com.common.view.ex.ExConstraintLayout
-import com.component.busilib.constans.GameModeType
 import com.kyleduo.switchbutton.SwitchButton
 import com.module.playways.R
 import com.module.playways.mic.room.MicRoomData
 import com.module.playways.mic.room.MicRoomServerApi
-import com.module.playways.room.room.comment.model.CommentSysModel
-import com.module.playways.room.room.event.PretendCommentMsgEvent
 import com.orhanobut.dialogplus.DialogPlus
 import com.orhanobut.dialogplus.ViewHolder
-import kotlinx.coroutines.launch
-import okhttp3.MediaType
-import okhttp3.RequestBody
-import org.greenrobot.eventbus.EventBus
 
 // 右边操作区域，投票
 class MicSettingView : ExConstraintLayout {
@@ -38,8 +27,6 @@ class MicSettingView : ExConstraintLayout {
 
     var mRoomData: MicRoomData? = null
 
-    internal var mRoomServerApi = ApiManager.getInstance().createService(MicRoomServerApi::class.java)
-
     var callUpdate: ((Boolean) -> Unit)? = null
 
     constructor(context: Context) : super(context) {}
@@ -52,12 +39,6 @@ class MicSettingView : ExConstraintLayout {
     init {
         View.inflate(context, R.layout.mic_setting_view_layout, this)
         mSbAcc = findViewById(R.id.sb_acc)
-
-        mSbAcc?.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
-            //EMMS_UNKNOWN = 0 : 未知 - EMMS_OPEN = 1 : match 打开 - EMMS_CLOSED = 2 : match 关闭
-            callUpdate?.invoke(isChecked)
-
-        })
 
         setOnClickListener {
             //拦截
@@ -80,6 +61,13 @@ class MicSettingView : ExConstraintLayout {
                 .create()
         mDialogPlus?.show()
         layoutParams.height = U.getDisplayUtils().dip2px(180f)
+
+        mSbAcc?.setOnCheckedChangeListener(null)
+        mSbAcc?.isChecked = !mRoomData?.matchStatusOpen!!
+        mSbAcc?.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+            //EMMS_UNKNOWN = 0 : 未知 - EMMS_OPEN = 1 : match 打开 - EMMS_CLOSED = 2 : match 关闭
+            callUpdate?.invoke(isChecked)
+        })
     }
 
     fun dismiss() {
