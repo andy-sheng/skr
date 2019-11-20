a26f2febd4cfbe420b889a098c569f8170e8457d
xuzhiyong <xuzhiyong@skrer.net>
2019-11-20 16:14:06 +0800

    添加查看效果相关
    Signed-off-by: xuzhiyong <xuzhiyong@skrer.net>

--- Module/Home/src/main/java-mall/com.module.mall/view/EffectView.kt
+++ Module/Home/src/main/java-mall/com.module.mall/view/EffectView.kt
@@ -4,14 +4,39 @@ import android.content.Context
 import android.support.constraint.ConstraintLayout
 import android.util.AttributeSet
 import android.view.View
+import com.common.core.avatar.AvatarUtils
+import com.common.core.myinfo.MyUserInfoManager
+import com.common.image.fresco.BaseImageView
+import com.component.level.utils.LevelConfigUtils
 import com.module.home.R
+import com.opensource.svgaplayer.SVGAImageView
 
 class EffectView : ConstraintLayout {
+    var bgSvga: SVGAImageView
+    var avatarIv: BaseImageView
+    var avatarBox: BaseImageView
+    var lightSvga: BaseImageView
+
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
     init {
         View.inflate(context, R.layout.all_effect_view_layout, this)
+
+        bgSvga = rootView.findViewById(R.id.bg_svga)
+        avatarIv = rootView.findViewById(R.id.avatar_iv)
+        avatarBox = rootView.findViewById(R.id.avatar_box)
+        lightSvga = rootView.findViewById(R.id.light_svga)
+
+        AvatarUtils.loadAvatarByUrl(avatarIv, AvatarUtils.newParamsBuilder(MyUserInfoManager.avatar)
+                .setCircle(true)
+                .build())
+
+        if (LevelConfigUtils.getImageResoucesLevel(MyUserInfoManager.myUserInfo?.ranking?.mainRanking
+                        ?: 0) != 0) {
+            avatarBox.setImageResource(LevelConfigUtils.getImageResoucesLevel(MyUserInfoManager.myUserInfo?.ranking?.mainRanking
+                    ?: 0))
+        }
     }
 }
