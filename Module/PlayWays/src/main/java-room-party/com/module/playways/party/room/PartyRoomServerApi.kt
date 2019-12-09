04d3802aa3b5a65c6484ecabddf7ff0d347c9d98
xuzhiyong <xuzhiyong@skrer.net>
2019-12-09 11:10:57 +0800

    添加房间管理员设置接口
    Signed-off-by: xuzhiyong <xuzhiyong@skrer.net>

--- Module/PlayWays/src/main/java-room-party/com/module/playways/party/room/PartyRoomServerApi.kt
+++ Module/PlayWays/src/main/java-room-party/com/module/playways/party/room/PartyRoomServerApi.kt
@@ -2,10 +2,12 @@ package com.module.playways.party.room
 
 import com.common.rxretrofit.ApiManager
 import com.common.rxretrofit.ApiResult
-import io.reactivex.Observable
 import okhttp3.RequestBody
 import retrofit2.Call
-import retrofit2.http.*
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.PUT
 
 interface PartyRoomServerApi {
     /**
@@ -21,49 +23,20 @@ interface PartyRoomServerApi {
     @PUT("http://dev.game.inframe.mobi/v1/partyroom/create-room")
     fun createRoom(@Body body: RequestBody): Call<ApiResult>
 
-        @Headers(ApiManager.ALWAYS_LOG_TAG)
+    @Headers(ApiManager.ALWAYS_LOG_TAG)
     @PUT("http://dev.game.inframe.mobi/v1/partyroom/exit-room")
     fun exitRoom(@Body body: RequestBody): Call<ApiResult>
 
     @PUT("http://dev.game.inframe.mobi/v1/partygame/heartbeat")
     fun heartbeat(@Body body: RequestBody): Call<ApiResult>
 
-//
-//    @Headers(ApiManager.ALWAYS_LOG_TAG)
-//    @PUT("http://dev.game.inframe.mobi/v1/relaygame/give-up")
-//    fun giveUpSing(@Body body: RequestBody): Call<ApiResult>
-//
-
-//
-
-//
-//    @GET("http://dev.game.inframe.mobi/v1/relaygame/sync-status")
-//    fun syncStatus(@Query("roomID") roomID: Long): Call<ApiResult>
-//
-//    @PUT("http://dev.game.inframe.mobi/v1/relaygame/unlock-user-info")
-//    fun sendUnlock(@Body body: RequestBody): Call<ApiResult>
-//
-//    @PUT("http://dev.game.inframe.mobi/v1/relaygame/mute")
-//    fun mute(@Body body: RequestBody): Observable<ApiResult>
-//
-//    @GET("http://dev.game.inframe.mobi/v1/relaygame/timestamp")
-//    fun timestamp(@Query("roomID") roomID: Int): Call<ApiResult>
-//
-//    /**
-//     *  同意合唱/PK的结果 {"roomID": 0,"uniqTag": "string"}
-//     */
-//    @PUT("http://dev.game.inframe.mobi/v1/relaygame/agree-sing")
-//    fun agreeSing(@Body body: RequestBody): Call<ApiResult>
-//
-//    /**
-//     *  查询同意合唱/PK的结果 {"roomID": 0,"uniqTag": "string"}
-//     */
-//    @GET("http://dev.game.inframe.mobi/v1/relaygame/agree-sing-result")
-//    fun getAgreeSingResult(@Query("roomID") roomID: Int, @Query("uniqTag") uniqTag: String): Call<ApiResult>
-//
-//    /**
-//     *  获取房间结果信息
-//     */
-//    @GET("http://dev.game.inframe.mobi/v1/relaygame/result")
-//    fun getRelayResult(@Query("roomID") roomID: Int): Call<ApiResult>
+    /**
+     * {
+    "adminUserID": 0,
+    "roomID": 0,
+    "setType": "SAT_UNKNOWN"  SAT_UNKNOWN, SAT_ADD, SAT_DEL
+    }
+     */
+    @PUT("http://dev.game.inframe.mobi/v1/partygame/set-admin")
+    fun setAdmin(@Body body: RequestBody): Call<ApiResult>
 }
--- Module/PlayWays/src/main/java-room-party/com/module/playways/party/room/fragment/AdministratorSelectFragment.kt
+++ Module/PlayWays/src/main/java-room-party/com/module/playways/party/room/fragment/AdministratorSelectFragment.kt
@@ -2,18 +2,30 @@ package com.module.playways.party.room.fragment
 
 import android.os.Bundle
 import android.support.v7.widget.RecyclerView
+import com.alibaba.fastjson.JSON
 import com.common.base.BaseFragment
 import com.common.core.view.setDebounceViewClickListener
+import com.common.rxretrofit.ApiManager
+import com.common.rxretrofit.ControlType
+import com.common.rxretrofit.RequestControl
+import com.common.rxretrofit.subscribe
 import com.common.utils.U
 import com.common.view.ex.ExImageView
-import com.common.view.ex.ExTextView
 import com.common.view.titlebar.CommonTitleBar
 import com.module.playways.R
+import com.module.playways.party.room.PartyRoomServerApi
+import com.module.playways.room.data.H
+import kotlinx.coroutines.launch
+import okhttp3.MediaType
+import okhttp3.RequestBody
 
 class AdministratorSelectFragment : BaseFragment() {
+    val mTag = "AdministratorSelectFragment"
     lateinit var titlebar: CommonTitleBar
     lateinit var bgIv: ExImageView
     lateinit var recyclerView: RecyclerView
+
+    val roomServerApi = ApiManager.getInstance().createService(PartyRoomServerApi::class.java)
 
     override fun initView(): Int {
         return R.layout.administrator_select_fragment_layout
@@ -25,6 +37,27 @@ class AdministratorSelectFragment : BaseFragment() {
         recyclerView = rootView.findViewById(R.id.recycler_view)
 
         titlebar.leftTextView.setDebounceViewClickListener { finish() }
+    }
+
+    private fun setAdmin(userID: Int, setType: Int) {
+        launch {
+            val map = mutableMapOf(
+                    "adminUserID" to userID,
+                    "roomID" to H.partyRoomData?.gameId,
+                    "setType" to setType
+            )
+
+            val body = RequestBody.create(MediaType.parse(ApiManager.APPLICATION_JSON), JSON.toJSONString(map))
+            val result = subscribe(RequestControl("${mTag} setAdmin", ControlType.CancelThis)) {
+                roomServerApi.setAdmin(body)
+            }
+
+            if (result.errno == 0) {
+
+            } else {
+                U.getToastUtil().showShort(result.errmsg)
+            }
+        }
     }
 
     override fun destroy() {
