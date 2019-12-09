b7de4b85c36c6805059f7014032bbde7cb652d6e
long jiang <longjiang@longdeMacBook-Pro.local>
2019-12-09 15:30:21 +0800

    接入发送表情的接口
    Signed-off-by: long jiang <longjiang@longdeMacBook-Pro.local>

--- Module/PlayWays/src/main/java-room-party/com/module/playways/party/room/seat/PartySeatView.kt
+++ Module/PlayWays/src/main/java-room-party/com/module/playways/party/room/seat/PartySeatView.kt
@@ -7,8 +7,15 @@ import android.util.AttributeSet
 import android.view.View
 import com.module.playways.R
 import android.support.v7.widget.RecyclerView
+import com.common.core.myinfo.MyUserInfoManager
 import com.module.playways.party.room.PartyRoomData
+import com.module.playways.party.room.event.PartySendEmojiEvent
 import com.module.playways.party.room.model.PartyActorInfoModel
+import com.module.playways.party.room.model.PartyPlayerInfoModel
+import com.zq.live.proto.PartyRoom.PDynamicEmojiMsg
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
 
 // 席位view
@@ -41,18 +48,34 @@ class PartySeatView : ConstraintLayout {
         adapter.notifyDataSetChanged()
     }
 
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onEvent(event: PDynamicEmojiMsg) {
+        // 房间内收到的发送表情信息
+        val user = PartyPlayerInfoModel.parseFromPb(event.user)
+        if (user.userID == MyUserInfoManager.uid.toInt()) {
+            // 把自己的过滤掉 donothing
+        } else {
+
+        }
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onEvent(event: PartySendEmojiEvent) {
+        // 自己发的 直接先更新
+    }
+
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
-//        if (!EventBus.getDefault().isRegistered(this)) {
-//            EventBus.getDefault().register(this)
-//        }
+        if (!EventBus.getDefault().isRegistered(this)) {
+            EventBus.getDefault().register(this)
+        }
     }
 
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
-//        if (EventBus.getDefault().isRegistered(this)) {
-//            EventBus.getDefault().unregister(this)
-//        }
+        if (EventBus.getDefault().isRegistered(this)) {
+            EventBus.getDefault().unregister(this)
+        }
     }
 
     //接收麦 和 表情 换位置 上下席位 的信息
