b9e03b992635c84d807b5cd77275d24eef517d88
chengsimin <chengsimin@xiaomi.com>
2019-12-18 16:44:41 +0800

    none

--- Module/PlayWays/src/main/java-room-party/com/module/playways/party/room/PartyRoomData.kt
+++ Module/PlayWays/src/main/java-room-party/com/module/playways/party/room/PartyRoomData.kt
@@ -111,20 +111,20 @@ class PartyRoomData : BaseRoomData<PartyRoundInfoModel>() {
      */
     var mySeatInfo: PartySeatInfoModel? = null
         private set(value) {
-            MyLog.w("chengsimin", "value=$value field=$field")
+//            MyLog.w("chengsimin", "value=$value field=$field")
             if (field == null && value == null) {
 
             } else {
                 if (field != null && value != null) {
                     if (field!!.same(value)) {
-                        MyLog.w("chengsimin", "same")
+//                        MyLog.w("chengsimin", "same")
                     } else {
-                        MyLog.w("chengsimin", "not same")
+//                        MyLog.w("chengsimin", "not same")
                         field = value
                         EventBus.getDefault().post(PartyMySeatInfoChangeEvent())
                     }
                 } else {
-                    MyLog.w("chengsimin", "not same")
+//                    MyLog.w("chengsimin", "not same")
                     field = value
                     EventBus.getDefault().post(PartyMySeatInfoChangeEvent())
                 }
