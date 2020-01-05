22718443498633060496191a98fe4d34abe54d06
xuzhiyong <xuzhiyong@skrer.net>
2020-01-05 11:17:17 +0800

    满槽添加动画
    Signed-off-by: xuzhiyong <xuzhiyong@skrer.net>

--- Module/PlayWays/src/main/java-room-grab/com/module/playways/grab/room/view/RelayEnergyView.kt
+++ Module/PlayWays/src/main/java-room-grab/com/module/playways/grab/room/view/RelayEnergyView.kt
@@ -3,12 +3,16 @@ package com.module.playways.grab.room.view
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.ScaleAnimation
 import com.common.log.MyLog
 import com.common.view.ex.ExRelativeLayout
 import com.common.view.ex.ExTextView
 import com.component.busilib.view.WaveProgressView
 import com.module.playways.R
 import com.module.playways.room.room.score.bar.ScoreTipsView
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class RelayEnergyView : ExRelativeLayout {
 
@@ -55,14 +59,34 @@ class RelayEnergyView : ExRelativeLayout {
         currentScore += score1
 
         if (currentScore > maxScore) {
-            //TODO 动画
             currentFullCount++
             fullCountTv.text = "x$currentFullCount"
             currentScore -= maxScore
-            waveProgressView.setCurrent(currentScore, "")
+            //满的情况下动画
+            waveProgressView.setCurrent(maxScore, "")
+            playScaleAnim {
+                waveProgressView.setCurrent(currentScore, "")
+            }
         } else {
             waveProgressView.setCurrent(currentScore, "")
         }
+    }
+
+    private fun playScaleAnim(call: (() -> Unit)) {
+        val scaleAnimation1 = ScaleAnimation(1.0f, 1.3f, 1.0f, 1.3f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+        scaleAnimation1!!.fillAfter = true
+        scaleAnimation1!!.duration = 200
+        val scaleAnimation2 = ScaleAnimation(1.3f, 1.0f, 1.3f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+        scaleAnimation2!!.fillAfter = true
+        scaleAnimation2!!.duration = 200
+        waveProgressView?.startAnimation(scaleAnimation1)
+        launch {
+            delay(300)
+            waveProgressView?.startAnimation(scaleAnimation2)
+            delay(200)
+            call.invoke()
+        }
+
     }
 
     fun reset() {
