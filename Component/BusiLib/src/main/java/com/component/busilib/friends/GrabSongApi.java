4dbac4dd4dabaacb3b0bc42023af059217861e0c
long jiang <jianglong@inframe.club>
2020-01-06 10:38:26 +0800

    好友房接口升到v3
    Signed-off-by: long jiang <jianglong@inframe.club>

--- Component/BusiLib/src/main/java/com/component/busilib/friends/GrabSongApi.java
+++ Component/BusiLib/src/main/java/com/component/busilib/friends/GrabSongApi.java
@@ -68,7 +68,7 @@ public interface GrabSongApi {
      *
      * @return
      */
-    @GET("http://dev.api.inframe.mobi/v2/fuel/tab-room-list")
+    @GET("http://dev.api.inframe.mobi/v3/fuel/tab-room-list")
     Observable<ApiResult> getRecommendRoomList(@Query("offset") int offset, @Query("testList") String testList, @Query("vars") String vars);
 
     /**
