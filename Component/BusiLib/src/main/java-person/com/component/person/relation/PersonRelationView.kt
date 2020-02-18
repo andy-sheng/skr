a3c4546dd00b09e41921d3f1c4aab3cd805d7a3f
jianglong <jianglong@inframe.club>
2020-02-18 22:41:36 +0800

    给加上
    Signed-off-by: jianglong <jianglong@inframe.club>

--- Component/BusiLib/src/main/java-person/com/component/person/relation/PersonRelationView.kt
+++ Component/BusiLib/src/main/java-person/com/component/person/relation/PersonRelationView.kt
@@ -35,6 +35,7 @@ class PersonRelationView(context: Context, attrs: AttributeSet?, defStyleAttr: I
     constructor(context: Context, attrs: AttributeSet) : this(context, attrs, 0)
 
     private val relationTitleTv: TextView
+    private val titleArrowIv: ImageView
     private val recyclerView: RecyclerView
     private val arrowIv: ImageView
     private val adapter: PersonRelationAdapter
@@ -46,12 +47,9 @@ class PersonRelationView(context: Context, attrs: AttributeSet?, defStyleAttr: I
         View.inflate(context, R.layout.person_relation_view_layout, this)
 
         relationTitleTv = this.findViewById(R.id.relation_title_tv)
+        titleArrowIv = this.findViewById(R.id.title_arrow_iv)
         recyclerView = this.findViewById(R.id.recycler_view)
         arrowIv = this.findViewById(R.id.arrow_iv)
-
-        arrowIv.setDebounceViewClickListener {
-            // todo 跳到fluttler页面
-        }
 
         adapter = PersonRelationAdapter(object : PersonRelationAdapter.Listener {
             override fun onClickItem(position: Int, model: RelationModel?) {
@@ -86,11 +84,15 @@ class PersonRelationView(context: Context, attrs: AttributeSet?, defStyleAttr: I
             if (result.errno == 0) {
                 val list = JSON.parseArray(result.data.getString("relationList"), RelationModel::class.java)
                 if (list.isNullOrEmpty()) {
-                    visibility = View.GONE
+                    recyclerView.visibility = View.GONE
+                    arrowIv.visibility = View.GONE
+                    titleArrowIv.visibility = View.VISIBLE
                     adapter.mDataList.clear()
                     adapter.notifyDataSetChanged()
                 } else {
-                    visibility = View.VISIBLE
+                    recyclerView.visibility = View.VISIBLE
+                    arrowIv.visibility = View.VISIBLE
+                    titleArrowIv.visibility = View.GONE
                     adapter.mDataList.clear()
                     adapter.mDataList.addAll(list)
                     adapter.notifyDataSetChanged()
