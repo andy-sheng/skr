7f821cfbadf580e359a34fd003fc45042b07f44e
jianglong <jianglong@inframe.club>
2020-02-20 23:39:54 +0800

    看大图吧加上
    Signed-off-by: jianglong <jianglong@inframe.club>

--- Component/BusiLib/src/main/java-person/com/component/person/photo/view/PersonPhotoView.kt
+++ Component/BusiLib/src/main/java-person/com/component/person/photo/view/PersonPhotoView.kt
@@ -2,22 +2,29 @@ package com.component.person.photo.view
 
 import android.content.Context
 import android.support.constraint.ConstraintLayout
+import android.support.v4.app.FragmentActivity
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.text.TextUtils
 import android.util.AttributeSet
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import com.alibaba.fastjson.JSON
+import com.common.callback.Callback
 import com.common.core.userinfo.UserInfoServerApi
 import com.common.rxretrofit.ApiManager
 import com.common.rxretrofit.ApiMethods
 import com.common.rxretrofit.ApiObserver
 import com.common.rxretrofit.ApiResult
+import com.common.utils.U
 import com.common.view.ex.ExTextView
 import com.component.busilib.R
 import com.component.person.photo.adapter.PhotoAdapter
 import com.component.person.photo.model.PhotoModel
+import com.imagebrowse.ImageBrowseView
+import com.imagebrowse.big.BigImageBrowseFragment
+import com.imagebrowse.big.DefaultImageBrowserLoader
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
@@ -40,6 +47,9 @@ class PersonPhotoView(context: Context, attrs: AttributeSet?, defStyleAttr: Int)
 
     private val photoAdapter: PhotoAdapter
 
+    private var userID:Long = 0
+    private var mHasMore = false
+
     init {
         View.inflate(context, R.layout.person_center_photo_view, this)
 
@@ -53,27 +63,74 @@ class PersonPhotoView(context: Context, attrs: AttributeSet?, defStyleAttr: Int)
         recyclerView.layoutManager = GridLayoutManager(context, 3)
         photoAdapter = PhotoAdapter(PhotoAdapter.TYPE_PERSON_CENTER_VIEW)
         recyclerView.adapter = photoAdapter
+
+        photoAdapter.mOnClickPhotoListener = { _, position, _ ->
+            BigImageBrowseFragment.open(true, context as FragmentActivity, object : DefaultImageBrowserLoader<PhotoModel>() {
+                override fun init() {
+
+                }
+
+                override fun load(imageBrowseView: ImageBrowseView, position: Int, item: PhotoModel) {
+                    if (TextUtils.isEmpty(item.picPath)) {
+                        imageBrowseView.load(item.localPath)
+                    } else {
+                        imageBrowseView.load(item.picPath)
+                    }
+                }
+
+                override fun getInitCurrentItemPostion(): Int {
+                    return position
+                }
+
+                override fun getInitList(): List<PhotoModel>? {
+                    return photoAdapter?.mDataList
+                }
+
+                override fun loadMore(backward: Boolean, position: Int, data: PhotoModel, callback: Callback<List<PhotoModel>>?) {
+                    if (backward) {
+                        // 向后加载
+                        getPhotos(photoAdapter?.successNum ?: 0, Callback { r, list ->
+                            if (callback != null && list != null) {
+                                callback.onCallback(0, list)
+                            }
+                        })
+                    }
+                }
+
+                override fun hasMore(backward: Boolean, position: Int, data: PhotoModel): Boolean {
+                    return if (backward) {
+                        mHasMore
+                    } else false
+                }
+            })
+        }
     }
 
     fun initData(userID: Long, flag: Boolean) {
         // 也设一个时间间隔吧
+        this.userID = userID
         val now = System.currentTimeMillis()
         if (!flag) {
             if (now - mLastUpdateTime < 60 * 1000) {
                 return
             }
         }
-        getPhotos(userID)
+        getPhotos(0)
     }
 
-    private fun getPhotos(userID: Long) {
-        ApiMethods.subscribe(userInfoServerApi.getPhotos(userID, 0, 20), object : ApiObserver<ApiResult>() {
+    private fun getPhotos(off: Int, callback: Callback<List<PhotoModel>>? = null) {
+        ApiMethods.subscribe(userInfoServerApi.getPhotos(userID, off, 20), object : ApiObserver<ApiResult>() {
             override fun process(result: ApiResult?) {
                 if (result != null && result.errno == 0) {
                     mLastUpdateTime = System.currentTimeMillis()
                     val list = JSON.parseArray(result.data?.getString("pic"), PhotoModel::class.java)
                     val totalCount = result.data!!.getIntValue("totalCount")
-                    addPhotos(list, totalCount, true)
+                    if (off == 0) {
+                        addPhotos(list, totalCount, true)
+                    } else {
+                        addPhotos(list, totalCount, false)
+                    }
+                    callback?.onCallback(0, list)
                 }
             }
 
@@ -84,6 +141,7 @@ class PersonPhotoView(context: Context, attrs: AttributeSet?, defStyleAttr: Int)
     }
 
     private fun addPhotos(list: List<PhotoModel>?, totalCount: Int, clear: Boolean) {
+        mHasMore = !list.isNullOrEmpty()
         if (totalCount == 0) {
             photoArrow.visibility = View.GONE
             photoTitleArrow.visibility = View.VISIBLE
