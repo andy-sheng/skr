011de78cd079dc2247e58e83073d5f2d5ec55728
xuzhiyong <xuzhiyong@skrer.net>
2020-02-20 10:58:16 +0800

    获取商品tag接口v2-v3
    Signed-off-by: xuzhiyong <xuzhiyong@skrer.net>

--- Module/Home/src/main/java-mall/com.module.mall/MallServerApi.kt
+++ Module/Home/src/main/java-mall/com.module.mall/MallServerApi.kt
@@ -16,7 +16,7 @@ interface MallServerApi {
      *
      * @return
      */
-    @GET("http://sandbox.api.inframe.mobi/v2/mall/display-type-tag")
+    @GET("http://sandbox.api.inframe.mobi/v3/mall/display-type-tag")
     fun getMallDisplayTags(): Call<ApiResult>
 
     /**
